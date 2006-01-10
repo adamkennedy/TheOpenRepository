adb12fe4ceef1c95d89ad949ce83d12d532c6a0d
adam@phase-n.com <adam@phase-n.com@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2006-01-10 19:51:30 +0000

    Adding the first version of Perl-PowerToys

---
+++ Perl-PowerToys/lib/Perl/PowerToys.pm
@@ -0,0 +1,58 @@
+package Perl::PowerToys;
+
+use 5.005;
+use strict;
+
+use vars qw{$VERSION};
+BEGIN {
+	$VERSION = '.01';
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Perl::PowerToys - A collection of PPI-based utilities from the author of PPI
+
+=head1 DESCRIPTION
+
+The Perl PowerToys are a neat little collection of utilities for working
+with Perl files from Adam Kennedy, the author of L<PPI>.
+
+=head2 ppichangeversion 0.01 0.02
+
+Takes two version numbers.
+
+Scans through all files without the current directory with the statement
+
+  $VERSION = '0.01';
+
+Changes it to the new version.
+
+=head1 SUPPORT
+
+Bugs should be reported via the CPAN bug tracker at
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Perl-PowerToys>
+
+For other issues, contact the author.
+
+=head1 AUTHOR
+
+Adam Kennedy E<lt>cpan@ali.asE<gt>, L<http://ali.as/>
+
+=head1 COPYRIGHT
+
+Copyright 2006 Adam Kennedy. All rights reserved.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
---
+++ Perl-PowerToys/t/01_compile.t
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+# Compile-testing for Perl::PowerToys
+
+use strict;
+use lib ();
+use File::Spec::Functions ':ALL';
+BEGIN {
+	$| = 1;
+	unless ( $ENV{HARNESS_ACTIVE} ) {
+		require FindBin;
+		$FindBin::Bin = $FindBin::Bin; # Avoid a warning
+		chdir catdir( $FindBin::Bin, updir() );
+		lib->import(
+			catdir('blib', 'lib'),
+			'lib',
+			);
+	}
+}
+
+use Test::More tests => 2;
+
+BEGIN {
+	ok( $] > 5.005, 'Perl version is 5.005 or newer' );
+	use_ok( 'Perl::PowerToys' );
+}
+
+exit(0);
