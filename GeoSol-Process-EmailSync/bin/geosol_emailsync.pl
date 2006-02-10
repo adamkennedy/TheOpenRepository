5e5817382d3d4a9327e0bcb8c85cb05f4d979a5f
adam@phase-n.com <adam@phase-n.com@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2006-02-10 09:49:24 +0000

    Adding the geosol_emailsync.pl script

---
+++ GeoSol-Process-EmailSync/bin/geosol_emailsync.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl -w
+
+# Basic test that ONLY loads the modules to ensure that all the code compiles
+
+use strict;
+use lib ();
+use File::Spec::Functions ':ALL';
+BEGIN {
+	$| = 1;
+	unless ( $ENV{TEST_HARNESS} ) {
+		# Special fast option for CGI environment, as FindBin won't work in
+		# the web environments of some web hosts.
+		if ( $ENV{SCRIPT_FILENAME} and $ENV{SCRIPT_FILENAME} =~ /^(.+\/)/ ) {
+			chdir catdir( $1, updir() );
+		} else {
+			require FindBin;
+			chdir catdir( $FindBin::Bin, updir() );
+			$FindBin::Bin = $FindBin::Bin; # Avoid a "only used once" warning
+		}
+
+		# Set the lib path if we aren't in a harness
+		lib->import(
+			catdir('blib', 'arch'),
+			catdir('blib', 'lib'),
+			'lib',
+			);
+	}
+}
+
+# Define and check the GEOSOL_ROOT location
+$ENV{GEOSOL_ROOT} = '/home/geosol/virtual/geologicalsolutions.com.au/http';
+unless ( $ENV{GEOSOL_ROOT} and -d $ENV{GEOSOL_ROOT} ) {
+	quit('GEOSOL_ROOT is not defined, or does not exist');
+}
+
+use GeoSol::Process::EmailSync;
+
+# Create the new default object and run it
+my $object = GeoSol::Process::EmailSync->default;
+unless ( $object ) {
+	quit('Failed to create default sync object');
+}
+unless ( $object->prepare ) {
+	quit('Failed to prepare sync object');
+}
+unless ( $object->run ) {
+	quit('Failed to run sync object');
+}
+
+quit();
+
+
+
+
+
+#####################################################################
+# Support Functions
+
+sub quit {
+	if ( $_[0] ) {
+		my $message = shift;
+		$message =~ s/[\012\015]+$/\n/;
+		print $message;
+		exit(255);
+	} else {
+		exit(0);
+	}
+}
+
+1;
