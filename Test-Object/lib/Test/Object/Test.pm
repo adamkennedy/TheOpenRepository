4c88f674c2aa427e0fe104c186b520aca4bf054f
adam <adam@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2006-05-04 01:47:07 +0000

    Adding the missing lib

---
+++ Test-Object/lib/Test/Object/Test.pm
@@ -0,0 +1,54 @@
+package Test::Object::Test;
+
+use strict;
+use Carp         ();
+use Scalar::Util ();
+
+use vars qw{$VERSION};
+BEGIN {
+	$VERSION = '0.03';
+}
+
+sub new {
+	my $class = shift;
+	my $self  = bless { @_ }, $class;
+
+	# Check params
+	unless ( _CLASS($self->class) ) {
+		Carp::croak("Did not provide a valid test class");
+	}
+	unless ( _CALLABLE($self->code) ) {
+		Carp::croak("Did not provide a valid CODE or callable object");
+	}
+
+	$self;
+}
+
+sub class {
+	$_[0]->{class};
+}
+
+sub tests {
+	$_[0]->{tests};
+}
+
+sub code {
+	$_[0]->{code};
+}
+
+sub run {
+	$_[0]->code->( $_[1] );
+}
+
+sub _CLASS ($) {
+	(defined $_[0] and ! ref $_[0] and $_[0] =~ m/^[^\W\d]\w*(?:::\w+)*$/s) ? $_[0] : undef;
+}
+
+sub _CALLABLE {
+	(Scalar::Util::reftype($_[0])||'') eq 'CODE'
+	or
+	Scalar::Util::blessed($_[0]) and overload::Method($_[0],'&{}')
+	? $_[0] : undef;
+}
+
+1;
