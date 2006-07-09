d5b986236d4f631fb833cd20776c114690e16d76
dagolden@cpan.org <dagolden@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2006-07-09 22:10:36 +0000

    - updated Build docs

--- Perl-Dist-Vanilla/lib/Perl/Dist/Vanilla/Build.pm
+++ Perl-Dist-Vanilla/lib/Perl/Dist/Vanilla/Build.pm
@@ -1,5 +1,5 @@
 package Perl::Dist::Vanilla::Build;
-$VERSION = 5;
+$VERSION = 6;
 use strict;
 use warnings;
 
@@ -9,10 +9,6 @@ __END__
 =head1 NAME
 
 Perl::Dist::Vanilla::Build - Building Vanilla Perl from scratch
-
-=head1 VERSION
-
-This document refers to Vanilla Perl Build 5.
 
 =head1 DESCRIPTION
 
@@ -24,86 +20,22 @@ installation itself.
 This document describes the build process for generating a Vanilla Perl
 executable installer.  
 
-As of Build 4, this documentation is incomplete.  Common build components will
-be refactored into a separate package soon and docs will be revised more 
-completely at that time.
-
 =head1 PREREQUISITES
 
 Perl::Dist::Vanilla requires an existing Win32 Perl installation to bootstrap
-the installation process.  It also requires numerous modules for
-the build scripts.  These modules are listed in the Perl::Dist::Vanilla 
-META.yml.  Installing Perl::Dist::Vanilla will pick up all necessary
-dependencies.
+the installation process and L<Perl::Dist::Builder>.  See
+L<Perl::Dist::Bootstrap> for details on bootstrapping a Perl that can build
+Vanilla Perl.
 
 Perl::Dist::Vanilla also requires the free Inno Setup tool to create the 
 executable installer.  Inno Setup can be downloaded from jrsoftware.org:
 
 L<http://www.jrsoftware.org/isinfo.php>
 
-=head1 BOOTSTRAPPING
-
-Perl hard-codes @INC directories into the binary, making it difficult to
-relocate on the fly.  As a result, Vanilla Perl needs to be built at 
-C<< C:\vanilla-perl >>.  To use Perl::Dist::Vanilla, a separate perl
-installation must be available on the same machine.  Here is one way of 
-boostrapping to that point, assuming no Perl already exists on the
-computer in question:
-
-=over
-
-=item *
-
-Download a Vanilla Perl executable installer and install it.  It will be
-located at C<< C:\vanilla-perl >>.  
-
-=item *
-
-Install the Perl::Dist::Vanilla tarball from CPAN to pick up all dependencies.
-
-=item *
-
-Download a separate copy of the Perl::Dist::Vanilla tarball to access the
-programs and extra files for the build process.  Unpack it and open a
-command shell in that directory.
-
-=item *
-
-Edit C<< vanilla.yml >> and change the "image_dir" parameter to a different
-path, e.g. C<< C:\bootstrap-perl >> for this example.
-
-=item *
-
-Run C<< perl bin\full_build.pl >>, which will build and install a
-Vanilla Perl installation at that location.  (See the next section for
-details.)
-
-=item *
-
-Edit the PATH, LIB, and INCLUDE environment variables to refer to 
-C<< bootstrap-perl >> instead of C<< vanilla-perl >>.
-
-=item *
-
-Install the Perl::Dist::Vanilla tarball from CPAN to pick up all dependencies
-again, this time installed in the fresh C<< bootstrap-perl >>.
-
-=item *
-
-Change the C<< vanilla.yml >> image_dir parameter back to 
-C<< C:\vanilla-perl >>.
-
-=back
-
-At this point, the perl located at C<< C:\bootstrap-perl >> will be the default
-perl and will not be affected by rebuilding a new Vanilla Perl at 
-C<< vanilla-perl >>.  
-
 =head1 BUILDING THE INSTALLATION SOURCE DIRECTORY
 
-From the Perl::Dist::Vanilla tarball directory, the C<< bin\full_build.pl >>
-program will build a full Vanilla Perl installation, just as in the
-bootstrap process.  This includes:
+From the Perl::Dist::Vanilla tarball directory, the C<< bin\build_all.pl >>
+program will build a full Vanilla Perl installation.  This includes:
 
 =over
 
@@ -133,10 +65,13 @@ will be documented in a subsequent release).
 =head1 PACKAGING AS AN EXECUTABLE INSTALLER
 
 Vanilla Perl is currently bundled into an executable installer with the
-Inno Setup program using the included C<< perl.iss >> script.  The 
+Inno Setup program using the included C<< vanilla.iss >> script.  The 
 script controls the name and location of the executable.
 
-To create the .exe file, open C<< perl.iss >> in Inno Setup and compile it.
+To create the .exe file, open C<< vanilla.iss >> in Inno Setup and compile it.
+
+If the Inno Setup tool is in a standard location, the C<bin\run_inno_setup.pl>
+script will compile vanilla.iss from the command line.
 
 =head1 CONTACTS AND BUGS REPORTING
 
