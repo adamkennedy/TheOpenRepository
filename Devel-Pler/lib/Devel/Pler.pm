aa94a187da33715d53de6e24ebd41324b0994b68
adam <adam@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2006-07-27 15:27:42 +0000

    Adding a few minor features

--- Devel-Pler/lib/Devel/Pler.pm
+++ Devel-Pler/lib/Devel/Pler.pm
@@ -9,7 +9,7 @@ use File::Spec::Functions ':ALL';
 
 use vars qw{$VERSION @ISA @EXPORT};
 BEGIN {
-	$VERSION = '0.11';
+	$VERSION = '0.12';
 	@ISA     = qw{ Exporter };
 	@EXPORT  = qw{
 		in_distroot has_makefile has_blib has_lib
@@ -71,6 +71,10 @@ BEGIN {
 	$VERBOSE ||= 0;
 }
 
+sub is_verbose {
+	$VERBOSE;
+}
+
 sub verbose ($) {
 	message( $_[0] ) if $VERBOSE;
 }
@@ -97,3 +101,62 @@ sub handoff ($) {
 }
 
 1;
+
+# Since not everything is smart about where to pull things from,
+# we add a copy of the POD docs here.
+
+=pod
+
+=head1 NAME
+
+pler - The DWIM Perl Debugger
+
+=head1 DESCRIPTION
+
+B<pler> is a small script which provides a sanity layer for debugging
+test scripts in Perl distributions.
+
+While L<prove> has proven itself to be a highly useful program for
+manually running one or more groups of scripts in a distribution,
+what we also need is something that provides a similar level of
+intelligence in a debugging context.
+
+B<pler> checks that the environment is sound, runs some cleanup tasks
+if needed, makes sure you are in the right directory, and then hands off
+to the perl debugger as normal.
+
+=head1 TO DO
+
+- Allow execution from the base or F<t> directory.
+
+- Automatically run the F<Makefile.PL> if it needs to be
+
+- Write a heuristic to determine if it needs to be
+
+=head1 SUPPORT
+
+All bugs should be filed via the bug tracker at
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Devel-Pler>
+
+For other issues, or commercial enhancement and support, contact the author
+
+=head1 AUTHOR
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+L<prove>, L<http://ali.as/>
+
+=head1 COPYRIGHT
+
+Copyright 2006 Adam Kennedy. All rights reserved.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
