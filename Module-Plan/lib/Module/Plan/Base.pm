9b624a4ef0c262640b475f0d9af5ab8f1c4c1c4f
adam <adam@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2006-10-14 19:03:52 +0000

    Syncing for rename

--- Module-Plan/lib/Module/Plan/Base.pm
+++ Module-Plan/lib/Module/Plan/Base.pm
@@ -127,6 +127,58 @@ sub add_file {
 	# Add the name and the file name
 	push @{ $self->{names} }, $name;
 	$self->{dists}->{$name} = $file;
+
+	return 1;
 }
 
 1;
+
+=pod
+
+=head1 SUPPORT
+
+This module is stored in an Open Repository at the following address.
+
+L<http://svn.phase-n.com/svn/cpan/trunk/Module-Plan-Base>
+
+Write access to the repository is made available automatically to any
+published CPAN author, and to most other volunteers on request.
+
+If you are able to submit your bug report in the form of new (failing)
+unit tests, or can apply your fix directly instead of submitting a patch,
+you are B<strongly> encouraged to do so. The author currently maintains
+over 100 modules and it may take some time to deal with non-Critical bug
+reports or patches.
+
+This will guarentee that your issue will be addressed in the next
+release of the module.
+
+If you cannot provide a direct test or fix, or don't have time to do so,
+then regular bug reports are still accepted and appreciated via the CPAN
+bug tracker.
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Module-Plan-Base>
+
+For other issues, for commercial enhancement and support, or to have your
+write access enabled for the repository, contact the author at the email
+address above.
+
+=head1 AUTHORS
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+L<Module::Plan>, L<Module::Inspector>
+
+=head1 COPYRIGHT
+
+Copyright 2006 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
