c8057bfcc7b49f6b89355f81a68ca6de63bac4f9
adam <adam@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2006-11-14 16:48:29 +0000

    Importing Win32::GuiTest

---
+++ Win32-GuiTest/Recorder/README.txt
@@ -0,0 +1,17 @@
+Win32-GuiTest Recorder v1.03 (05-29-2004)
+----------------------------------------------
+An initial script recording application to use with
+Win32::GuiTest (1.50.x-ad and greater).
+
+Updates, source, and general information regarding this
+product are currently available at:
+http://sourceforge.net/projects/winguitest
+
+Also, you should read the application's About Box;
+which can be found in its system menu.
+
+You may distribute under the terms of the GNU General 
+Public License.  Please refer to Copying.txt for more
+information.
+
+----------------------------------------------
---
+++ Win32-GuiTest/makedist.pl
@@ -0,0 +1,59 @@
+#!perl -w
+# $Id: makedist.pl,v 1.4 2004/07/21 22:21:46 szabgab Exp $
+
+# Compile everything (including things in the eg directory) and
+# generate the Win32::GuiTest distribution.
+
+use strict;
+use File::Path;
+use File::Copy;
+
+use FindBin qw($Bin);
+sub sys {
+  my $s = shift;
+  `echo --- $s --- >>makedist.log`;
+  system("$s >>makedist.log 2>&1")==0 or die "Cannot '$s'\n";
+}
+
+unlink("makedist.log");
+sys("nmake clean") if -e "makefile";
+sys("perl make_eg.pl");
+sys("perl makefile.pl");
+sys("nmake");
+
+open(MAN, "<manifest");
+my @manifest = <MAN>;
+close(MAN);
+
+$ENV{PERL5LIB}="$Bin/blib/lib;$Bin/blib/arch";
+foreach my $file (@manifest) {
+  next if $file =~ /Examples.pm/;
+}
+sys("nmake test");
+sys("makepod GuiTest");
+sys("makepod Examples");
+sys("copy guitest.txt README");
+sys("copy guitest.html README.html");
+
+# this will enable ppm to install the HTML files in the regular HTMl tree
+mkpath "blib/html/site/lib/Win32/GuiTest";
+copy "guitest.html", "blib/html/site/lib/Win32/";
+copy "Examples.html", "blib/html/site/lib/Win32/GuiTest";
+
+#sys("call makepod lib\win32\guitest\cpl");
+#sys("call makepod lib\win32\guitest\which");
+unlink("Win32-GuiTest.tar.gz");
+sys("tar cvf Win32-GuiTest.tar blib");
+sys("gzip --best Win32-GuiTest.tar");
+sys("nmake ppd");
+
+local($^I, @ARGV) = ('.bak', 'Win32-GuiTest.ppd');
+while (<>) {
+    s|<CODEBASE HREF=""\s*/>|<CODEBASE HREF="Win32-GuiTest.tar.gz"/>|;
+    print;
+    close ARGV if eof;
+}
+
+sys("nmake zipdist");
+sys("notepad makedist.log");
+
