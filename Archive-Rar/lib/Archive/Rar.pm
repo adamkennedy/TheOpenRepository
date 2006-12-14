435134cbd3420cec99298ac3d84d2b9f4e46f240
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2006-12-14 12:36:23 +0000

    This is Archive::Rar 1.91. Changes: * Changed distribution file layout to be a little more modern. * Added dependency on Data::Dumper to Makefile.PL. * Added dependency on Win32::Registry to Makefile.PL (for win32 only). * Machine-readable license statement in Makefile.PL. * Add META.yml to distribution. * Corrected MANIFEST. * Now using "use strict". * Removed "#!/usr/local/bin" from module. * Converted to unix line endings. * Corrected error in README. * Converted tests to use Test::Simple. * Added (optional!) POD tests.

--- Archive-Rar/lib/Archive/Rar.pm
+++ Archive-Rar/lib/Archive/Rar.pm
@@ -1,11 +1,11 @@
-#!/usr/local/bin/perl 
-
-
 package Archive::Rar;
 
-
 require 5.002;
+
+use strict;
 require Exporter;
+use vars ('@ISA', '@EXPORT', '$VERSION', '$version');
+
 @ISA = qw(Exporter);
 @EXPORT = qw( );
 
@@ -88,6 +88,7 @@ sub WarnOutput {
 # ---------------------------------------------------------------------------
 # --  
 # -- 
+my $unique_instance;
 sub self_or_default {
     return @_ if defined($_[0]) && (!ref($_[0])) &&($_[0] eq __PACKAGE__);
 	unless (defined($_[0]) && ref($_[0]) eq __PACKAGE__) {
@@ -105,7 +106,7 @@ sub TestExe {
 	my $redirect ='';
 	# $redirect =' > NUL:' if ($me->{sys} eq 'w');
 	# print "--" . system("$cmd $redirect") . "--\n";
-	@r =qx/$cmd/;
+	my @r =qx/$cmd/;
 	return 1 if ($#r > 10);
 	return undef;
 	print "$#r \n";
@@ -121,14 +122,14 @@ sub SearchExe {
 	
     if ($me->{sys} eq 'w') {
     	my ($clef,$type,$value);
-    	# on essaie de piquer le chemin d'install par la clef d'ex‚cution.
+    	# on essaie de piquer le chemin d'install par la clef d'execution.
     	if ($::HKEY_LOCAL_MACHINE->Open('SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\WinRAR.exe', $clef)
     		&& $clef->QueryValueEx("path", $type, $value)) {
 		    $value =~ s/\\/\//g;
 		    $cmd =$value . '/rar.exe';
 		    goto Good if (-e $cmd);
     	}
-    	# ou alors par le d‚sinstalleur.
+    	# ou alors par le desinstalleur.
     	if ($::HKEY_LOCAL_MACHINE->Open('SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\WinRAR archiver', $clef)
     		&& $clef->QueryValueEx("UninstallString", $type, $value)) {
 		    $value =~ s/\\/\//g;
@@ -139,7 +140,7 @@ sub SearchExe {
     	# on tente le chemin 'normal'.
 	    $cmd ='c:/program files/winrar/rar.exe';
 	    goto Good if (-e $cmd);
-    	# alors une ex‚cution direct.
+    	# alors une execution direct.
 		$cmd ='rar';
 		goto Good if ($me->TestExe($cmd));
     	# en dernier recourt...
@@ -183,7 +184,7 @@ sub initialize {
     	# use Win32::Registry;
 		eval("use Win32::Registry");
 		if ($@ ne '') {
-			die "Impossible de charger le module Win32::Registry";
+			die "Cannot load module Win32::Registry";
 		}
     	$me->{sys} ='w';
     } else {
@@ -351,6 +352,7 @@ sub List {
 	}
 	$res =$me->Execute("$me->{rar} $me->{command} $me->{options} $me->{archive} " . join(' ',@{$args->{'-files'}}));
 	$in =0;
+    my $first;
 	foreach (@{$me->{output}}) {
 		s/[\s\n\r]+$//;
 		next if ($_ eq '');
@@ -556,105 +558,105 @@ Level of verbosity.
 
 =head1 AUTHORS
 
-jean-marc boulade E<lt>F<jmbperl@hotmail.com>E<gt>
+jean-marc boulade E<lt>jmbperl@hotmail.comE<gt>
 
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2002 jean-marc boulade. All rights reserved.
+Copyright (c) 2002-2006 jean-marc boulade. All rights reserved.
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
 
-=pod
+=head1 RAR DOCUMENTATION
 
-"C:/Program Files/WinRAR/rar.exe"
+  "C:/Program Files/WinRAR/rar.exe"
 
-RAR 2.80    Copyright (c) 1993-2001 Eugene Roshal    2 Mar 2001
-Shareware version         Type RAR -? for help
-
-Usage:     rar <command> -<switch 1> -<switch N> <archive> <files...>
-               <@listfiles...> <path_to_extract\>
-
-<Commands>
-  a             Add files to archive
-  c             Add archive comment
-  cf            Add files comment
-  cw            Write archive comment to file
-  d             Delete files from archive
-  e             Extract files to current directory
-  f             Freshen files in archive
-  k             Lock archive
-  l[t]          List archive [technical]
-  m[f]          Move to archive [files only]
-  p             Print file to stdout
-  r             Repair archive
-  rr[N]         Add data recovery record
-  s[name|-]     Convert archive to or from SFX
-  t             Test archive files
-  u             Update files in archive
-  v[t]          Verbosely list archive [technical]
-  x             Extract files with full path
-
-<Switches>
-  -             Stop switches scanning
-  ac            Clear Archive attribute after compression or extraction
-  ag[format]    Generate archive name using the current date
-  ao            Add files with Archive attribute set
-  ap<path>      Set path inside archive
-  as            Synchronize archive contents
-  av            Put authenticity verification (registered versions only)
-  av-           Disable authenticity verification check
-  c-            Disable comments show
-  cfg-          Disable read configuration
-  cl            Convert names to lower case
-  cu            Convert names to upper case
-  df            Delete files after archiving
-  dh            Open shared files
-  ds            Disable name sort for solid archive
-  e<attr>       Set file exclude attributes
-  ed            Do not add empty directories
-  ep            Exclude paths from names
-  ep1           Exclude base directory from names
-  ep2           Expand paths to full
-  f             Freshen files
-  idp           Disable percentage display
-  ierr          Send all messages to stderr
-  ilog          Log errors to file (registered versions only)
-  inul          Disable all messages
-  isnd          Enable sound
-  k             Lock archive
-  kb            Keep broken extracted files
-  m<0..5>       Set compression level (0-store...3-default...5-maximal)
-  md<size>      Set dictionary size in KB (64,128,256,512,1024 or A,B,C,D,E)
-  mm[f]         Multimedia compression [force]
-  o+            Overwrite existing files
-  o-            Do not overwrite existing files
-  os            Save NTFS streams
-  ow            Save or restore file owner and group
-  p[password]   Set password
-  p-            Do not query password
-  r             Recurse subdirectories
-  r0            Recurse subdirectories for wildcard names only
-  ri<P>[:<S>]   Set priority (0-default,1-min..15-max) and sleep time in ms
-  rr[N]         Add data recovery record
-  s[<N>,d,e]    Create solid archive
-  s-            Disable solid archiving
-  sfx[name]     Create SFX archive
-  tk            Keep original archive time
-  tl            Set archive time to latest file
-  tn<time>      Add files newer than <time>
-  to<time>      Add files older than <time>
-  u             Update files
-  v             Create volumes with size autodetection or list all volumes
-  v<size>[k,b]  Create volumes with size=<size>*1000 [*1024, *1]
-  vd            Erase disk contents before creating volume
-  vp            Pause before each volume
-  w<path>       Assign work directory
-  x<file>       Exclude specified file
-  x@            Read file names to exclude from stdin
-  x@<list>      Exclude files in specified list file
-  y             Assume Yes on all queries
-  z<file>       Read archive comment from file
+  RAR 2.80    Copyright (c) 1993-2001 Eugene Roshal    2 Mar 2001
+  Shareware version         Type RAR -? for help
+  
+  Usage:     rar <command> -<switch 1> -<switch N> <archive> <files...>
+                 <@listfiles...> <path_to_extract\>
+  
+  <Commands>
+    a             Add files to archive
+    c             Add archive comment
+    cf            Add files comment
+    cw            Write archive comment to file
+    d             Delete files from archive
+    e             Extract files to current directory
+    f             Freshen files in archive
+    k             Lock archive
+    l[t]          List archive [technical]
+    m[f]          Move to archive [files only]
+    p             Print file to stdout
+    r             Repair archive
+    rr[N]         Add data recovery record
+    s[name|-]     Convert archive to or from SFX
+    t             Test archive files
+    u             Update files in archive
+    v[t]          Verbosely list archive [technical]
+    x             Extract files with full path
+  
+  <Switches>
+    -             Stop switches scanning
+    ac            Clear Archive attribute after compression or extraction
+    ag[format]    Generate archive name using the current date
+    ao            Add files with Archive attribute set
+    ap<path>      Set path inside archive
+    as            Synchronize archive contents
+    av            Put authenticity verification (registered versions only)
+    av-           Disable authenticity verification check
+    c-            Disable comments show
+    cfg-          Disable read configuration
+    cl            Convert names to lower case
+    cu            Convert names to upper case
+    df            Delete files after archiving
+    dh            Open shared files
+    ds            Disable name sort for solid archive
+    e<attr>       Set file exclude attributes
+    ed            Do not add empty directories
+    ep            Exclude paths from names
+    ep1           Exclude base directory from names
+    ep2           Expand paths to full
+    f             Freshen files
+    idp           Disable percentage display
+    ierr          Send all messages to stderr
+    ilog          Log errors to file (registered versions only)
+    inul          Disable all messages
+    isnd          Enable sound
+    k             Lock archive
+    kb            Keep broken extracted files
+    m<0..5>       Set compression level (0-store...3-default...5-maximal)
+    md<size>      Set dictionary size in KB (64,128,256,512,1024 or A,B,C,D,E)
+    mm[f]         Multimedia compression [force]
+    o+            Overwrite existing files
+    o-            Do not overwrite existing files
+    os            Save NTFS streams
+    ow            Save or restore file owner and group
+    p[password]   Set password
+    p-            Do not query password
+    r             Recurse subdirectories
+    r0            Recurse subdirectories for wildcard names only
+    ri<P>[:<S>]   Set priority (0-default,1-min..15-max) and sleep time in ms
+    rr[N]         Add data recovery record
+    s[<N>,d,e]    Create solid archive
+    s-            Disable solid archiving
+    sfx[name]     Create SFX archive
+    tk            Keep original archive time
+    tl            Set archive time to latest file
+    tn<time>      Add files newer than <time>
+    to<time>      Add files older than <time>
+    u             Update files
+    v             Create volumes with size autodetection or list all volumes
+    v<size>[k,b]  Create volumes with size=<size>*1000 [*1024, *1]
+    vd            Erase disk contents before creating volume
+    vp            Pause before each volume
+    w<path>       Assign work directory
+    x<file>       Exclude specified file
+    x@            Read file names to exclude from stdin
+    x@<list>      Exclude files in specified list file
+    y             Assume Yes on all queries
+    z<file>       Read archive comment from file
 
 =cut
