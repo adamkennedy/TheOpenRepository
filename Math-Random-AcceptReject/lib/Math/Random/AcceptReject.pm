df470db013ae949b90c2ea283ae2635459b6e673
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2007-01-05 11:11:00 +0000

    Replace ü in my name with ue...

--- Math-Random-AcceptReject/lib/Math/Random/AcceptReject.pm
+++ Math-Random-AcceptReject/lib/Math/Random/AcceptReject.pm
@@ -203,11 +203,11 @@ The examples in the F<examples/> subdirectory of this distribution.
 
 =head1 AUTHOR
 
-Steffen Müller, E<lt>smueller@cpan.orgE<gt>
+Steffen Mueller, E<lt>smueller@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2007 by Steffen Müller
+Copyright (C) 2007 by Steffen Mueller
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.6 or,
