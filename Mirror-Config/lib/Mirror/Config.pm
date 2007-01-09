d8b0bf2842ad5129a838781dfc16098b1f21d333
adam <adam@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2007-01-09 13:23:42 +0000

    Adding POD

--- Mirror-Config/lib/Mirror/Config.pm
+++ Mirror-Config/lib/Mirror/Config.pm
@@ -13,3 +13,43 @@ sub name {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Mirror Configuration Object
+
+=head1 DESCRIPTION
+
+blah
+
+=head1 SUPPORT
+
+Bugs should be reported via the CPAN bug tracker at
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mirror-Config>
+
+For other issues, or commercial enhancement or support, contact the author.
+
+=head1 AUTHOR
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+L<YAML::Tiny>
+
+=head1 COPYRIGHT
+
+Copyright 2007 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
