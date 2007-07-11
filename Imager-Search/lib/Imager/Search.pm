45fb49479509e0393d9ef5a66da3b6e4469ff811
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2007-07-11 13:18:55 +0000

    Adding the skeleton for Imager-Search

---
+++ Imager-Search/lib/Imager/Search.pm
@@ -0,0 +1,86 @@
+package Imager::Search;
+
+=pod
+
+=head1 NAME
+
+Imager::Search - The author of the module is an idiot
+
+=head1 SYNOPSIS
+
+  The author is an idiot who forgot to write the synopsis
+
+=head1 DESCRIPTION
+
+The author is an idiot who forgot to write the description
+
+=head1 METHODS
+
+=cut
+
+use 5.005;
+use strict;
+
+use vars qw{$VERSION};
+BEGIN {
+	$VERSION = '0.01';
+}
+
+
+
+
+
+#####################################################################
+# Constructor and Accessors
+
+=pod
+
+=head2 new
+
+The author is an idiot that forgot to write the docs for the new method
+
+=cut
+
+sub new {
+	my $class = shift;
+
+	# Create the object
+	my $self = bless { @_ }, $class;
+
+	# Check params
+	die "CODE INCOMPLETE";
+
+	return $self;
+}
+
+
+
+
+
+#####################################################################
+# Main Methods
+
+1;
+
+=pod
+
+=head1 SUPPORT
+
+No support is available for this module
+
+=head1 AUTHOR
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2007 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
---
+++ Imager-Search/t/02_main.t
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+BEGIN {
+	$|  = 1;
+	$^W = 1;
+}
+
+use Test::More tests => 1;
+use Imager::Search;
+
+
+
+
+
+#####################################################################
+# Main Tests
+
+ok( 0, 'The author forgot to write any tests' );
+
