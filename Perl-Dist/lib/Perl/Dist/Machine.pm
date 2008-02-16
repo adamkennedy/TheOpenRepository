781131d129f311dc893bf2bd01e3fc2a5cb85a65
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-02-16 07:38:11 +0000

    Completed Perl::Dist::Machine

--- Perl-Dist/lib/Perl/Dist/Machine.pm
+++ Perl-Dist/lib/Perl/Dist/Machine.pm
@@ -3,8 +3,7 @@ package Perl::Dist::Machine;
 use 5.005;
 use strict;
 use Carp         'croak';
-use Perl::Dist   ();
-use Params::Util qw{ _STRING _IDENTIFIER _ARRAY _DRIVER };
+use Params::Util qw{ _STRING _IDENTIFIER _ARRAY _HASH _DRIVER };
 
 use vars qw{$VERSION};
 BEGIN {
@@ -13,8 +12,8 @@ BEGIN {
 
 use Object::Tiny qw{
 	class
-	common
 	output
+	state
 };
 
 
@@ -32,10 +31,11 @@ sub new {
 		dimensions => [ ],
 		options    => { },
 		state      => undef,
+		eos        => 0, # End of State
 	}, $class;
 
 	# Check params
-	unless ( _DRIVER($self->class) ) {
+	unless ( _DRIVER($self->class, 'Perl::Dist::Inno') ) {
 		croak("Missing or invalid class param");
 	}
 	unless ( _STRING($self->output) ) {
@@ -44,11 +44,18 @@ sub new {
 	unless ( -d $self->output and -w $self->output ) {
 		croak("The output directory does not exist, or is not writable");
 	}
-	unless ( _ARRAY($self->common) ) {
+	if ( _HASH($self->{common}) ) {
+		$self->{common} = [ %{ $self->{common} } ];
+	}
+	unless ( _ARRAY($self->{common}) ) {
 		croak("Did not provide a common param");
 	}
 
 	return $self;
+}
+
+sub common {
+	return @{$_[0]->{common}};
 }
 
 sub dimensions {
@@ -71,7 +78,7 @@ sub add_dimension {
 	if ( $self->{options}->{$name} ) {
 		croak("The dimension '$name' already exists");
 	}
-	push @{ $self->{dimensions}->{$name} }, $name;
+	push @{ $self->{dimensions} }, $name;
 	$self->{options}->{$name} = [ ];
 	return 1;
 }
@@ -96,23 +103,66 @@ sub add_option {
 #####################################################################
 # Iterator Methods
 
+sub all {
+	my $self    = shift;
+	my @objects = ();
+	while ( 1 ) {
+		my $object = $self->next or last;
+		push @objects, $object;
+	}
+	return @objects;
+}
+
 sub next {
 	my $self = shift;
+	if ( $self->{eos} ) {
+		# Already at last state
+		return undef;
+	}
 
 	# Initialize the iterator if needed
-	if ( $self->state ) {
+	my $options = $self->{options};
+	my $state   = $self->state;
+	if ( $state ) {
 		# Move to the next position
-	} else {
-		$self->{state} = [ ];
+		my $found = 0;
 		foreach my $name ( $self->dimensions ) {
-			unless ( @{ $self->options->{$name} } ) {
+			unless ( $state->{$name} == $#{ $options->{$name} } ) {
+				# Normal iteration
+				$state->{$name}++;
+				$found = 1;
+				last;
+			}
+
+			# We've hit the end of a dimension.
+			# Loop the state to the start, so the
+			# next dimension will iterate to the
+			# correct value.
+			$state->{$name} = 0;
+		}
+		unless ( $found ) {
+			$self->{eos} = 1;
+			return undef;
+		}
+	} else {
+		# Initialize to the first position
+		$state = $self->{state} = { };
+		foreach my $name ( $self->dimensions ) {
+			unless ( @{ $options->{$name} } ) {
 				croak("No options for dimension '$name'");
 			}
-			push @{ $self->{state} }, $name;
+			$state->{$name} = 0;
 		}
 	}
 
+	# Create the param-set
+	my @params = $self->common;
+	foreach my $name ( $self->dimensions ) {
+		push @params, @{ $options->{$name}->[ $state->{$name} ] };
+	}
 
+	# Create the object with those params
+	return $self->class->new( @params );
 }
 
 
