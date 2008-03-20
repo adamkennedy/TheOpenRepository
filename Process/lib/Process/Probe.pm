edeaa84803af092bd27e1991e90e3134045e0737
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-03-20 11:48:26 +0000

    Small changes

---
+++ Process/lib/Process/Probe.pm
@@ -0,0 +1,64 @@
+package Process::Probe;
+
+=pod
+
+=head1 NAME
+
+Process::Probe - Process to test if any named classes are installed
+
+=head1 SYNOPSIS
+
+  my $probe = Process::Probe->new( qw{
+      My::Process
+      CPAN::Module::Process
+      Something::Else
+  } );
+  
+  $probe->run;
+  
+  # Lists of classes
+  my @yep   = $probe->available;
+  my @nope  = $probe->unavailable;
+  my @maybe = $probe->unknown;
+  
+  # Test for single class with any of the above
+  if ( $probe->available('My::Process') ) {
+      print "My::Process is available\n";
+  }
+
+=head1 DESCRIPTION
+
+B<Process::Probe> is a simple and standardised class available that is
+available with the core L<Process> distribution. It is used to probe
+a host to determine whether or not the remote host has certain process
+classes installed.
+
+By default, the object will search through the system's include path to
+find the .pm files that match the particular classes.
+
+Typical examples of using the default functionality could include
+executing a B<Process::Probe> object via a SSH login on a remote host
+to determine which of a set of desired classes exist on the remote host.
+
+The probe will ONLY check for the existance of classes that are in the
+unknown state at the time the C<run> method is called.
+
+In scenarios where the requestor does not have direct execution rights
+on the remote host, and the request is being marshalled via a server
+process, this allows security code on the server to preset forbidden
+classes to no before the probe is run, or to otherwise manipulate the
+"answer" to the "question" that B<Process::Probe> represents. 
+
+No functionality is provided to query ALL the C<Process>-compatible
+classes on a remote host. This is intentional. It prevents very
+disk-intensive scans, protects remote host against hostile requests,
+and prevents the use of these objects en-mass as a denial of service.
+
+=head1 METHODS
+
+=cut
+
+use strict;
+use base qw{
+	
+};
---
+++ Process/lib/Process/YAML.pm
@@ -0,0 +1,175 @@
+package Process::Storable;
+
+# Storable-based implementation of Process::Serializable
+
+use 5.005;
+use strict;
+use base 'Process::Serializable';
+use YAML::Tiny   ();
+use IO::Handle   ();
+use IO::String   ();
+use Scalar::Util ();
+use Params::Util '_INSTANCE';
+
+use vars qw{$VERSION};
+BEGIN {
+	$VERSION = '0.22';
+
+	# Hack IO::String to be a real IO::Handle
+	unless ( IO::String->isa('IO::Handle') ) {
+		@IO::String::ISA = qw{IO::Handle IO::Seekable};
+	}
+}
+
+sub serialize {
+	my $self = shift;
+
+	# Serialize to a named file (locking it)
+	if ( defined $_[0] and ! ref $_[0] and length $_[0] ) {
+		return Storable::lock_nstore($self, shift);
+	}
+
+	# Serialize to a string (via a handle)
+	if ( Params::Util::_SCALAR0($_[0]) ) {
+		my $string = shift;
+		$$string   = 'pst0' . Storable::nfreeze($self);
+		return 1;
+	}
+
+	# Serialize to a generic handle
+	if ( defined fileno($_[0]) ) {
+		local $/ = undef;
+		return Storable::nstore_fd($self, shift);
+	}
+
+	# Serialize to an IO::Handle object
+	if ( Params::Util::_INSTANCE($_[0], 'IO::Handle') ) {
+		my $string   = Storable::nfreeze($self);
+		my $iohandle = shift;
+		$iohandle->print( 'pst0' )  or return undef;
+		$iohandle->print( $string ) or return undef;
+		return 1;
+	}
+
+	# We don't support anything else
+	undef;
+}
+
+sub deserialize {
+	my $class = shift;
+	my $self  = $class->_deserialize(@_);
+
+	# Integrity check
+	_INSTANCE($self, $class) or return undef;
+
+	$self;
+}
+
+sub _deserialize {
+	my $class = shift;
+
+	# Serialize from a named file (locking it)
+	if ( defined $_[0] and ! ref $_[0] and length $_[0] ) {
+		return Storable::lock_retrieve(shift);
+	}
+
+	# Serialize from a string (via a handle)
+	if ( Params::Util::_SCALAR0($_[0]) ) {
+		my $string = shift;
+
+		# Remove the magic header if it exists
+		if ( substr($$string, 0, 4) eq 'pst0' ) {
+			substr($$string, 0, 4, '');
+		}
+
+		return Storable::thaw($$string);
+	}
+
+	# Serialize from a generic handle
+	if ( defined fileno($_[0]) ) {
+		return Storable::retrieve_fd(shift);
+	}
+
+	# Serialize from an IO::Handle object
+	if ( Params::Util::_INSTANCE($_[0], 'IO::Handle') ) {
+		local $/   = undef;
+		my $string = $_[0]->getline;
+
+		# Remove the magic header if it exists
+		if ( substr($string, 0, 4) eq 'pst0' ) {
+			substr($string, 0, 4, '');
+		}
+
+		return Storable::thaw($string);
+	}
+
+	# We don't support anything else
+	undef;
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Process::Storable - Storable-based implementation of Process::Serializable
+
+=head1 SYNOPSIS
+
+Create your package...
+
+  package MyStorable;
+  
+  use base qw{Process::Storable Process};
+  
+  sub prepare { ... }
+  sub run     { ... }
+  
+  1;
+
+And then use it...
+
+  use MyStorable;
+  
+  my $process = MyStorable->new( ... );
+  $process->serialize( 'filename.dat' );
+  
+  # and so on...
+
+=head1 DESCRIPTION
+
+C<Process::Storable> provides an implementation of the
+L<Process::Serializable> role using the standard L<Storable> module
+from the Perl core. It is not itself a subclass of L<Process> so you
+will need to inherit from both L<Process> (or a subclass) and
+L<Process::Storable> if you want to make use of it.
+
+Objects that inherit from C<Process::Storable> must follow the C<new>,
+C<prepare>, C<run> rules of L<Process::Serializable>.
+
+=head1 SUPPORT
+
+Bugs should be reported via the CPAN bug tracker at
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Process>
+
+For other issues, contact the author.
+
+=head1 AUTHOR
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>, L<http://ali.as/>
+
+=head1 COPYRIGHT
+
+Copyright 2006 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
