6ff17d1bbe8fefde337a4a5f3b8f9873715f2270
jkegl@cpan.org <jkegl@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-03-30 01:35:02 +0000

    Upped version to 0.205.8. 0.205.8 will, I hope, be the 0.206 release candidate. Stored developer's version warnings away, for future use.

---
+++ Parse-Marpa/etc/developer/developer_Marpa.pm
@@ -0,0 +1,13 @@
+=head1 BEWARE: THIS RELEASE IS FOR DEVELOPERS ONLY
+
+This is a developer's release, not for use by non-developers.
+I use these releases to avail myself of the cpantesters results,
+and to test the release process itself.
+
+Of course, it's open source, and you're entitled to appoint yourself
+a developer if you insist on it.  But that will usually not be a reasonable
+thing to do.
+
+Right now the documentation in this release is half-converted from one interface
+to another, so it's incomplete and inconsistent.
+
