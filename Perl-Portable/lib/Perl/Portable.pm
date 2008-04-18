7a9a1b13aeda518b2e45e6b47fbe0373805d97f2
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-04-18 17:32:08 +0000

    Allow Perl::Portable to build on unix

--- Perl-Portable/lib/Perl/Portable.pm
+++ Perl-Portable/lib/Perl/Portable.pm
@@ -4,7 +4,7 @@ package Perl::Portable;
 
 =head1 NAME
 
-Perl::Portable - Support code for "Perl on a Stick" distribtions
+Perl::Portable - Support code for "Perl on a Stick"
 
 =head1 DESCRIPTION
 
@@ -41,7 +41,7 @@ use strict;
 use Carp             'croak';
 use File::Spec       ();
 use List::Util       ();
-use Params::Util     qw{ _STRING _HASH _ARRAY };
+use Params::Util     qw{ _STRING _STRING0 _HASH _ARRAY };
 use YAML::Tiny       ();
 
 use vars qw{$VERSION $FAKE_PERL};
@@ -90,7 +90,7 @@ sub new {
 	my $self  = $class->SUPER::new( @_ );
 
 	# Param checking
-	unless ( _STRING($self->dist_volume) ) {
+	unless ( _STRING0($self->dist_volume) ) {
 		croak('Missing or invalid dist_volume param');
 	}
 	unless ( _STRING($self->dist_dirs) ) {
