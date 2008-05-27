97e60f88629a24991f562e8959e88745ff7880ef
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-05-27 03:53:23 +0000

    Preparing for release

--- ORLite/t/lib/Test.pm
+++ ORLite/t/lib/Test.pm
@@ -8,7 +8,7 @@ use File::Spec::Functions ':ALL';
 
 use vars qw{$VERSION @ISA @EXPORT};
 BEGIN {
-	$VERSION = '0.03';
+	$VERSION = '0.04';
 	@ISA     = qw{ Exporter };
 	@EXPORT  = qw{ test_db connect_ok create_ok };
 }
