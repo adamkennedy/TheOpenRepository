100c0987c659b4c7a08a9f82b8cbafee384757f0
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-06-02 02:43:32 +0000

    Prepping for incremental release

--- File-BLOB/lib/File/BLOB.pm
+++ File-BLOB/lib/File/BLOB.pm
@@ -88,7 +88,7 @@ eval "use prefork 'File::Type';";
 
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '0.05';
+	$VERSION = '1.06';
 }
 
 
@@ -606,11 +606,11 @@ For other issues, contact the author.
 
 =head1 AUTHOR
 
-Adam Kennedy E<lt>adamk@cpan.orgE<gt>, L<http://ali.as/>
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
 
 =head1 COPYRIGHT
 
-Copyright 2005, 2006 Adam Kennedy.
+Copyright 2005 - 2008 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
