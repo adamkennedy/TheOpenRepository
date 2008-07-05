a8404003088b8cccd6c8d98346ff8a2be9f7aca9
jbisbee@cpan.org <jbisbee@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-07-05 09:54:25 +0000

    * added an _init to beginning of get/getandstore/mirror reset state   between uses * moved the pod to the __END__ block, feel free to move it back if   you feel strongly about it :) * moved name out of AUTHORS and into ACKNOWLEDGEMENTS. * fixed Marlon's cpan mail address (missing the 'e' in bailey) * also take a look at get.  I'm not sure if you want to return an   array of content or just HTTP::Response objects.  From the   SYNOPSIS it looks you'd want content

--- HTTP-Client-Parallel/lib/HTTP/Client/Parallel.pm
+++ HTTP-Client-Parallel/lib/HTTP/Client/Parallel.pm
@@ -1,80 +1,11 @@
 package HTTP::Client::Parallel;
-
-=pod
-
-=head1 NAME
-
-HTTP::Client::Parallel - A HTTP client that fetchs all URIs in parallel
-
-=head1 SYNOPSIS
-
-  # Create the parallising client
-  my $client = HTTP::Client::Parallel->new;
-  
-  # Simple fetching
-  my $pages = $client->get(
-    'http://www.google.com/',
-    'http://www.yapc.org/',
-    'http://www.yahoo.com/',
-  );
-  
-  # Mirroring to disk
-  my $responses = $client->mirror(
-    'http://www.google.com/' => 'mirrors/google.html',
-    'http://www.yapc.org/'   => 'mirrors/yapc.html',
-    'http://www.yahoo.com/'  => 'mirrors/yahoo.html',
-  );
-
-=head1 DESCRIPTION
-
-Fetching a URI is a very common network-bound task in many types of
-programming. Fetching more than one URI is also very common, but unless
-the fetches are capable of entirely saturating a connection, typically
-time is wasted because there is often no logical reason why multiple
-requests cannot be made in parallel.
-
-Executing IO-bound and network-bound tasks is extremely easy in any
-event-based programming model such as L<POE>, but these event-based
-systems normally require complete control of the application and
-that the program be written in a very different way.
-
-Thus, the biggest problem preventing running HTTP requests in
-parallel is not that it isn't possible, but that mixing procedural
-and event programming is difficult.
-
-The few existing mechanisms generally rely on forking or other
-platform-specific methods.
-
-B<HTTP::Client::Parallel> is designed to bridge the gap between
-typical cross-platform procedural code and typical cross-platform
-event-based code.
-
-It allows you to set up a series of HTTP tasks (fetching to memory,
-fetching to disk, and mirroring to disk) and then issue a single
-method call which will block and execute all of them in parallel.
-
-Behind the scenes HTTP::Client::Parallel will B<temporarily> hand
-over control of the process to L<POE> to execute the HTTP tasks.
-
-Once all of the HTTP tasks are completed (using the standard
-L<POE::Component::HTTP::Client> module, the POE kernel will shut
-down and hand control of the application back to the normal
-procedural code, and thus back to your code.
-
-As a result, a developer with no knowledge of L<POE> or event-based
-programming can still take advantage of the capabilities of POE and
-gain major speed increases in HTTP-based programs with relatively
-little work.
-
-=cut
-
 use 5.006;
 use strict;
 use warnings;
-use Exporter     ();
-use IO::File     ();
+use Exporter ();
+use IO::File ();
 use Params::Util '_INSTANCE';
-use HTTP::Date   ();
+use HTTP::Date ();
 use HTTP::Request;
 use POE;
 use POE::Session;
@@ -86,9 +17,9 @@ use constant DEFAULT_TIMEOUT        => 60;
 
 use vars qw{$VERSION @ISA @EXPORT_OK};
 BEGIN {
-	$VERSION   = '0.01';
-	@ISA       = 'Exporter';
-	@EXPORT_OK = qw{ mirror getstore get };
+    $VERSION   = '0.01';
+    @ISA       = 'Exporter';
+    @EXPORT_OK = qw{ mirror getstore get };
 }
 
 sub new {
@@ -108,29 +39,55 @@ sub urls {
     return wantarray ? @{ $_[0]->{urls} } : $_[0]->{urls};
 }
 
-sub _set_urls {
-    my ( $self, @urls ) = @_;
-    $self->{urls} = \@urls;
-    $self->{url_count} = @urls;
-    return;
-}
-
 sub get {
     my $self = _INSTANCE($_[0], HCP) ? shift : HCP->new;
+    $self->_init;
     $self->_set_urls(@_);
     $self->poe_loop;
     my @responses = map { $self->{responses}{$_} } $self->urls;   
     return wantarray ? @responses : \@responses
+
+    # XXX from the pod, this may be the desired behavior
+    # my @content = map { $self->{responses}{$_}->content } $self->urls;
+    # return wantarray ? @content : \@content
 }
 
 sub getstore {
     my $self = _INSTANCE($_[0], HCP) ? shift : HCP->new;
     my %url_file_map = @_;
+    $self->_init;
     $self->_set_urls( keys %url_file_map );
     $self->{local_files} = \%url_file_map;
     $self->poe_loop;
     my @responses = values %{ $self->{responses} };
     return wantarray ? @responses : \@responses;
+}
+
+sub mirror {
+    my $self = _INSTANCE($_[0], HCP) ? shift : HCP->new;
+    my %url_file_map = @_;
+    $self->_init;
+    $self->_set_urls( keys %url_file_map );
+    $self->{local_files} = \%url_file_map;
+    $self->_build_modified_since( \%url_file_map );
+    $self->poe_loop;
+    my @responses = values %{ $self->{responses} };
+    return wantarray ? @responses : \@responses
+}
+
+# cleanup between uses just in case
+sub _init {
+    my ($self) = @_;
+    $self->{count} = 0;
+    delete @{$self}{qw(requests responses urls url_count local_files modified_since)};
+    return;
+}
+
+sub _set_urls {
+    my ( $self, @urls ) = @_;
+    $self->{urls} = \@urls;
+    $self->{url_count} = @urls;
+    return;
 }
 
 sub _build_modified_since {
@@ -144,17 +101,6 @@ sub _build_modified_since {
     }
 }
 
-sub mirror {
-    my $self = _INSTANCE($_[0], HCP) ? shift : HCP->new;
-    my %url_file_map = @_;
-    $self->_set_urls( keys %url_file_map );
-    $self->{local_files} = \%url_file_map;
-    $self->_build_modified_since( \%url_file_map );
-    $self->poe_loop;
-    my @responses = values %{ $self->{responses} };
-    return wantarray ? @responses : \@responses
-}
-
 sub _store_local_file {
     my ( $self, $response, $file ) = @_;
 
@@ -164,6 +110,7 @@ sub _store_local_file {
     print $tmp_fh $response->content;
     close $tmp_fh;
 
+    # the following is taken from LWP::UserAgent->mirror
     my $file_length = ( stat($tmpfile) )[7];
     my ($content_length) = $response->header('Content-length');
 
@@ -193,8 +140,6 @@ sub _store_local_file {
 # POE 
 sub poe_loop {
     my $self = shift;
-
-    $self->{count} = 0;
 
     POE::Component::Client::HTTP->spawn(
         Alias           => $self->{http_alias} || 'ua',
@@ -267,7 +212,8 @@ sub _response {
         warn '[' . $request->uri . "] Not Modified\n" if $self->{debug};
     }
     else {
-        warn '[' . $request->uri . "] HTTP Response Code: " . $response->code . "\n" if $self->{debug};
+        warn '[' . $request->uri . "] HTTP Response Code: " . $response->code . "\n"
+            if $self->{debug};
     }
 
     $kernel->call( ua => 'shutdown' ) if $self->{url_count} == $self->{count};
@@ -287,7 +233,71 @@ sub _stop {
 
 1;
 
-=pod
+__END__
+
+=head1 NAME
+
+HTTP::Client::Parallel - A HTTP client that fetchs all URIs in parallel
+
+=head1 SYNOPSIS
+
+  # Create the parallising client
+  my $client = HTTP::Client::Parallel->new;
+  
+  # Simple fetching
+  my $pages = $client->get(
+    'http://www.google.com/',
+    'http://www.yapc.org/',
+    'http://www.yahoo.com/',
+  );
+  
+  # Mirroring to disk
+  my $responses = $client->mirror(
+    'http://www.google.com/' => 'mirrors/google.html',
+    'http://www.yapc.org/'   => 'mirrors/yapc.html',
+    'http://www.yahoo.com/'  => 'mirrors/yahoo.html',
+  );
+
+=head1 DESCRIPTION
+
+Fetching a URI is a very common network-bound task in many types of
+programming. Fetching more than one URI is also very common, but unless
+the fetches are capable of entirely saturating a connection, typically
+time is wasted because there is often no logical reason why multiple
+requests cannot be made in parallel.
+
+Executing IO-bound and network-bound tasks is extremely easy in any
+event-based programming model such as L<POE>, but these event-based
+systems normally require complete control of the application and
+that the program be written in a very different way.
+
+Thus, the biggest problem preventing running HTTP requests in
+parallel is not that it isn't possible, but that mixing procedural
+and event programming is difficult.
+
+The few existing mechanisms generally rely on forking or other
+platform-specific methods.
+
+B<HTTP::Client::Parallel> is designed to bridge the gap between
+typical cross-platform procedural code and typical cross-platform
+event-based code.
+
+It allows you to set up a series of HTTP tasks (fetching to memory,
+fetching to disk, and mirroring to disk) and then issue a single
+method call which will block and execute all of them in parallel.
+
+Behind the scenes HTTP::Client::Parallel will B<temporarily> hand
+over control of the process to L<POE> to execute the HTTP tasks.
+
+Once all of the HTTP tasks are completed (using the standard
+L<POE::Component::HTTP::Client> module, the POE kernel will shut
+down and hand control of the application back to the normal
+procedural code, and thus back to your code.
+
+As a result, a developer with no knowledge of L<POE> or event-based
+programming can still take advantage of the capabilities of POE and
+gain major speed increases in HTTP-based programs with relatively
+little work.
 
 =head1 SUPPORT
 
@@ -299,11 +309,14 @@ For other issues, contact the author.
 
 =head1 AUTHORS
 
-Jeff Bisbee E<lt>jbisbee@cpan.orgE<gt>
+Marlon Bailey E<lt>mbailey@cpan.orgE<gt>
 
 Adam Kennedy E<lt>adamk@cpan.orgE<gt>
 
-Marlon Bailey E<lt>mbaily@cpan.orgE<gt>
+=head1 ACKNOWLEDGEMENTS
+
+Jeff Bisbee, for the bit about passing the request as an argument to the
+response and for also working out the "If-Modified" details in mirror context.
 
 =head1 SEE ALSO
 
@@ -311,7 +324,7 @@ L<LWP::Simple>, L<POE>
 
 =head1 COPYRIGHT
 
-Copyright 2008 Jeff Bisbee, Adam Kennedy and Marlon Bailey.
+Copyright 2008 Marlon Bailey and Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
