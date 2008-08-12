74238440e1c24cd0dbbe56effdd28359a0c6706b
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-08-12 14:14:14 +0000

    Adding the beginnings of a simple Python style Perl stell

---
+++ Perl-Shell/lib/Perl/Shell.pm
@@ -0,0 +1,101 @@
+package Perl::Shell;
+
+use 5.006;
+use strict;
+use Carp         ();
+use Params::Util '_INSTANCE';
+use PPI          ();
+
+use vars qw{$VERSION};
+BEGIN {
+	$VERSION = '0.01';
+}
+
+
+
+
+
+#####################################################################
+# Shell Functions
+
+sub shell {
+	my @buffer = ();
+	while ( 1 ) {    
+		# Read in a line
+		my $line = term_readline(@buffer ? '...' : '>>>');
+		unless ( defined $line ) {
+			die "Failed to readline\n";
+		}
+		push @buffer, $line;
+
+		# Continue if the statement is not complete
+		next unless complete( @buffer );
+
+		# Execute the code
+		my $code = join '', @buffer;
+		my @rv   = eval $code;
+		print "ERROR: $@" if $@;
+		print "\n";
+
+		
+	}
+}
+
+my $term;
+sub term_readline {
+	my $prompt = shift;
+	if ( -t STDIN ) {
+		unless ( $term ) {
+			require Term::Readline;
+			$term = Term::Readline->new('Perl-Shell');
+		}
+		return $term->readline($prompt);
+	} else {
+		print $prompt;
+		my $line = <>; 
+		chomp if defined $line;
+		return $line;
+	}
+}
+
+
+
+
+
+#####################################################################
+# Support Functions
+
+# To be "complete" a fragment of Perl must have no open structures
+# and terminate with a clear statement end.
+sub complete {
+	my $string = join '', @_;
+
+	# As a quick and dirty way to check for a clear statement
+	# end, we append a semi-colon to the string. If this is
+	# subsequently parsed as a null statement, we know the
+	# string is a complete document.
+	# The newline is added to get us out of comment blocks
+	# and similar line-specific things.
+	$string .= "\n;";
+
+	# Parse the string into a document
+	my $document = PPI::Document->new( \$string );
+	unless ( $document ) {
+		die "PPI failed to parse document";
+	}
+
+	# The document must end in a null statement
+	unless ( _INSTANCE($document->schild(-1), 'PPI::Statement::Null') ) {
+		return '';
+	}
+
+	# The document must not contain any open braces
+	$document->find_any( sub {
+		$_[1]->isa('PPI::Structure') and ! $_[1]->finish
+	} ) and return '';
+
+	# The document is complete
+	return 1;
+}
+
+1;
