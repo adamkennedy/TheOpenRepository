62f9450f626ecb8fa2a52568a2d6234ed4e9647c
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-08-16 14:24:47 +0000

    Updating copyright

--- Module-Inspector/lib/Module/Inspector.pm
+++ Module-Inspector/lib/Module/Inspector.pm
@@ -445,7 +445,7 @@ L<Class::Inspector>
 
 =head1 COPYRIGHT
 
-Copyright 2006 Adam Kennedy.
+Copyright 2006 - 2008 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
