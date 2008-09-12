994611fc5bf1f241de9348b71a0e28236347a7cb
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-09-12 02:27:18 +0000

    Preparing for 0.03 release

--- Template-Provider-Preload/lib/Template/Provider/Preload.pm
+++ Template-Provider-Preload/lib/Template/Provider/Preload.pm
@@ -8,61 +8,51 @@ Template::Provider::Preload - Preload templates to save memory when forking
 
 =head1 SYNOPSIS
 
-  # Create the provider
-  my $provider = Template::Provider::Preload->new(
-      STAT_TTL     => 31536000, # 1 year (possibly too short)
-      INCLUDE_PATH => 'my/templates',
-      COMPILE_DIR  => 'my/cache',
-  );
-  
-  # Precompile all of the templates into the cache
-  $provider->precompile('*.*');
-  
-  # Preload the .html files into memory now
-  $provider->prefetch('*.html');
-  
-  # Create the Template with the (cache-bloated) provider
   my $template = Template->new(
-      LOAD_TEMPLATES => [ $provider ],
+      LOAD_TEMPLATES => [
+          Template::Provider::Preload->new(
+              PRECACHE     => 1,
+              PREFETCH     => '*.tt',
+              INCLUDE_PATH => 'my/templates',
+              COMPILE_DIR  => 'my/cache',
+          ); 
+      ],
   );
 
 =head1 DESCRIPTION
 
-B<THIS MODULE IS CONSIDERED EXPERIMENTAL>
-
-B<FUTURE CHANGES MAY RESULT IN CRITICAL API CHANGES>
-
-B<YOU HAVE BEEN WARNED!>
-
 One of the nicer things that the Template Toolkit modules do in the default
-L<Template::Provider> is provide several different caching features.
+L<Template::Provider> is provide two powerful caching features.
 
-The first mechanism is to cache the result of the slow and expensive
-compilation phase, storing the Perl version of the template in a specific
-cache directory. This mechanism is disabled by default, and enabled with
-the COMPILE_DIR parameter.
+The first is to cache the result of the slow and expensive compilation
+phase, storing the Perl version of the template in a specific cache
+directory. This mechanism is disabled by default, and enabled with
+the C<COMPILE_DIR> parameter.
 
 The second is that the compiled templates will be cached in memory the
-first time they are used, based on template path. By default, this is
-enabled and permitted to grow to infinite size. It can be limited or
-disabled via the CACHE_SIZE param.
+first time they are used, based on template path. This feature is enabled
+by default and permitted to grow to infinite size. It can be limited or
+disabled via the C<CACHE_SIZE> param.
 
 The default cache strategy works just fine in a single-process application,
-and in fact in many cases are a reasonably optimum caching strategy.
+and in fact in many such cases is the optimum caching strategy.
 
 However, the default cache strategy can perform horribly in several
 situations relating to large-scale and high-demand applications.
 
-B<Template::Provider::Preload> can be used to set caching strategies that
-are more appropriate for various types of heavily forking applications,
-such as large clustered high-traffic mod_perl systems.
+B<Template::Provider::Preload> can be used to create caching strategies
+that are more appropriate for heavily forking applications such as
+clustered high-traffic mod_perl systems.
 
-While B<Template::Provider::Preload> is useful in other high-forking
-scenarios, we use the (dominant) example of a forking Apache application
-in all of the following examples. You should be able to exchange all uses
-of terms like "Apache child" with your equivalent interchangably.
+=head1 USE CASES
 
-=head2 High-Security Use Case
+While B<Template::Provider::Preload> is useful in many high-forking
+scenarios, we will use the (dominant) case of a forking Apache
+application in all of the following examples. You should be able to
+exchange all uses of terms like "Apache child" with your equivalent
+interchangably.
+
+=head2 High-Security Server
 
 In some very high security environments, the web user will not have the
 right to create any files whatsoever, including temporary files.
@@ -79,43 +69,81 @@ Most of the following cases also assume a well-control static production
 environment, where the template content will not change (and a web server
 restart is done each time a new version of the application is deployed).
 
-=head2 Large Template Use Case
+=head2 Large Templates, Many Templates
 
 Under the default cache strategy (with a compilation directory enabled)
 the first Apache child that uses each template will compile and cache
 the template. Each Apache child that uses the templates will then need
 to separately load the compiled templates into memory.
 
-With web servers having 20 or 50 or 100 child processes each, templates
-that expand into 10 meg of memory overhead for a single process (which can
-be quite possible with large templates) can easily expand into a gigabyte
-of memory that contributes nothing other than to eat into your object
-or disk caches.
+With web servers often having 20 or 50 or 100 child processes each,
+templates that expand into 10 meg of memory for a single process
+(which can be quite possible with a reasonable number of templates) can
+easily expand into a gigabyte of memory that contributes nothing other
+than to eat into your more useful object or disk caches.
 
-With large numbers of large templates on high-core serves with many many
-child processes, you can even put yourself in the situation of needing
-to requisition additional web front ends due to memory contraints,
+With large numbers of large templates on multi-core servers with many
+many child processes, you can even put yourself in the situation of
+needing to requisition additional web servers due to memory contraints,
 rather than CPU constraints.
 
-Memory spent on loading templates once means enormous memory savings
-across the collective children.
+Memory saved by loading a template once instead of 100 times can be
+retasked to enable higher throughput (by providing more children) or
+reduced latency (by boosting various caches).
 
-=head2 Networked-Storage Cluster Use Case
+=head2 Networked Templates
 
 In cluster environments where all front-end servers will use a common
-back-end network-attached storage server, reducing the number of disk
-interations (both reads and stats) is important to reduce (or if possible
-eliminate entirely) traffic to the critical storage server.
+back-end Network-Attached Storage to store the website, reducing the number,
+frequency and size of disk interations (both reads and stats) is an
+important element in reducing or eliminating disk contention and network
+load on what is often a critical shared resource.
 
-This serves a triple purpose of reducing the size and cost of network
-and storage equipment, allowing additional front-end growth without
-requiring upgrades to network and storage, and eliminating potentially
-high-latency network requests.
+Reducing the number of template-related requests serves a triple purpose of
+reducing the size, speed, capacity and cost of required networking
+equipment, allowing additional front-end server growth with lower change of
+requiring (highly disruptive) upgrades to central network or storage kit,
+and eliminating high-latency network IO requests from the web pipeline.
 
-By compiling and loading all templates in advance, in combination with
-a very high STAT_TTL setting to disable update checking, you can create
-an environment in which the individual Apache children will not need
-issue network filesystem requests at all for their templates.
+By compiling and loading all of the common templates in advance into a
+seperate pre-fork memory cache (hereafter "precache") you can create an
+environment in which the individual Apache children will not need issue
+network filesystem requests except in the case of rare and unusual website
+requests that load rarely used templates.
+
+This can be taken to the extreme by loading every possible template into
+the precache, which will eliminate template entirely and enable more-unusual
+tricks like allowing the web server to disconnect entirely from the
+source of the templates.
+
+=head1 FEATURES
+
+B<Template::Provider::Preload> provides two additional caching features
+that can be used on their own to implement the most common caching strategy,
+or in combination with the default cache settings to allow for more varied
+and subtle caching strategies.
+
+=head2 Template Search and Compilation
+
+B<Template::Provider::Preload> provides the C<prefetch> method that can be
+used to search the template include path and compile/load templates in bulk.
+
+The set of templates to load can be defined very flexibly, allowing you to
+preload templates at various levels. Typical examples might be loading the
+entire template library, loading only the common include templates (page
+headers and footers etc), or loading all templates accessible by the public
+while not loading rarely used staff or administration templates.
+
+=head2 Secondary Pre-Fork Cache
+
+B<Template::Provider::Preload> adds an optional third cache that sits
+between the disk compilation cache and the run-time memory cache.
+
+The precache is designed to cache templates in memory separately from
+the run-time memory cache. Holding pre-fork templates in a separate
+cache allows the normal cache to behave appropriately at run-time for
+templates that aren't in the precache, without being distracted by the
+precache entries and without having to stat the precache templates
 
 =head1 METHODS
 
@@ -135,6 +163,75 @@ use Class::Adapter::Builder
 use vars qw{$VERSION};
 BEGIN {
 	$VERSION = '0.02';
+}
+
+
+
+
+
+#####################################################################
+# Constructor
+
+=pod
+
+=head2 new
+
+  my $provider = Template::Provider::Preload->new(
+      PRECACHE     => 1,
+      PREFETCH     => '*.tt',
+      INCLUDE_PATH => 'my/templates',
+      COMPILE_DIR  => 'my/cache',
+  );
+
+The C<new> constructor accepts all the same parameters as the underlying
+L<Template::Provider> class, with two additions.
+
+The C<PRECACHE> param indicates that the secondary prefetch cache should
+be created and used for this provider. The precatch is not
+size-constrained by the C<CACHE_SIZE> param, that option only affects
+the default run-time cache.
+
+The C<PREFETCH> param indicates that an immediate C<prefetch> request
+should be made at constructor time to pre-populate the cache (the
+precache if it exists, or the run-time cache otherwise) with a specified
+set of templates.
+
+The value of C<PREFETCH> can be either a single string or a
+L<File::Find::Rule> object, which will be passed to prefetch.
+Alternatively, if the C<PREFETCH> param is an C<ARRAY> reference,
+the reference will be flattened to a list, allowing you to provide more
+than one file type string to C<prefetch>.
+
+Returns a L<Template::Provider::Preload> object, or throws an excpetion
+(dies) on error.
+
+=cut
+
+sub new {
+	my $class    = shift;
+	my %param    = @_;
+	my $precache = delete $param{PRECACHE};
+	my $prefetch = delete $param{PREFETCH};
+
+	# Create the provider as normal
+	my $self = $class->SUPER::new(
+		Template::Provider->new(%param),
+	);
+
+	# Create the precache if needed
+	if ( $precache ) {
+		$self->{PRECACHE} = {};
+	}
+
+	# Prefetch immediately if needed
+	if ( defined $prefetch ) {
+		my @args = Params::Util::_ARRAYLIKE($prefetch)
+			? ( @$prefetch )
+			: (  $prefetch );
+		$self->prefetch( @args );
+	}
+
+	return $self;
 }
 
 
@@ -166,16 +263,13 @@ files should be immediately compiled (with the compiled templates
 cached if possible) and then the compiled templates are loaded into
 memory.
 
-When used in combination with a very long C<STAT_TTL> parameter (longer than
-the longest possible time between Apache restarts) the C<prefetch> method
-creates a caching strategy where the templates will never be looked at once
-the call to C<prefetch> has completed.
+When used in combination with C<PRECACHE> the C<prefetch> method creates
+a caching strategy where the template files will never be looked at once
+the call to C<prefetch> has completed. Both positive (template found) and
+negative (not found or error) results will be cached.
 
-The compilation will be done via the public but undocumented
-L<Template::Provider> method C<load>, so the compilation will be done
-via the normal caching mechanism. If existing compiled versions exist and
-there is no newer template file, then the compiled version will not be
-rebuilt.
+When filling the precache, the use of the internal cache will be
+temporarily disabled to avoid polluting the run-time cache state.
 
 Selection of the files to compile is done via a L<File::Find::Rule> search
 across all C<INCLUDE_PATH> directories. If the same file exists within more
@@ -198,11 +292,29 @@ Returns true on success, or throws an exception (dies) on error.
 =cut
 
 sub prefetch {
-	my $self   = shift;
-	my $object = $self->_OBJECT_;
-	my @paths  = $self->_find(@_);
-	foreach my $path ( @paths ) {
-		$object->fetch($path);
+	my $self     = shift;
+	my $object   = $self->_OBJECT_;
+	my @names    = $self->_find(@_);
+	my $precache = $self->{PRECACHE};
+	if ( $precache ) {
+		# Disable the internal cache while prefetching to prevent
+		# prefetched documents from polluting the cache.
+		my $size = $object->{SIZE};
+		$object->{SIZE} = 0;
+
+		# Fetch and add to the precache if not in it already
+		foreach my $name ( @names ) {
+			next if $precache->{$name};
+			$precache->{$name} = [ $object->fetch($name) ];
+		}
+
+		# Enable the internal cache now that we're done
+		$object->{SIZE} = $size;
+	} else {
+		# Just pull them to get them into the child's cache
+		foreach my $name ( @names ) {
+			$object->fetch($name);
+		}
 	}
 	return 1;
 }
@@ -230,14 +342,39 @@ sub _filter {
 	Carp::croak("Invalid filter param");
 }
 
+
+
+
+
+#####################################################################
+# Template::Provider Methods
+
+sub fetch {
+	my $self = shift;
+	my $name = shift;
+
+	# If caching and we get a name in the cache, return it
+	if ( $self->{PRECACHE} and not ref $name ) {
+		my $cached = $self->{PRECACHE}->{$name};
+		return @$cached if $cached;
+	}
+
+	# Otherwise, hand off to the child
+	return $self->_OBJECT_->fetch( $name, @_ );
+}
+
 1;
 
 =pod
 
-=head1 TO DO
+=head1 CAVEATS
 
-Have the C<prefetch> method prime the cache in a manner that does not
-require the use of the C<STAT_TTL> param at all.
+The B<Template::Provider::Preload> precaching logic assumes a stable
+production environment in which the template files will not be changed.
+
+It is assumed that if a production release is made, then there will be
+a server or application restart that allows the caches to be refilled
+and then the children reforked.
 
 =head1 SUPPORT
 
--- Template-Provider-Preload/t/03_precache.t
+++ Template-Provider-Preload/t/03_precache.t
@@ -5,10 +5,12 @@ BEGIN {
 	$^W = 1;
 }
 
-use Test::More tests => 6;
+use Test::More tests => 13;
+use Test::SubCalls;
 use File::Spec::Functions       ':ALL';
 use File::Temp                  ();
 use Template                    ();
+use Template::Provider          ();
 use Template::Provider::Preload ();
 
 my $INCLUDE_PATH = catdir( 't', 'template' );
@@ -18,18 +20,41 @@ ok( -d $COMPILE_DIR,  'Found compile directory'  );
 
 # Create the preloader
 my $provider = Template::Provider::Preload->new(
-	DEBUG        => 1,
-	STAT_TTL     => 1,
+	CACHE_SIZE   => 10,
+	PRECACHE     => 1,
         INCLUDE_PATH => $INCLUDE_PATH,
         # COMPILE_DIR  => $COMPILE_DIR,
 );
 isa_ok( $provider, 'Template::Provider' );
+isa_ok( $provider->_OBJECT_, 'Template::Provider' );
+is_deeply(
+	[
+		$provider->_OBJECT_->{SLOTS},
+		$provider->_OBJECT_->{LOOKUP},
+		$provider->_OBJECT_->{SIZE},
+	],
+	[ 0, {}, 10 ],
+	'Externals look as expected',
+);
 
 # Can we get the transformed paths
 is_deeply( $provider->paths, [ $INCLUDE_PATH ], '->paths ok' );
 
 # Fetch a compiled template directly
-$provider->prefetch;
+sub_track( 'Template::Provider::fetch' );
+ok( $provider->prefetch, '->prefetch returns true' );
+sub_calls( 'Template::Provider::fetch', 6, 'Initial fetches called' );
+
+# Internals should remain unchanged
+is_deeply(
+	[
+		$provider->_OBJECT_->{SLOTS},
+		$provider->_OBJECT_->{LOOKUP},
+		$provider->_OBJECT_->{SIZE},
+	],
+	[ 0, {}, 10 ],
+	'Externals look as expected',
+);
 
 # Create a Template processor
 my $template = Template->new(
@@ -40,10 +65,21 @@ isa_ok( $template, 'Template' );
 
 # Do a template run
 my $output = '';
+sub_reset( 'Template::Provider::fetch' );
 $template->process('a/b/c/hello.tt', { name => 'Ingy' }, \$output )
 	or do {
 		die $template->error;
 	};
 is( $output, "Hello, Ingy.\n", "output is correct" );
+sub_calls( 'Template::Provider::fetch', 0, 'Provider fetch not called' );
 
-1;
+# Internals should remain unchanged
+is_deeply(
+	[
+		$provider->_OBJECT_->{SLOTS},
+		$provider->_OBJECT_->{LOOKUP},
+		$provider->_OBJECT_->{SIZE},
+	],
+	[ 0, {}, 10 ],
+	'Externals look as expected',
+);
