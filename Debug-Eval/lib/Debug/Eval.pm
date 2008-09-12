1eccdbf8639a8d46f95de16fcccbc489004e4273
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-09-12 10:08:41 +0000

    Adding the first version of Debug::Eval

---
+++ Debug-Eval/lib/Debug/Eval.pm
@@ -0,0 +1,33 @@
+package Debug::Eval;
+
+use 5.006;
+use strict;
+use Exporter   ();
+use File::Temp ();
+
+use vars qw{$VERSION @ISA @EXPORT $TRACE $UNLINK};
+BEGIN {
+	$VERSION = '1.00';
+	@ISA     = 'Exporter';
+	@EXPORT  = 'eval';
+	$TRACE   = 'STDERR' unless defined $TRACE;
+	$UNLINK  = 1        unless defined $UNLINK;
+}
+
+# Compile the combined code via a temp file
+sub eval {
+	my ($fh, $filename) = File::Temp::tempfile();
+	$fh->print("$_[0]") or die "print: $!";
+	close( $fh )        or die "close: $!";
+	my $message = "# require $filename\n";
+	if ( defined $TRACE and not ref $TRACE ) {
+		print STDOUT $message if $TRACE eq 'STDOUT';
+		print STDERR $message if $TRACE eq 'STDERR';
+	} elsif ( $TRACE ) {
+		$TRACE->print($message);
+	}
+	require $filename;
+	unlink $filename if $UNLINK;
+}
+
+1;
---
+++ Debug-Eval/t/01_compile.t
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+BEGIN {
+	$|  = 1;
+	$^W = 1;
+}
+
+use Test::More tests => 2;
+
+ok( $] >= 5.006, 'Perl version is new enough' );
