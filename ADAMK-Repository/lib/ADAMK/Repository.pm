4c5f8f0618aa4241a9ef09801b4298de75e9cd7d
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-11-10 12:50:54 +0000

    The constructor now works

--- ADAMK-Repository/lib/ADAMK/Repository.pm
+++ ADAMK-Repository/lib/ADAMK/Repository.pm
@@ -5,11 +5,18 @@ package ADAMK::Repository;
 use 5.008;
 use strict;
 use warnings;
+use Carp         'croak';
+use File::pushd  'pushd';
+use Params::Util '_STRING';
 
 use vars qw{$VERSION};
 BEGIN {
 	$VERSION = '0.01';
 }
+
+use Object::Tiny qw{
+	root
+};
 
 
 
@@ -20,7 +27,60 @@ BEGIN {
 
 sub new {
 	my $class = shift;
-	
+	my $self  = $class->SUPER::new(@_);
+
+	# Check params
+	unless ( -d $self->svn_dir($self->root) ) {
+		croak("Missing or invalid SVN root directory");
+	}
+
+	return $self;
+}
+
+
+
+
+
+#####################################################################
+# Simple SVN Interfaces
+
+sub svn_dir {
+	my $self = shift;
+	my $dir  = shift;
+	unless ( defined _STRING($dir) ) {
+		return undef;
+	}
+	unless ( -d $dir ) {
+		return undef;
+	}
+	unless ( -d File::Spec->catdir($dir, '.svn') ) {
+		return undef;
+	}
+	return $dir;
+}
+
+sub svn_command {
+	my $self = shift;
+	my $root = pushd( $self->root );
+	my $cmd  = join( ' ', 'svn', @_ );
+	my @rv   = `$cmd`;
+	chomp(@rv);
+	return @rv;
+}
+
+sub svn_info {
+	my $self = shift;
+	my $dir  = $self->svn_dir(shift);
+	my @info = $self->svn_command('info');
+	my %hash = map {
+		/^([^:]+)\s*:\s*(.*)$/;
+		my $key   = "$1";
+		my $value = "$2";
+		$key =~ s/\s+//g;
+		( $key, $value );
+	} grep { length $_ } @info;
+	$hash{Directory} = $dir;
+	return \%hash;
 }
 
 1;
---
+++ ADAMK-Repository/t/02_new.t
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+
+use strict;
+BEGIN {
+	$|  = 1;
+	$^W = 1;
+}
+
+use Test::More;
+if ( $ENV{ADAMK_SVN} ) {
+	plan( tests => 6 );
+} else {
+	plan( skip_all => '$ENV{ADAMK_SVN} is not defined' );
+}
+
+use ADAMK::Repository;
+
+my $root = $ENV{ADAMK_SVN};
+
+
+
+
+
+
+#####################################################################
+# Simple Constructor
+
+my $repository = ADAMK::Repository->new( root => $root );
+isa_ok( $repository, 'ADAMK::Repository' );
+is( $repository->root, $root, '->root ok' );
+
+
+
+
+
+#####################################################################
+# Trivial Methods
+
+my $hash = $repository->svn_info( $repository->root );
+is( ref($hash), 'HASH', '->svn_info' );
+is(
+	$hash->{RepositoryRoot},
+	'http://svn.ali.as/cpan',
+	'svn_info: Repository Root ok',
+);
+is(
+	$hash->{RepositoryUUID},
+	'88f4d9cd-8a04-0410-9d60-8f63309c3137',
+	'svn_info: Repository UUID ok',
+);
+is(
+	$hash->{NodeKind},
+	'directory',
+	'svn_info: Node Kind ok',
+);
