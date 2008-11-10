ece4e286988d8a420b7f5ea9140578ac4e076b6e
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-11-10 11:06:20 +0000

    Adding a module to try and centralise automated repository functionality

---
+++ ADAMK-Repository/lib/ADAMK/Repository.pm
@@ -0,0 +1,26 @@
+package ADAMK::Repository;
+
+# Implements a basic convenience model of ADAMK's repository.
+
+use 5.008;
+use strict;
+use warnings;
+
+use vars qw{$VERSION};
+BEGIN {
+	$VERSION = '0.01';
+}
+
+
+
+
+
+#####################################################################
+# Constructor
+
+sub new {
+	my $class = shift;
+	
+}
+
+1;
