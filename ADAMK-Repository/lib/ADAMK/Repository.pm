06ddc3275a9fe1bb8e75cf6a5c37384e51a55a1b
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-11-11 05:35:41 +0000

    Can now get a list of archive files

--- ADAMK-Repository/lib/ADAMK/Repository.pm
+++ ADAMK-Repository/lib/ADAMK/Repository.pm
@@ -5,9 +5,12 @@ package ADAMK::Repository;
 use 5.008;
 use strict;
 use warnings;
-use Carp         'croak';
-use File::pushd  'pushd';
-use Params::Util '_STRING';
+use Carp                  'croak';
+use File::Spec            ();
+use File::pushd           'pushd';
+use File::Find::Rule      ();
+use File::Find::Rule::VCS ();
+use Params::Util          qw{ _STRING _CODE };
 
 use vars qw{$VERSION};
 BEGIN {
@@ -17,6 +20,9 @@ BEGIN {
 use Object::Tiny qw{
 	root
 };
+
+# Preroll file find rules
+my $RELEASES = File::Find::Rule->name('*.tar.gz', '*.zip')->file;
 
 
 
@@ -33,8 +39,38 @@ sub new {
 	unless ( -d $self->svn_dir($self->root) ) {
 		croak("Missing or invalid SVN root directory");
 	}
+	if ( $self->{trace} and not _CODE($self->{trace}) ) {
+		$self->{trace} = sub { print @_ };
+	}
 
 	return $self;
+}
+
+sub dir {
+	File::Spec->catdir( shift->root, @_ );
+}
+
+sub file {
+	File::Spec->catfile( shift->root, @_ );
+}
+
+sub trace {
+	$_[0]->{trace}->( @_[1..$#_] ) if $_[0]->{trace};
+}
+
+
+
+
+
+#####################################################################
+# Releases
+
+sub release_dir {
+	$_[0]->dir('releases');
+}
+	
+sub release_archives {
+	$RELEASES->in( $_[0]->release_dir );
 }
 
 
@@ -63,6 +99,7 @@ sub svn_command {
 	my $self = shift;
 	my $root = pushd( $self->root );
 	my $cmd  = join( ' ', 'svn', @_ );
+	$self->trace("> $cmd\n");
 	my @rv   = `$cmd`;
 	chomp(@rv);
 	return @rv;
