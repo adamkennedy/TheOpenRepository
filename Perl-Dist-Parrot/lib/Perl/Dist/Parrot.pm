f181eec4a6c50e25dc5b71773855d75731d241db
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-11-19 12:28:27 +0000

    Basic metadata set

--- Perl-Dist-Parrot/lib/Perl/Dist/Parrot.pm
+++ Perl-Dist-Parrot/lib/Perl/Dist/Parrot.pm
@@ -1,5 +1,18 @@
 package Perl::Dist::Parrot;
 
+=pod
+
+=head1 NAME
+
+Perl::Dist::Parrot - Parrot and its languages for Win32
+
+=head1 DESCRIPTION
+
+This is an experimental distribution builder, aiming to create a usable
+installer for Parrot (and Rakudo, the Perl 6 implementation) on Windows.
+
+=cut
+
 use 5.008;
 use strict;
 use warnings;
@@ -48,3 +61,21 @@ sub install_custom {
 }
 
 1;
+
+=pod
+
+=head1 SUPPOT
+
+There is no support.
+
+=head1 COPYRIGHT
+
+Copyright 2008 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
