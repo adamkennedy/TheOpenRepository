7148b8d4021f9e6d9e37a994cabb0eab60e87884
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-11-19 20:36:14 +0000

    first, reeeeeaaaally ugly implementation, couple of tests, no docs yet

--- Text-FindIndent/lib/Text/FindIndent.pm
+++ Text-FindIndent/lib/Text/FindIndent.pm
@@ -18,17 +18,108 @@ use strict;
 
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '0.01';
+  $VERSION = '0.01';
 }
 
 sub parse {
-	my $class = shift;
-	my $text  = shift;
+  my $class = shift;
+  my $text  = shift;
+  my $textref = ref($text) ? $text : \$text; # accept references, too
 
-	# ...
+  my %indentdiffs;
+  my $lines                 = 0;
+  my $prev_indent           = undef;
+  my $skip                  = 0;
+  while ($$textref =~ /\G([ \t]*)([^\r\n]*)[\r\n]+/cgs) {
+    my $ws = $1;
+    my $rest = $2;
+    $lines++;
 
-	# By default, lets just say 4 spaces
-	return 's4';
+    if ($skip) {
+      $skip--;
+      next;
+    }
+
+    next if $rest eq '';
+
+    if ($ws eq '') {
+      $prev_indent = $ws;
+      next;
+    }
+
+    # skip next line if the last char is a backslash.
+    # Doesn't matter for Perl, but let's be generous!
+    $skip = 1 if $rest =~ /\\$/;
+    
+    # skip single-line comments
+    next if $rest =~ /^(?:#|\/\/|\/\*)/; # TODO: parse /* ... */!
+
+    # prefix-matching higher indentation level
+    if ($ws =~ /^\Q$prev_indent\E(.+)$/) {
+      my $diff = $1;
+      _grok_indent_diff($diff, \%indentdiffs);
+      $prev_indent = $ws;
+      next;
+    }
+
+    # prefix-matching lower indentation level
+    if ($prev_indent =~ /^\Q$ws\E(.+)$/) {
+      my $diff = $1;
+      _grok_indent_diff($diff, \%indentdiffs);
+      $prev_indent = $ws;
+      next;
+    }
+
+    # at this point, we're desperate!
+    my $prev_spaces = $prev_indent;
+    $prev_spaces =~ s/[ ]{0,7}\t/        /g;
+    my $spaces = $ws;
+    $spaces =~ s/[ ]{0,7}\t/        /g;
+    my $len_diff = length($spaces) - length($prev_spaces);
+    if ($len_diff) {
+      $len_diff = abs($len_diff);
+      $indentdiffs{"m$len_diff"}++;
+    }
+    $prev_indent = $ws;
+        
+  } # end while lines
+
+  # nothing found
+  return 'u' if not keys %indentdiffs;
+
+  my $max = 0;
+  my $maxkey = undef;
+  while (my ($key, $value) = each %indentdiffs) {
+    $maxkey = $key, $max = $value if $value > $max;
+  }
+
+  if ($maxkey =~ /^s(\d+)$/) {
+    my $mixedkey = "m" . $1;
+    my $mixed = $indentdiffs{$mixedkey};
+    if (defined($mixed) and $mixed >= $max * 0.2) {
+      return $mixedkey;
+    }
+  }
+
+  return $maxkey;
+}
+
+sub _grok_indent_diff {
+  my $diff = shift;
+  my $indentdiffs = shift;
+
+  if ($diff =~ /^ +$/) {
+    $indentdiffs->{"s" . length($diff)}++;
+  }
+  elsif ($diff =~ /^\t+$/) {
+    $indentdiffs->{"t" . length($diff)}++;
+  }
+  else { # mixed!
+    $diff =~ s/( +)$//;
+    my $trailing_spaces = $1;
+    $diff =~ s/ +//g; #  assume the spaces are all contained in tabs!
+    $indentdiffs->{"m" . (length($diff)*8+length($trailing_spaces))}++;
+  }
 }
 
 1;
@@ -39,17 +130,19 @@ sub parse {
 
 Bugs should be reported via the CPAN bug tracker at
 
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Class-Adapter>
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Text-FindIndent>
 
 For other issues, contact the author.
 
 =head1 AUTHOR
 
-Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>, Steffen Mueller E<lt>smueller@cpan.orgE<gt>
 
 =head1 COPYRIGHT
 
-Copyright 2008 Adam Kennedy.
+Copyright 2008 Adam Kennedy,
+
+Copyright 2008 Steffen Mueller.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
---
+++ Text-FindIndent/t/02_check_indentation.t
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+use strict;
+BEGIN {
+	$|  = 1;
+	$^W = 1;
+}
+
+use Test::More;
+use File::Spec;
+use Text::FindIndent;
+
+my %tests = (
+  t1 => [qw(
+    tabs1_1.txt
+  )],
+  s2 => [qw(
+    spaces2_1.txt
+  )],
+  s4 => [qw(
+    spaces4_1.txt
+  )],
+  u => [qw(
+    unknown_1.txt
+  )],
+  m4 => [qw(
+    mixed4_1.txt
+    mixed4_2.txt
+  )],
+);
+
+my $no_tests = 0;
+foreach (map {scalar @$_} values %tests) {
+  $no_tests += $_ * 3;
+}
+plan tests => $no_tests;
+
+
+chdir('t') if -d 't';
+
+
+foreach my $exp_result (keys %tests) {
+  my $testfiles = $tests{$exp_result};
+  foreach my $file (@$testfiles) {
+    my $testfile = File::Spec->catfile("data", $file);
+
+    my $text = slurp($testfile);
+    ok(defined $text, "slurped file '$testfile'");
+    my $result = Text::FindIndent->parse($text);
+    ok(defined $result, "Text::FindIndent->parse(text) returns something");
+    is($result, $exp_result, "Text::FindIndent->parse(text) returns correct result");
+
+  }
+}
+
+sub slurp {
+  my $file = shift;
+  open FH, "<$file" or die $!;
+  local $/ = undef;
+  my $text = <FH>;
+  close FH;
+  return $text;
+}
+
