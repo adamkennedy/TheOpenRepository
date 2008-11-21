5715638df4458eaf0333fbe09677f5c0478cc64e
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-11-21 15:41:38 +0000

    understand emacs modelines, this is Text::FindIndent 0.03

--- Text-FindIndent/lib/Text/FindIndent.pm
+++ Text-FindIndent/lib/Text/FindIndent.pm
@@ -53,6 +53,11 @@ override the heuristics. The modeline's options that are recognized
 are C<sts>/C<softtabstob>, C<et>/C<noet>/C<expandtabs>/C<noexpandtabs>,
 and C<ts>/C<tabstop>.
 
+Similarly, parsing of C<emacs> I<Local Variables> is somewhat supported.
+C<parse> use explicit settings to override the heuristics but uses style settings
+only as a fallback. The following options are recognized:
+C<tab-width>, C<indent-tabs-mode>, C<c-basic-offset>, and C<style>.
+
 =cut
 
 use 5.00503;
@@ -60,7 +65,7 @@ use strict;
 
 use vars qw{$VERSION};
 BEGIN {
-  $VERSION = '0.02';
+  $VERSION = '0.03';
 }
 
 sub parse {
@@ -78,18 +83,30 @@ sub parse {
   while ($$textref =~ /\G([ \t]*)([^\r\n]*)[\r\n]+/cgs) {
     my $ws = $1;
     my $rest = $2;
+    my $fullline = "$ws$rest";
     $lines++;
     
+    # check emacs start line stuff with some slack (shebang)
+    if ($lines < 3) {
+      $class->_check_emacs_local_variables_first_line($fullline, \%modeline_settings);
+    }
+
     # Do we have vim smart comments?
-    $class->_check_vim_modeline("$ws$rest", \%modeline_settings);
+    $class->_check_vim_modeline($fullline, \%modeline_settings);
+
+    # Do we have emacs smart comments?
+    $class->_check_emacs_local_variables($fullline, \%modeline_settings);
+
     if (exists $modeline_settings{softtabstop} and exists $modeline_settings{usetabs}) {
+      $modeline_settings{mixedmode} = $modeline_settings{usetabs}
+        if not defined $modeline_settings{mixedmode};
       return(
-        ($modeline_settings{usetabs} ? "m" : "s")
+        ($modeline_settings{mixedmode} ? "m" : "s")
         . $modeline_settings{softtabstop}
       );
     }
     elsif (exists $modeline_settings{tabstop} and $modeline_settings{usetabs}) {
-      return( "t" . $modeline_settings{tabstop} );
+      return( ($modeline_settings{mixedmode} ? "m" : "t") . $modeline_settings{tabstop} );
     }
     elsif (exists $modeline_settings{tabstop} and exists $modeline_settings{usetabs}) {
       return( "s" . $modeline_settings{tabstop} );
@@ -163,6 +180,14 @@ sub parse {
     }
   }
 
+  # fallback to emacs styles which are guesses only
+  foreach my $key (qw(softtabstop tabstop usetabs)) {
+    if (not exists $modeline_settings{$key}
+        and exists $modeline_settings{"style_$key"}) {
+      $modeline_settings{$key} = $modeline_settings{"style_$key"};
+    }
+  }
+
   if (exists $modeline_settings{softtabstop}) {
     $maxkey =~ s/\d+/$modeline_settings{softtabstop}/;
   }
@@ -178,6 +203,10 @@ sub parse {
     }
   }
 
+  # mixedmode explicitly asked for
+  if ($modeline_settings{mixedmode}) {
+    $maxkey =~ s/^./m/;
+  }
 
   return $maxkey;
 }
@@ -289,6 +318,182 @@ sub _check_vim_modeline {
   return;
 }
 
+
+
+
+
+{
+# lookup for emacs tab modes
+  my %tabmodelookup = (
+   t   => sub {
+     $_[0]->{mixedmode} = 1;
+     $_[0]->{usetabs} = 1;
+   },
+   nil => sub {
+     delete $_[0]->{mixedmode};
+     $_[0]->{usetabs} = 0;
+   },
+  );
+
+# lookup for emacs styles
+  my %stylelookup = (
+   kr => sub {
+     $_[0]->{style_softtabstop} = 4;
+     $_[0]->{style_tabstop} = 8;
+     $_[0]->{style_usetabs} = 1;
+   },
+   linux => sub {
+     $_[0]->{style_softtabstop} = 8;
+     $_[0]->{style_tabstop} = 8;
+     $_[0]->{style_usetabs} = 1;
+   },
+   'gnu' => sub {
+     $_[0]->{style_softtabstop} = 2;
+     $_[0]->{style_tabstop} = 8;
+     $_[0]->{style_usetabs} = 1;
+   },
+   'bsd' => sub {
+     $_[0]->{style_softtabstop} = 4;
+     $_[0]->{style_tabstop} = 8;
+     $_[0]->{style_usetabs} = 1;
+   },
+   'ellemtel' => sub {
+     $_[0]->{style_softtabstop} = 3;
+     $_[0]->{style_tabstop} = 3;
+     $_[0]->{style_usetabs} = 0;
+   },
+   'java' => sub {
+     $_[0]->{style_softtabstop} = 4;
+     $_[0]->{style_tabstop} = 8;
+   },
+  );
+  $stylelookup{'k&r'} = $stylelookup{kr};
+  $stylelookup{'bsd'} = $stylelookup{kr};
+  $stylelookup{'whitesmith'} = $stylelookup{kr};
+  $stylelookup{'stroustrup'} = $stylelookup{kr};
+
+  
+  
+  sub _check_emacs_local_variables_first_line {
+    my $class = shift;
+    my $line = shift;
+    my $settings = shift;
+
+# on first line (second if there is a shebang):
+#     -*- mode: $MODENAME; $VARNAME: $VALUE; ... -*-
+# ($FOO is not a literal)
+# Example with a Lisp comment:
+# ;; -*- mode: Lisp; fill-column: 75; comment-column: 50; -*-
+
+
+    my $var = qr/[^\s:]+/;
+    my $value = qr/[^;]+/; # dumb
+    my $pair = qr/\s*$var\s*:\s*$value;/;
+    my $firstline = qr/-\*-\s*mode:\s*[^\s;]+;\s*($pair+)\s*-\*-/;
+    if ($line =~ $firstline) {
+      my @pairs = split /\s*;\s*/, $1;
+      foreach my $pair (@pairs) {
+        my ($key, $value) = split /\s*:\s*/, $pair, 2;
+        if ($key eq 'tab-width') {
+          $settings->{tabstop} = $value;# FIXME: check var
+        }
+        elsif ($key eq 'indent-tabs-mode') {
+          $tabmodelookup{$value}->($settings) if defined $tabmodelookup{$value};
+        }
+        elsif ($key eq 'c-basic-offset') {
+          $settings->{tabstop} ||= $value; # tab-width takes precedence!?
+        }
+        elsif ($key eq 'style') { # this is quite questionable practice...
+          $stylelookup{$value}->($settings) if defined $stylelookup{$value};
+        }
+      }
+    }
+
+    # do this only as a LAST resort!
+    #$settings->{tabstop}     = $settings->{style_tabstop}     if not exists $settings->{tabstop};
+    #$settings->{softtabstop} = $settings->{style_softtabstop} if not exists $settings->{softtabstop};
+    #$settings->{usetabs}     = $settings->{style_usetabs}     if not exists $settings->{usetabs};
+
+    return();
+  }
+
+  sub _check_emacs_local_variables {
+    my $class = shift;
+    my $line = shift;
+    my $settings = shift;
+
+# A local variables list goes near the end of the file, in the last page.[...]
+# The local variables list starts with a line containing the string `Local Variables:',
+# and ends with a line containing the string `End:'. In between come the variable names
+# and values, one set per line, as `variable: value'. The values are not evaluated;
+# they are used literally. If a file has both a local variables list and a `-*-'
+# line, Emacs processes everything in the `-*-' line first, and everything in the
+# local variables list afterward.
+# 
+# Here is an example of a local variables list:
+# 
+#     ;; Local Variables: **
+#     ;; mode:lisp **
+#     ;; comment-column:0 **
+#     ;; comment-start: ";; "  **
+#     ;; comment-end:"**" **
+#     ;; End: **
+# 
+# Each line starts with the prefix `;; ' and each line ends with the suffix ` **'.
+# Emacs recognizes these as the prefix and suffix based on the first line of the
+# list, by finding them surrounding the magic string `Local Variables:'; then it
+# automatically discards them from the other lines of the list.
+# 
+# The usual reason for using a prefix and/or suffix is to embed the local variables
+# list in a comment, so it won't confuse other programs that the file is intended as
+# input for. The example above is for a language where comment lines start with `;; '
+# and end with `**'; the local values for comment-start and comment-end customize the
+# rest of Emacs for this unusual syntax. Don't use a prefix (or a suffix) if you don't need one. 
+#
+#
+# Can it be any more annoying to parse? --Steffen
+
+    if ($settings->{in_local_variables_section}) {
+      my $prefix = $settings->{local_variable_prefix};
+      $prefix = '' if not defined $prefix;
+      $prefix = quotemeta($prefix);
+      my $suffix = $settings->{local_variable_suffix};
+      $suffix = '' if not defined $suffix;
+      $suffix = quotemeta($suffix);
+
+      if ($line =~ /^\s*$prefix\s*([^\s:]+):\s*(.+)$suffix\s*$/) {
+        my $key = $1;
+        my $value = $2;
+        $value =~ s/\s+$//;
+        if ($key eq 'tab-width') {
+          $settings->{tabstop} = $value;
+        }
+        elsif ($key eq 'indent-tabs-mode') {
+          $tabmodelookup{$value}->($settings) if defined $tabmodelookup{$value};
+        }
+        elsif ($key eq 'c-basic-offset') {
+          $settings->{tabstop} ||= $value; # tab-width takes precedence!?
+        }
+        elsif ($key eq 'style') { # this is quite questionable practice...
+          $stylelookup{$value}->($settings) if defined $stylelookup{$value};
+        }
+      } # end if variable line
+      else {
+        delete $settings->{in_local_variables_section};
+        delete $settings->{local_variable_prefix};
+        delete $settings->{local_variable_suffix};
+      }
+    }
+    elsif ($line =~ /^\s*(\S*)\s*Local Variables:\s*(\S*)\s*$/) {
+      $settings->{local_variable_prefix} = $1;
+      $settings->{local_variable_suffix} = $2;
+      $settings->{in_local_variables_section} = 1;
+    }
+  }
+
+} # end lexical block for emacs lookups
+
+
 1;
 
 =pod
@@ -306,6 +511,7 @@ For other issues, contact the author.
 Adam Kennedy E<lt>adamk@cpan.orgE<gt>, Steffen Mueller E<lt>smueller@cpan.orgE<gt>
 
 =head1 COPYRIGHT
+
 
 Copyright 2008 Adam Kennedy,
 
