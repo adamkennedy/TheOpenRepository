98e3a00d8322676e99385ceec1d75cb3a8c49fbc
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-11-30 11:42:00 +0000

    add XS benchmark

--- Object-Tiny-XS/examples/benchmark.pl
+++ Object-Tiny-XS/examples/benchmark.pl
@@ -4,20 +4,35 @@ BEGIN {
     $DB::single = 1;
 }
 
+# For this benchmark, you also need Object::Tiny:
 use Object::Tiny          ();
+
+use Object::Tiny::XS      ();
 use Class::Accessor::Fast ();
 use Foo_Bar_Accessor      ();
 use Foo_Bar_Accessor2     ();
 use Foo_Bar_Tiny          ();
 use Foo_Bar_Tiny2         ();
+use Foo_Bar_Tiny_XS       ();
+use Foo_Bar_Tiny_XS2      ();
 
 use Benchmark ':all';
 
 print "\nBenchmarking constructor plus accessors...\n";
 
-cmpthese( 1000000, {
+cmpthese( -2, {
     'tiny' => '
         my $object = Foo_Bar_Tiny->new(
+            foo => 1,
+            bar => 2,
+            baz => 4,
+        );
+        $object->foo;
+        $object->bar;
+        $object->baz;
+    ',
+    'tiny_xs' => '
+        my $object = Foo_Bar_Tiny_XS->new(
             foo => 1,
             bar => 2,
             baz => 4,
@@ -41,9 +56,16 @@ cmpthese( 1000000, {
 sleep 1;
 print "\nBenchmarking constructor alone...\n";
 
-cmpthese( 1000000, {
+cmpthese( -1, {
     'tiny' => '
         Foo_Bar_Tiny->new(
+            foo => 1,
+            bar => 2,
+            baz => 4,
+        );
+    ',
+    'tiny_xs' => '
+        Foo_Bar_Tiny_XS->new(
             foo => 1,
             bar => 2,
             baz => 4,
@@ -67,18 +89,32 @@ my $tiny = Foo_Bar_Tiny->new(
     baz => 4,
 );
 
-my $accessor = Foo_Bar_Tiny->new( {
+my $tiny_xs = Foo_Bar_Tiny_XS->new(
+    foo => 1,
+    bar => 2,
+    baz => 4,
+);
+
+
+my $accessor = Foo_Bar_Accessor->new( {
     foo => 1,
     bar => 2,
     baz => 3,
 } );
 
-cmpthese( 1000, {
+cmpthese( -2, {
     'tiny' => sub {
         foreach ( 1 .. 1000 ) {
             $tiny->foo;
             $tiny->bar;
             $tiny->baz;
+        }
+    },
+    'tiny_xs' => sub {
+        foreach ( 1 .. 1000 ) {
+            $tiny_xs->foo;
+            $tiny_xs->bar;
+            $tiny_xs->baz;
         }
     },
     'accessor' => sub {
