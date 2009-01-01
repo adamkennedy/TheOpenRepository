b6a248127ed0cf84f25b8755b14a696e82f6f975
bramble.andrew@gmail.com <bramble.andrew@gmail.com@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-01-01 08:06:51 +0000

    consider moving this out of macropod trunk. latest padre hack against padre@2337

--- Macropod-dev/trunk/lib/Padre/Plugin/Macropod.pm
+++ Macropod-dev/trunk/lib/Padre/Plugin/Macropod.pm
@@ -1,3 +1,151 @@
+package Padre::Macropod::Task;
+use strict;
+use warnings; 
+
+use Carp qw( confess );
+use Scalar::Util qw( blessed );
+use base 'Padre::Task';
+use Padre::Pod2HTML;
+#our $EV_OUTPUT: shared = Wx::NewEventType();
+
+
+
+
+#sub prepare {
+#    my $self = shift;
+#    my $padre = Padre->ide->wx->main_window;
+#    Wx::Event::EVT_COMMAND($main, -1, $EV_OUTPUT, \&display_document);	
+#    return;
+#    	
+#}
+{
+my  $parser = Macropod::Parser->new();
+#$parser->init_cache;	
+sub run {
+    my $self = shift;
+    my $ref = $self->{doc};
+    my $doc;
+eval {
+  if ( ref $ref eq 'SCALAR' ) {
+	  warn "Parsing SCALAR";
+    $doc = $parser->parse_text( $ref );
+  }
+    elsif ( blessed $ref && $ref->isa('Padre::Document') ) {
+	warn "Parsing Padre::Document";
+	$doc = $parser->parse_text( $ref->text_get);
+  }
+  else {
+    my $path = Padre::Pod::Viewer->module_to_path( $ref );
+    warn "Parsing resolved '$ref' to $path ";
+    $doc = $parser->parse_file( $path );
+  }
+};
+
+if ($@ ) {
+  warn "FAILED PARSE $@";
+  return;	
+}
+  unless ( defined $doc ) {
+	 warn "Could not parse $ref " ;
+	 return;
+  }
+  
+  my $html = $self->inflate( $doc );
+  $self->{html} = $html;
+delete $self->{doc};
+  #$self->post_event( $EV_OUTPUT , \$html );
+  return 1;
+    
+	
+}
+
+}
+
+sub inflate {
+	my ($self,$doc) = @_;
+	my $pod;
+	eval {
+		my $processor= Macropod::Processor->new();
+		$pod     = $processor->process( $doc );
+
+	};
+	if ($@) {
+		    warn "Failed to inflate $doc , $@";
+		    return;
+	}
+	
+#	my $html = eval { Padre::Pod2HTML->pod2html( $$pod ) };
+#	warn "GOT HTML $html";
+#	if ( $@ ) {
+#		    warn "INFLATE FAIL $@";
+#		    return;
+#	}
+	
+	open ( FILEOUT, '>' , '/tmp/padre.macropod' );
+	print FILEOUT $$pod;
+	close FILEOUT;
+	my $view = Padre::Pod::Viewer::POD->new;
+	$view->start_html;
+	$view->parse_from_file('/tmp/padre.macropod');
+	my $html = $view->get_html;
+	return $html;
+}
+
+
+sub finish {
+    my ($self) = @_;
+    $self->{main_thread_only}->( $self->{html} );
+    return 1;
+}
+1;
+
+
+
+
+package Padre::Macropod::Viewer;
+use strict;
+use warnings;
+
+use base qw( Padre::Pod::Viewer );
+use Scalar::Util qw( blessed );
+
+sub display {
+  my ($self,$ref) = @_;
+
+  my $task = eval {
+	    Padre::Macropod::Task->new( doc=>$ref, main_thread_only =>
+		sub { $self->collect(@_);  } 
+	);
+	  };
+  if ( $@ ) { warn $@ ; return };
+	  
+  $task->schedule;
+  
+  
+}
+  
+sub collect {
+    my ($self,$html) = @_;
+    #warn "Collecting $html";
+    $self->SetPage( $html );    
+    $self->SetFocus;
+    $self->Show(1);
+}
+
+
+
+sub macropod_currentdoc {
+	my ($self) = shift;
+	my $doc = Padre::Documents->current;
+	warn "Current doc $doc";
+	$self->display( \$doc->text_get );
+
+
+};
+
+1;
+
+
 package Padre::Plugin::Macropod;
 use 5.008;
 use strict;
@@ -6,13 +154,19 @@ use base qw( Padre::Plugin );
 
 use Scalar::Util qw( blessed );
 
+#use lib qw( /home/smee/dev/tmp/Macropod-dev/trunk/lib );
+use vars qw( $VERSION $BUILD );
+$BUILD = 'cat';
+$VERSION = '0.22';
+
+warn __PACKAGE__ . "version $VERSION , build $BUILD";	
+
 use Padre::Pod::Frame;
 use Padre::Pod::Viewer;
 use Padre::Documents;
 use Macropod::Parser;
 use Macropod::Processor;
 
-our $VERSION = '0.22';
 
 sub padre_interfaces {
   'Padre::Plugin' => 0.20,
@@ -24,9 +178,9 @@ sub plugin_enable {
   my ($self) = @_;
  eval {
 
-    require Macropod::Parser;
-    require Macropod::Processor;
- 
+    #require Padre::Macropod::Viewer;
+    #require Padre::Macropod::Task;
+    
     my $display = Padre::Pod::Frame->new();
     my $oldhelp = Padre->ide->wx->main_window->{help};
     my $oldhtml = $display->{html};
@@ -52,7 +206,9 @@ sub plugin_enable {
  Padre->ide->wx->main_window->{help} = $display;
 };
 
-warn $@ if $@;
+warn $@ if @$;
+
+return $@ ? 1 : 0;
 
 }
 
@@ -72,8 +228,8 @@ sub plugin_disable {
 	else {
 	    Padre->ide->wx->main_window->{help} = undef;
 	}
-   #Class::Unload->unload( "Macropod::Parser" );
-   # Class::Unload->unload( "Macropod::Processor" );
+   Class::Unload->unload( __PACKAGE__ . '::Task' );
+   Class::Unload->unload( __PACKAGE__ . '::Viewer' );
 
 }
 
@@ -99,6 +255,8 @@ sub about {
 Macropod hijacks the Padre POD Viewer , replacing it
 with a Macropod equivalent. 
 
+version $VERSION , build $BUILD
+
 See Macropod for more details
 http://svn.ali.as/cpan/trunk/Macropod-dev/trunk
 
@@ -107,94 +265,6 @@ END_MESSAGE
 	# Show the About dialog
 	Wx::AboutBox( $about );     
 }
-
-1;
-
-
-
-
-
-package Padre::Macropod::Viewer;
-use strict;
-use warnings;
-
-use base qw( Padre::Pod::Viewer );
-use Scalar::Util qw( blessed );
-our  $parser = Macropod::Parser->new();
-#$parser->init_cache;
-
-sub display {
-  my ($self,$ref) = @_;
-  my ($doc);
-  if ( ref $ref eq 'SCALAR' ) {
-	  warn "Parsing SCALAR";
-    $doc = $parser->parse_text( $ref );
-  }
-    elsif ( blessed $ref && $ref->isa('Padre::Document') ) {
-	warn "Parsing Padre::Document";
-	$doc = $parser->parse_text( \$ref->{original_content} );
-  }
-  else {
-    my $path = $self->module_to_path( $ref );
-    warn "Parsing resolved '$ref' to $path ";
-    $doc = $parser->parse_file( $path );
-  }
-  
-  unless ( defined $doc ) {
-	 warn "Could not parse $ref " ;
-	 return;
-  }	
-  my $html = $self->inflate( $doc );
-
-    $self->SetPage( $html );
-    #$self->SetTitle( 'Macropod : ' . $doc->title );
-$self->SetFocus;
-$self->Show(1);
-  
-
-}
-
-sub inflate {
-	my ($self,$doc) = @_;
-	my $pod;
-	eval {
-		my $processor= Macropod::Processor->new();
-		$pod     = $processor->process( $doc );
-
-	};
-	if ($@) {
-		    warn "Failed to inflate $doc , $@";
-		    return;
-	}
-	#warn $$pod;
-	
-	open ( FILEOUT, '>' , '/tmp/padre.macropod' );
-	print FILEOUT $$pod;
-	close FILEOUT;
-	my $view = Padre::Pod::Viewer::POD->new;
-	$view->start_html;
-	$view->parse_from_file('/tmp/padre.macropod');
-	my $html = $view->get_html;
-	return $html;
-}
-
-sub macropod_currentdoc {
-	my ($self) = shift;
-	my $doc = Padre::Documents->current;
-	$self->display( $doc );
-
-
-};
-
-sub _setup_podviewer {
-   my ($self) = @_;
-   $self->SUPER::_setup_podviewer();
-   my $old = $self->{html};
-   my $new = Padre::Macropod::Viewer->new( $old->GetParent, -1 );
-  
-}
-
-
 
 1;
 
