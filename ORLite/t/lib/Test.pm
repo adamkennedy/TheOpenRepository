6453ad81e60710837b33775aba8f2209115181e8
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-01-03 05:49:23 +0000

    Correcting version

--- ORLite/t/lib/Test.pm
+++ ORLite/t/lib/Test.pm
@@ -8,7 +8,7 @@ use File::Spec::Functions ':ALL';
 
 use vars qw{$VERSION @ISA @EXPORT};
 BEGIN {
-        $VERSION = '1.16';
+        $VERSION = '1.17';
 	@ISA     = qw{ Exporter };
 	@EXPORT  = qw{ test_db connect_ok create_ok };
 }
