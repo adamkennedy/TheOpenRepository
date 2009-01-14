f47f9b6877b295275738c281f62086add0ba94fb
jkegl@cpan.org <jkegl@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-01-14 18:36:13 +0000

    Perlcritic fixes. All tests successful.

--- Parse-Marpa/lib/Parse/Marpa/MDL.pm
+++ Parse-Marpa/lib/Parse/Marpa/MDL.pm
@@ -1,43 +1,47 @@
-use 5.010_000;
-
 package Parse::Marpa::MDL;
+
+use 5.010_000;
+use strict;
+use warnings;
 
 sub gen_symbol_from_regex {
     my $regex = shift;
     my $data  = shift;
-    if ( scalar @$data == 0 ) {
+    if ( scalar @{$data} == 0 ) {
         my $number = 0;
-        push( @$data, {}, \$number );
+        push @{$data}, {}, \$number;
     }
-    my ( $regex_hash, $uniq_number ) = @$data;
+    my ( $regex_hash, $uniq_number ) = @{$data};
     given ($regex) {
-        when (/^qr/) { $regex = substr( $regex, 3, -1 ); }
-        default      { $regex = substr( $regex, 1, -1 ); };
+        when (/^qr/xms) { $regex = substr $regex, 3, -1; }
+        default      { $regex = substr $regex, 1, -1; };
     }
     my $symbol = $regex_hash->{$regex};
     return $symbol if defined $symbol;
-    $symbol = substr( $regex, 0, 20 );
-    $symbol =~ s/%/%%/g;
-    $symbol =~ s/([^[:alnum:]_-])/sprintf("%%%.2x", ord($1))/ge;
-    $symbol .= sprintf( ":k%x", ($$uniq_number)++ );
+    $symbol = substr $regex, 0, 20;
+    $symbol =~ s/%/%%/gxms;
+    $symbol =~ s/([^[:alnum:]_-])/sprintf("%%%.2x", ord($1))/gexms;
+    $symbol .= sprintf ':k%x', (${$uniq_number})++;
     $regex_hash->{$regex} = $symbol;
-    ( $symbol, 1 );
+    return ( $symbol, 1 );
 }
 
 sub canonical_symbol_name {
     my $symbol = lc shift;
-    $symbol =~ s/[-_\s]+/-/g;
-    $symbol;
+    $symbol =~ s/[-_\s]+/-/gxms;
+    return $symbol;
 }
 
 sub get_symbol {
     my $grammar     = shift;
     my $symbol_name = shift;
-    Parse::Marpa::Grammar::get_symbol( $grammar,
+    return Parse::Marpa::Grammar::get_symbol( $grammar,
         canonical_symbol_name($symbol_name) );
 }
 
 1;
+
+__END__
 
 =head1 NAME
 
