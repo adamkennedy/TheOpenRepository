04c7e0a5598dffbb04192e904e601ed80728fb77
jkegl@cpan.org <jkegl@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-01-14 18:45:28 +0000

    Perlcritic fixes. All tests successful.

--- Parse-Marpa/lib/Parse/Marpa/Offset.pm
+++ Parse-Marpa/lib/Parse/Marpa/Offset.pm
@@ -8,19 +8,21 @@ use integer;
 sub import {
     my $class = shift;
     my $struct_name = shift;
+    my @fields = @_;
     my $pkg = caller;
-    # say STDERR $pkg;
-    my $prefix = $pkg . '::' . $struct_name . '::';
+    my $prefix = $pkg . q{::} . $struct_name . q{::};
     my $offset = 0;
 
-    no strict "refs";
-    for my $field (@_) {
+    ## no critic (TestingAndDebugging::ProhibitNoStrict)
+    no strict 'refs';
+    ## use critic
+    for my $field (@fields) {
        # offset must be copy, because contents of sub must be constant
        my $field_name = $prefix . $field;
-       # say STDERR $field_name, ' => ', $offset;
-       *$field_name = sub () { $offset };
+       *{$field_name} = sub () { $offset };
        $offset++;
     }
+    return 1;
 }
 
 1;
