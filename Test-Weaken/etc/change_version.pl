158d8e8497f3d0403c529ba31fdf89ce896f08e3
jkegl@cpan.org <jkegl@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-01-22 18:22:47 +0000

    Upped version number.

---
+++ Test-Weaken/etc/change_version.pl
@@ -0,0 +1,86 @@
+#!perl
+
+use 5.010;
+use strict;
+use warnings;
+use Fatal qw(:void open close unlink select);
+use English;
+
+our $FH;
+
+croak("usage: $0: old_version new_version") unless scalar @ARGV == 2;
+
+my ($old, $new) = @ARGV;
+
+say STDERR "$old $new";
+
+sub check_version {
+   my $version = shift;
+   my ($major, $minor1, $underscore, $minor2) = ($version =~ m/^ ([0-9]+) [.] ([0-9.]{3}) ([_]?) ([0-9.]{3}) $/x);
+   if (not defined $minor2) {
+       croak("Bad format in version number: $version");
+   }
+   if ($minor1 % 2 and $underscore ne '_') {
+       croak("No underscore in developer's version number: $version");
+   }
+   if ($minor1 % 2 == 0 and $underscore eq '_') {
+       croak("Underscore in official release version number: $version");
+   }
+}
+
+check_version($old);
+check_version($new);
+
+croak("$old >= $new") if eval $old >= eval $new;
+
+sub change {
+   my $fix = shift;
+   my @files = @_;
+   for my $file (@files) {
+     open(FH, '<', $file);
+     my $text = do {local $RS; <FH> };
+     my $backup = "save/$file";
+     rename($file, $backup);
+     open(ARGVOUT, '>', $file);
+     select(ARGVOUT);
+     print ARGVOUT ${$fix->(\$text, $file)};
+     select(STDOUT);
+     close(FH);
+   }
+}
+
+sub fix_META_yml {
+    my $text_ref = shift;
+    my $file_name = shift;
+
+    say STDERR "failed to change version from $old to $new in $file_name"
+        unless ${$text_ref} =~ s/(version:\s*)$old/$1$new/g;
+    $text_ref;
+}
+
+sub fix_Marpa_pm {
+    my $text_ref = shift;
+    my $file_name = shift;
+
+    say STDERR "failed to change VERSION from $old to $new in $file_name"
+        unless ${$text_ref} =~ s/(our\s+\$VERSION\s*=\s*')$old';/$1$new';/;
+    say STDERR "failed to change version from $old to $new in $file_name"
+        unless ${$text_ref} =~ s/(version\s+is\s+)$old/$1$new/;
+    $text_ref;
+}
+
+sub update_changes {
+    my $text_ref = shift;
+    my $file_name = shift;
+
+    my $date_stamp = `date`;
+    say STDERR "failed to add $new to $file_name"
+        unless ${$text_ref} =~ s/(\ARevision\s+history\s+[^\n]*\n\n)/$1$new $date_stamp\n/xms;
+    $text_ref;
+}
+
+change(\&fix_META_yml, 'META.yml');
+change(\&fix_Marpa_pm, 'lib/Test/Weaken.pm');
+change(\&update_changes, 'Changes');
+
+say STDERR "REMEMBER TO UPDATE Changes file";
--- Test-Weaken/lib/Test/Weaken.pm
+++ Test-Weaken/lib/Test/Weaken.pm
@@ -4,7 +4,7 @@ require Exporter;
 
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw(poof);
-our $VERSION   = '1.001_000';
+our $VERSION   = '1.001_001';
 $VERSION   = eval $VERSION;
 
 use warnings;
