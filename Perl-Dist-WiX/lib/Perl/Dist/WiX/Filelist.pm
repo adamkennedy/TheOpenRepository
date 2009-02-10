32bd4a6804c0bda8fb62c78373be35df60a170c7
csjewell@cpan.org <csjewell@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-02-10 19:01:16 +0000

    Changed Filelist.pm to be a hashref internally instead of an arrayref.
    Changed and added tests appropriately, including dealing with sorting in Filelist.pm->files

--- Perl-Dist-WiX/lib/Perl/Dist/WiX/Filelist.pm
+++ Perl-Dist-WiX/lib/Perl/Dist/WiX/Filelist.pm
@@ -8,21 +8,18 @@ package Perl::Dist::WiX::Filelist;
 #
 # License is the same as perl. See Wix.pm for details.
 #
-# $Rev$ $Date$ $Author$
-# $URL$
 #<<<
 use     5.008;
 use     strict;
 use     warnings;
-use     Carp                   qw( croak                        );
-use     File::Spec::Functions  qw( catdir catfile               );
-use     List::MoreUtils        qw( indexes                      );
-use     Params::Util           qw( _INSTANCE _STRING _NONNEGINT );
+use     Carp                   qw( croak                             );
+use     File::Spec::Functions  qw( catdir catfile splitpath splitdir );
+use     Params::Util           qw( _INSTANCE _STRING _NONNEGINT      );
 use     IO::Dir                qw();
 use     IO::File               qw();
 require Perl::Dist::WiX::Misc;
 
-use vars qw( $VERSION @ISA );
+use vars qw( $VERSION @ISA %sortcache);
 BEGIN {
 	$VERSION = '0.13_01';
     @ISA = 'Perl::Dist::WiX::Misc';
@@ -33,7 +30,7 @@ BEGIN {
 #   files: Returns the list of files as an arrayref.
 
 use Object::Tiny qw {
-  files
+  files_hashref
 };
 
 #####################################################################
@@ -49,7 +46,7 @@ sub new {
     my $self = shift->SUPER::new();
 
     # Initialize files area.
-    $self->{files} = [];
+    $self->{files_hashref} = {};
 
     # Set defaults and check parameters
     if ( not defined $self->{trace} ) {
@@ -58,8 +55,6 @@ sub new {
     if ( not defined _NONNEGINT( $self->{trace} ) ) {
         croak "Invalid trace parameter";
     }
-
-
 
     return $self;
 } ## end sub new
@@ -79,14 +74,82 @@ sub clone {
     }
 
     # Add filelist passed in.
-    $self->{files} = [];
-    push @{ $self->{files} }, @{ $source->{files} };
+    $self->{files_hashref} = {};
+    %{ $self->{files_hashref} } = %{ $source->{files_hashref} };
+    
+    # Keep trace level the same.
+    $self->{trace} = $source->{trace};
 
     return $self;
 } ## end sub clone
 
 #####################################################################
 # Main Methods
+
+sub _splitdir {
+    my $dirs = shift;
+    
+    my @dirs = splitdir($dirs);
+    
+    @dirs = grep { defined $_ and $_ ne q{} } @dirs;
+    
+    return \@dirs;
+}
+
+sub _splitpath {
+    my $path = shift;
+    
+    my @answer = splitpath($path, 0);
+    
+    return \@answer;
+}
+
+sub _sorter {
+# Takes advantage of $a and $b, using the Orcish Manoevure to cache
+# calls to File::Spec::Functions::splitpath and splitdir
+
+    # Short-circuit.
+    return 0 if ($a eq $b);
+    
+    # Get directoryspec and file 
+    my (undef, $dirspec_1, $file_1) = @{( $sortcache{$a} ||= _splitpath($a) )};
+    my (undef, $dirspec_2, $file_2) = @{( $sortcache{$b} ||= _splitpath($b) )};
+
+    # Deal with equal directories by comparing their files.
+    return ($file_1 cmp $file_2) if ($dirspec_1 eq $dirspec_2);
+
+    # Get list of directories.
+    my @dirs_1 = @{( $sortcache{$dirspec_1} ||= _splitdir($dirspec_1) )};
+    my @dirs_2 = @{( $sortcache{$dirspec_2} ||= _splitdir($dirspec_2) )};
+    
+    # Find first directory that is not equal.
+    my ( $dir_1, $dir_2 ) = ( q{}, q{} );
+    while ($dir_1 eq $dir_2) {
+        $dir_1 = shift @dirs_1 || q{};
+        $dir_2 = shift @dirs_2 || q{};
+    }
+
+    # Compare directories/
+    return  1 if $dir_1 eq q{};
+    return -1 if $dir_2 eq q{};
+    return $dir_1 cmp $dir_2;
+}
+
+
+########################################
+# files
+# Parameters:
+#   None.
+# Returns:
+#   Sorted list of files in this object
+
+sub files { 
+    my $self = shift; 
+
+    my @answer = sort { _sorter } keys(%{$self->files_hashref}); 
+    return \@answer;
+}
+
 
 ########################################
 # count
@@ -95,7 +158,7 @@ sub clone {
 # Returns:
 #   Number of files in this object
 
-sub count { my $self = shift; return scalar @{ $self->{files} }; }
+sub count { my $self = shift; return scalar keys(%{ $self->files_hashref }); }
 
 ########################################
 # clear
@@ -106,7 +169,7 @@ sub count { my $self = shift; return scalar @{ $self->{files} }; }
 # Action:
 #   Clears this filelist.
 
-sub clear { my $self = shift; $self->{files} = []; return $self; }
+sub clear { my $self = shift; $self->{files_hashref} = {}; return $self; }
 
 ########################################
 # readdir($dir)
@@ -151,7 +214,7 @@ sub readdir {
             } else {
 
                 # Add the file!
-                push @{ $self->files }, $filespec;
+                $self->files_hashref->{$filespec} = 1;
             }
         } ## end if ( ( $file ne q{.} )...
 
@@ -189,10 +252,16 @@ sub load_file {
     }
     my @files = <$fh>;
     $fh->close;
-
+    my $file;
+    
     # Insert list of files read into this object. Chomp on the way.
-    @{ $self->files } = map { chomp $_; $_ } @files;
-
+    my %files = map {
+        $file = $_;
+        chomp $file;
+        ($file => 1);
+    } @files;
+    $self->{files_hashref} = \%files;
+    
     return $self;
 } ## end sub load_file
 
@@ -211,7 +280,7 @@ sub load_array {
     # Add each file in the array - if it is a file.
     foreach my $file ( @files ) {
         next if not -f $file;
-        push @{ $self->files }, $file;
+        $self->files_hashref->{$file} = 1;
     }
 
     return $self;
@@ -231,10 +300,10 @@ sub add_file {
 
     # Check parameters.
     unless ( _STRING( $file ) ) {
-        croak 'Missing or invalid dir parameter';
+        croak 'Missing or invalid file parameter';
     }
 
-    push @{ $self->files }, $file;
+    $self->files_hashref->{$file} = 1;
 
     return $self;
 } ## end sub add_file
@@ -256,40 +325,12 @@ sub subtract {
         croak 'Missing or invalid subtrahend parameter';
     }
 
-    # Define variables.
-    my @loc;
-    my @files = @{ $self->files };
-    my @files2;
-    my $f;
-
-    # For each file on the list passed in...
-    foreach my $f ( @{ $subtrahend->files } ) {
-
-        # Find if it is in us.
-        @loc = indexes { $_ eq $f } @files;
-        if ( @loc ) {
-            if ( $#loc > 1 ) {
-                print
-"[*] Subtracting more than one file with one entry [$f]:\n";
-                foreach my $loc ( @loc ) {
-                    print '[*] ' . $loc . q{ } . $files[$loc] . "\n";
-                }
-            }
-            delete @files[@loc];
-            undef @loc;
-
-            # 'compress' @files;
-            undef @files2;
-            while ( $#files > -1 ) {
-                $f = shift @files;
-                push @files2, $f if defined( $f );
-            }
-            @files = @files2;
-        } ## end if ( @loc )
-    } ## end foreach my $f ( @{ $subtrahend...
-
-    # Reload ourselves.
-    return $self->clear->load_array( @files );
+    my @files_to_remove = keys(%{$subtrahend->files_hashref});
+    my %files = %{$self->files_hashref};
+    delete @files{@files_to_remove};
+    $self->{files_hashref} = \%files;
+    
+    return $self;
 } ## end sub subtract
 
 ########################################
@@ -306,10 +347,12 @@ sub add {
 
     # Check parameters
     unless ( _INSTANCE( $term, 'Perl::Dist::WiX::Filelist' ) ) {
-        croak 'Missing or invalid subtrahend parameter';
+        croak 'Missing or invalid term parameter';
     }
 
-    push @{ $self->files }, @{ $term->files };
+    # Add the two hashes together.
+    my %files = ( %{$self->files_hashref}, %{$term->files_hashref} );
+    $self->{files_hashref} = \%files;
 
     return $self;
 } ## end sub add
@@ -335,14 +378,10 @@ sub move {
         croak 'Missing or invalid to parameter';
     }
 
-    # Find which files need moved.
-    my @loc = indexes { $_ =~ m/\A\Q$from\E\z/ } @{ $self->files };
-    if ( @loc ) {
-        foreach my $loc ( @loc ) {
-
-            # "move" them.
-            $self->files->[$loc] = $to;
-        }
+    # Move the file if it exists.
+    if ( $self->files_hashref->{$from} ) {
+        delete $self->files_hashref->{$from}; 
+        $self->files_hashref->{$to} = 1; 
     }
 
     return $self;
@@ -370,14 +409,17 @@ sub move_dir {
     }
 
     # Find which files need moved.
-    my @loc = indexes { "$_\\" =~ m(\A\Q$from\E\\) } @{ $self->files };
+    my @files_to_move = grep { "$_\\" =~ m(\A\Q$from\E\\) } keys(%{ $self->files_hashref });
     my $to_file;
-    if ( @loc ) {
-        foreach my $loc ( @loc ) {
-
-            # "move" them.
-            $self->files->[$loc] =~ s(\A\Q$from\E)($to);
-        }
+    foreach my $file_to_move ( @files_to_move ) {
+        
+        # Get the correct name.
+        $to_file = $file_to_move;
+        $to_file =~ s(\A\Q$from\E)($to);
+        
+        # "move" the file.
+        delete $self->files_hashref->{$file_to_move}; 
+        $self->files_hashref->{$to_file} = 1; 
     }
 
     return $self;
@@ -399,16 +441,20 @@ sub filter {
     my ( $self, $re_list ) = @_;
 
     # Define variables to use.
-    my @files = @{ $self->files };
+    my %files = %{ $self->files_hashref };
+    my @files_list = keys(%files);
 
+    my @files_to_remove;
+    
     # Filtering out values that match the regular expressions.
     foreach my $re ( @{$re_list} ) {
-        $self->trace_line( 2, "Filtering on $re\n" );
-        @files = grep { not( $_ =~ m/\A\Q$re\E/ ) } @files;
+        $self->trace_line( 4, "Filtering on $re\n" );
+        push @files_to_remove, grep { m/\A\Q$re\E/ } @files_list;
     }
 
-    $self->clear->load_array( @files );
-
+    delete @files{@files_to_remove};
+    $self->{files_hashref} = \%files;
+    
     return $self;
 } ## end sub filter
 
@@ -423,7 +469,9 @@ sub filter {
 sub as_string {
     my $self = shift;
 
-    return join "\n", @{ $self->files };
+    my @files = sort { _sorter } keys(%{ $self->files_hashref });
+    
+    return join "\n", @files;
 }
 
 1;
--- Perl-Dist-WiX/t/10_filelist.t
+++ Perl-Dist-WiX/t/10_filelist.t
@@ -15,13 +15,14 @@ my @file = (
     catfile( rel2abs(curdir()), qw(t test10 file3.txt)),
     catfile( rel2abs(curdir()), qw(t test10 excluded file1.txt)),
     catfile( rel2abs(curdir()), qw(t test10 excluded file2.txt)),    
-    catfile( rel2abs(curdir()), qw(t test10 dir2 file1.txt)),    
+    catfile( rel2abs(curdir()), qw(t test10 dir2 file1.txt)),    # dir2 deliberately does not exist.
     catfile( rel2abs(curdir()), qw(t test10 dir2 file2.txt)),    
+    catfile( rel2abs(curdir()), qw(t test10 dir3 file3.txt)),    
 );
 
 BEGIN {
 	if ( $^O eq 'MSWin32' ) {
-		plan tests => 11;
+		plan tests => 13;
 	} else {
 		plan skip_all => 'Not on Win32';
 	}
@@ -33,11 +34,13 @@ is( Perl::Dist::WiX::Filelist->new->add_file($file[0])->as_string,
     $file[0],
     'adding single file' );
 
-my @files = @file[1, 2];
-
-is( Perl::Dist::WiX::Filelist->new->load_array(@files)->as_string, 
+is( Perl::Dist::WiX::Filelist->new->load_array(@file[1, 2])->as_string, 
     "$file[1]\n$file[2]",
     'adding array' );
+
+is( Perl::Dist::WiX::Filelist->new->load_array(@file[1, 2, 5])->as_string, 
+    "$file[1]\n$file[2]",
+    'adding array with missing files' );
     
 my $add1 = Perl::Dist::WiX::Filelist->new->add_file($file[0]);
 my $add2 = Perl::Dist::WiX::Filelist->new->add_file($file[1]);
@@ -55,9 +58,10 @@ is( $sub1->subtract($sub2)->as_string,
     'subtraction' );
 
 my $filter = Perl::Dist::WiX::Filelist->new->load_array(@file);
-my $re = catdir( rel2abs(curdir()), qw(t test10 excluded));
+my $re_1 = catdir( rel2abs(curdir()), qw(t test10 excluded));
+my $re_2 = catdir( rel2abs(curdir()), qw(t test10 dir3));
 
-is( $filter->filter([$re])->as_string,
+is( $filter->filter([$re_1, $re_2])->as_string,
     "$file[0]\n$file[1]\n$file[2]",
     'filtering'); 
 
@@ -82,7 +86,7 @@ my $packlist_file = catfile(rel2abs(curdir()), qw(t test10 filelist.txt));
 my $fh;
 my $answer = open $fh, '>', $packlist_file;
 if ($answer) {
-    print $fh "$file[0]\n$file[1]\n$file[2]\n";
+    print $fh "$file[0]\n$file[2]\n$file[1]\n";
     close $fh;
 
     is (Perl::Dist::WiX::Filelist->new->load_file($packlist_file)->as_string,
@@ -93,5 +97,9 @@ if ($answer) {
     fail('reading from packlist file'); 
     diag('Could not create packlist in test directory.');
 }
+
+is( Perl::Dist::WiX::Filelist->new->load_array(@file[1, 2, 3, 7])->as_string, 
+    "$file[7]\n$file[3]\n$file[1]\n$file[2]",
+    'as_string with mixed directories' );
 
 
