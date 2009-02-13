9d3350550f43aa3c797240d70028880609a28a96
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-02-13 02:16:50 +0000

    Adding the beginnings of HTML::Spry::DataSet

---
+++ HTML-Spry-DataSet/lib/HTML/Spry/DataSet.pm
@@ -0,0 +1,24 @@
+package HTML::Spry::DataSet;
+
+=pod
+
+=head1 NAME
+
+HTML::Spry::DataSet - Generate HTML Table Data Set files for the Spry Javascript toolkit
+
+=head1 DESCRIPTION
+
+Spry is a JavaScript framework produced by Adobe. The following is taken
+from their website.
+
+I<"The Spry framework for Ajax is a JavaScript library that provides
+easy-to-use yet powerful Ajax functionality that allows designers
+to build pages that provide a richer experience for their users.>
+
+I<It is designed to take the complexity out of Ajax and allow designers
+to easily create Web 2.0 pages.">
+
+=cut
+
+use 5.008005;
+use strict;
