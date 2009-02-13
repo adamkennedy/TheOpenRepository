6e93e051f538951f0925d116998a55fb00a6f5be
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-02-13 12:19:48 +0000

    Adding initial bits of Acme::Mom::Yours

---
+++ Acme-Mom-Yours/lib/Acme/Mom/Yours.pm
@@ -0,0 +1,58 @@
+package Acme::Mom::Yours;
+
+=pod
+
+=head1 NAME
+
+Acme::Mom::Yours - Your mom is so fat she takes 2 months to compile
+
+=head1 DESCRIPTION
+
+This module is intended to demonstrate a CPAN distribution with a
+dependency chain significantly larger than even the largest legitimate
+application, but not SO large that it would be impossible to install.
+
+This module was created with a dependency on all the major Perl
+applications that form the CPANTS Heavy 100 index. This is currently:
+
+L<MojoMojo>
+
+L<Task::Catalyst::Tutorial>
+
+L<Task-Email-PEP-All>
+
+L<Parley>
+
+L<Foorum>
+
+L<Angerwhale>
+
+L<CommitBit>
+
+L<Jifty>
+
+L<Reaction>
+
+L<Buscador>
+
+L<Task::CatInABox>
+
+L<App::CamelPKI>
+
+L<Task::SOSA>
+
+L<App::HistHub>
+
+L<Test::Apocalypse>
+
+L<Devel::ebug::HTTP>
+
+L<Padre>
+
+L<Pod::Browser>
+
+L<Titanium>
+
+L<Handle>
+
+=cut
