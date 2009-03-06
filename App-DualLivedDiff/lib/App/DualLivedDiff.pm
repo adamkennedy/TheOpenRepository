28c0095513ec59936779b105605305bfc2b98238
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-03-06 18:46:34 +0000

    dual lived module to blead perl diffs

---
+++ App-DualLivedDiff/lib/App/DualLivedDiff.pm
@@ -0,0 +1,385 @@
+package App::DualLivedDiff;
+use strict;
+use warnings;
+
+our $VERSION = '1.00';
+
+use Getopt::Long;
+use Parse::CPAN::Meta ();
+use LWP::Simple;
+use File::Temp ();
+use File::Spec;
+use Archive::Extract;
+use File::Find ();
+
+our $diff_cmd = 'diff';
+our @exclude_files = (
+  qr(\.{1,2}$),
+  qr(\.svn$),
+  qr(\.git$),
+);
+
+sub usage {
+  print "@_\n\n" if @_;
+  print <<HERE;
+Usage: $0 -d source-dist -b /path/to/blead/checkout
+Does a diff FROM a dual lived module distribution TO blead perl
+
+-b/--blead   blead perl path
+-d/--dual    dual lived module distribution path, file, URL, or name
+-r/--reverse reverses the diff (blead to lib)
+-c/--config  name of the configuration file with file mappings
+             (defaults to .dualLivedDiffConfig in the module path or current path)
+HERE
+  exit(1);
+}
+
+my $bleadpath;
+my $dualmodule;
+my $reverse = 0;
+my $default_config_file = '.dualLivedDiffConfig';
+my $config_file = $default_config_file;
+sub run {
+  GetOptions(
+    'b|blead=s' => \$bleadpath,
+    'h|help' => \&usage,
+    'r|reverse' => \$reverse,
+    'd|dual=s' => \$dualmodule,
+    'c|conf|config|configfile=s' => \$config_file,
+  );
+
+  usage() if not defined $bleadpath or not -d $bleadpath;
+
+  my $workdir        = get_dual_lived_distribution_dir($dualmodule);
+  my $config         = get_config($workdir, $config_file);
+
+  my $files          = $config->{files} || {};
+  my $dirs_flat      = $config->{"dirs-flat"} || {};
+  my $dirs_recursive = $config->{"dirs-recursive"} || {};
+
+  foreach my $source_file (keys %$files) {
+    my $blead_file = $files->{$source_file};
+
+    my $absolute_source_file = File::Spec->catdir($workdir, $source_file);
+
+    if (-f $absolute_source_file) {
+      file_diff( $workdir, $bleadpath, $source_file, $blead_file );
+    }
+    elsif (-d $absolute_source_file) {
+      warn "'$absolute_source_file' is not a file but a directory. Use the 'dirs-flat' or 'dirs-recursive' config options instead!";
+      next;
+    }
+    else {
+      warn "Explicitly mapped file '$source_file' missing from dual lived module source tree!";
+      next;
+    }
+  }
+
+  foreach my $source_dir (keys %$dirs_flat) {
+    my $blead_dir = $dirs_flat->{$source_dir};
+
+    my $absolute_source_dir = File::Spec->catdir($workdir, $source_dir);
+    if (-f $absolute_source_dir) {
+      warn "'$absolute_source_dir' is not a directory but a file. Use the 'files' config option instead!";
+      next;
+    }
+    elsif (-d $absolute_source_dir) {
+      dir_diff( $workdir, $bleadpath, $source_dir, $blead_dir, 0 );
+    }
+    else {
+      warn "Explicitly mapped directory '$source_dir' missing from dual lived module source tree!";
+      next;
+    }
+  }
+
+  foreach my $source_dir (keys %$dirs_recursive) {
+    my $blead_dir = $dirs_recursive->{$source_dir};
+
+    my $absolute_source_dir = File::Spec->catdir($workdir, $source_dir);
+    if (-f $absolute_source_dir) {
+      warn "'$absolute_source_dir' is not a directory but a file. Use the 'files' config option instead!";
+      next;
+    }
+    elsif (-d $absolute_source_dir) {
+      dir_diff( $workdir, $bleadpath, $source_dir, $blead_dir, 1 );
+    }
+    else {
+      warn "Explicitly mapped directory '$source_dir' missing from dual lived module source tree!";
+      next;
+    }
+  }
+}
+
+# given a source specification, get the path to an extracted distribution
+sub get_dual_lived_distribution_dir {
+  my $source = shift;
+  usage("Bad source of the dual lived module distribution '$source'")
+    if not defined $source;
+  
+  my $distfile;
+  if (-d $source) {
+    # already extracted or checkout
+    return $source;
+  }
+  elsif (-f $source) {
+    # distribution file
+    $distfile = $source;
+  }
+  elsif ($source =~ m{(?:f|ht)tp://}) {
+    # download file
+    my $disttmpdir = File::Temp::tempdir( CLEANUP => 1 );
+    $source =~ m{/([^/]+)$} or die;
+    my $file = File::Spec->catfile($disttmpdir, $1);
+    if (is_success(getstore( $source, $file ))) {
+      $distfile = $file;
+    }
+    else {
+      die "Could not fetch '$source'";
+    }
+  }
+  else {
+    die "Support for 'Module::Name' or VCS checkout usage not implemented";
+  }
+
+  # extract distribution
+  my $tmpdir = File::Temp::tempdir( CLEANUP => 1 );
+  my $ae = Archive::Extract->new( archive => $distfile );
+  $ae->extract( to => $tmpdir )
+    or die "Failed to extract distribution '$distfile' to temp. dir: " . $ae->error();
+
+  # find the extracted distribution dir
+  opendir my $dh, $tmpdir
+    or die "Could not opendir '$tmpdir': $!";
+  my @stuff = readdir($dh);
+  my @files = grep {-f File::Spec->catfile($tmpdir, $_)} @stuff;
+  my @dirs  = grep {!/^\.\.?$/ and -d File::Spec->catdir($tmpdir, $_)} @stuff;
+  closedir $dh;
+
+  if (@files or @dirs != 1) {
+    die "Failed to find extracted distribution directory in '$tmpdir'. Found ".scalar(@files)." files and ".scalar(@dirs)." dirs";
+  }
+
+  return File::Spec->catdir($tmpdir, shift(@dirs)); 
+}
+
+# find and load the configuration file
+sub get_config {
+  my $source_dir = shift;
+  my $config_file = shift;
+  my $yaml;
+  if (-f $config_file) {
+    $yaml = Parse::CPAN::Meta::LoadFile($config_file);
+  }
+  elsif ( -f File::Spec->catfile($source_dir, $config_file) ) {
+    $yaml = Parse::CPAN::Meta::LoadFile(
+      File::Spec->catfile($source_dir, $config_file)
+    );
+  }
+  elsif ( -f File::Spec->catfile($source_dir, $default_config_file) ) {
+    $yaml = Parse::CPAN::Meta::LoadFile(
+      File::Spec->catfile($source_dir, $default_config_file)
+    );
+  }
+  else {
+    die "Could not find nor load configuration file";
+  }
+
+  $yaml = $yaml->[0] if ref($yaml) eq 'ARRAY';
+
+  return $yaml;
+}
+
+# given the two base dirs and two relative paths, transform a
+# directory mapping into file mappings recursively
+sub dirs_to_filemapping {
+  my $source_base_dir = shift;
+  my $blead_base_dir  = shift;
+  my $source_dir      = shift;
+  my $blead_dir       = shift;
+  my $recursive       = shift;
+  
+  my $full_source_dir = File::Spec->catdir($source_base_dir, $source_dir);
+  my $full_blead_dir  = File::Spec->catdir($blead_base_dir, $blead_dir);
+
+  if (not -d $full_blead_dir) {
+    warn "Specified directory '$blead_dir' could not be found in blead perl source tree!";
+    return();
+  }
+  if (not -d $full_source_dir) {
+    warn "Specified directory '$source_dir' could not be found in dual lived module source tree!";
+    return();
+  }
+
+  my @source_files = $recursive ? recur_get_all_files($full_source_dir) : get_all_files($full_source_dir);
+  if (!@source_files) {
+    warn "Specified source directory '$source_dir' does not contain any files!";
+    return({});
+  }
+
+  my $mapping = {};
+  $mapping->{File::Spec->catfile($source_dir, $_)} = File::Spec->catfile($blead_dir, $_) for @source_files;
+
+  return $mapping;
+}
+
+# get all files in a path with relative paths
+sub recur_get_all_files {
+  my $path = shift;
+
+  my @files;
+  return() if not -d $path;
+  
+  File::Find::find(
+    {
+      preprocess => sub {
+        my @return;
+        FILE: foreach my $file (@_) {
+          foreach my $exclude_regex (@exclude_files) {
+            next FILE if $file =~ $exclude_regex;
+          }
+          push @return, $file;
+        }
+        return(@return);
+      },
+      wanted => sub {
+        foreach my $exclude_regex (@exclude_files) {
+          return if $_ =~ $exclude_regex;
+        }
+        return unless -f $_;
+        s{^\Q$path\E[\\/]*}{};
+        push @files, $_;
+      },
+      no_chdir => 1,
+    },
+    $path
+  );
+  return(@files);
+}
+
+# get all files in a path with relative paths
+sub get_all_files {
+  my $path = shift;
+
+  return() if not -d $path;
+  
+  opendir my $dh, $path or die "Could not open path '$path': $!";
+  my @files = readdir($dh);
+  closedir $dh;
+
+  my @use_files;
+  FILE: foreach my $file (@files) {
+    foreach my $exclude_regex (@exclude_files) {
+      next FILE if $file =~ $exclude_regex;
+    }
+    push @use_files, $file if -f File::Spec->catfile($path, $file);
+  }
+
+  return(@use_files);
+}
+
+# produce the diff of a full directory
+sub dir_diff {
+  my $source_base_dir = shift;
+  my $blead_base_dir  = shift;
+  my $source_dir      = shift;
+  my $blead_dir       = shift;
+  my $recursive       = shift;
+
+  my $map = dirs_to_filemapping( $source_base_dir, $blead_base_dir, $source_dir, $blead_dir, $recursive );
+
+  foreach my $source_file (keys %$map) {
+    my $blead_file = $map->{$source_file};
+    file_diff( $source_base_dir, $blead_base_dir, $source_file, $blead_file );
+  }
+}
+
+# produce the diff of a single file
+sub file_diff {
+  my $source_base_dir = shift;
+  my $blead_base_dir  = shift;
+  my $source_file     = shift;
+  my $blead_file      = shift;
+
+  my $absolute_source_file = File::Spec->catfile($source_base_dir, $source_file);
+  my $absolute_blead_file  = File::Spec->catfile($blead_base_dir, $blead_file);
+  #warn "Diffing '$absolute_source_file' to '$absolute_blead_file'";
+
+  my @cmd = ($diff_cmd, qw(-u -N));
+  if ($reverse) {
+    push @cmd, $absolute_blead_file, $absolute_source_file;
+  }
+  else {
+    push @cmd, $absolute_source_file, $absolute_blead_file;
+  }
+  my $result = `@cmd`;
+  my $blead_prefix = quotemeta($reverse ? '---' : '+++');
+  my $source_prefix = quotemeta($reverse ? '+++' : '---');
+
+  my $patched_filename = $reverse ? $source_file : $blead_file;
+
+  #$result =~ s{^($blead_prefix\s*)(\S+)}{$1 . remove_path_prefix($2, $blead_base_dir)}gme;
+  #$result =~ s{^($source_prefix\s*)(\S+)}{$1 . remove_path_prefix($2, $source_base_dir)}gme;
+  
+  $result =~ s{^($blead_prefix\s*)(\S+)}{$1 . $patched_filename}gme;
+  $result =~ s{^($source_prefix\s*)(\S+)}{$1 . $patched_filename}gme;
+
+  print $result;
+}
+
+# remove a prefix from a path
+sub remove_path_prefix {
+  my $path   = shift;
+  my $prefix = shift;
+  $path =~ s/^\Q$prefix\E//;
+  $path =~ s/^[\/\\]+//;
+  return $path;
+}
+
+
+1;
+__END__
+
+=head1 NAME
+
+App::DualLivedDiff - Diff between the perl core and dual-lived modules' CPAN distributions
+
+=head1 SYNOPSIS
+
+Example: Filter::Simple.
+
+Given a simple YAML file F<.dualLivedDiffConfig> in the current working directory
+or the Filter::Simple CPAN distribution:
+
+  ---
+  files:
+    lib/Filter/Simple.pm: lib/Filter/Simple.pm
+    Changes: lib/Filter/Simple/Changes
+  dirs-flat:
+    t/: lib/Filter/Simple/t/
+  dirs-recursive:
+    t/lib/Filter/Simple/: t/lib/Filter/Simple/
+
+By running the following command, you can get the diff between your blead perl checkout and
+the CPAN distribution:
+
+  dualLivedDiff --dual http://search.cpan.org/CPAN/authors/id/S/SM/SMUELLER/Filter-Simple-0.84.tar.gz --blead $HOME/perl-ssh
+
+=head1 DESCRIPTION
+
+Very early version of a tool to automatically generate diffs/patches between CPAN distributions
+of dual lived Perl modules and the perl core.
+
+=head1 AUTHOR
+
+Steffen Mueller, E<lt>smueller@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Steffen Mueller
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
