10c017c1785f8ce1d26c0cae677493c34ec41c66
FREQUENCY@cpan.org <FREQUENCY@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-03-08 20:36:22 +0000

    Commit a basic bootloader module.
    TODO: figure out how to make an XS module fall back on Perl if necessary

---
+++ Math-Random-ISAAC/ISAAC.pm
@@ -0,0 +1,14 @@
+package Math::Random::ISAAC;
+
+use strict;
+use warnings;
+
+require DynaLoader;
+our @ISA = ('DynaLoader');
+our $VERSION = '0.01';
+
+bootstrap Math::Random::ISAAC $VERSION;
+
+
+
+1;
