33b2a7299ce40855fe376f5000e55e83f05e4aa0
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-03-29 11:05:02 +0000

    Correcting path

--- ORDB-CPANUploads/lib/ORDB/CPANUploads.pm
+++ ORDB-CPANUploads/lib/ORDB/CPANUploads.pm
@@ -169,7 +169,7 @@ Documentation created by L<ORLite::Pod> 0.06.
 
 Bugs should be reported via the CPAN bug tracker at
 
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=ORDB-CPANTesters>
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=ORDB-CPANUploads>
 
 For other issues, contact the author.
 
