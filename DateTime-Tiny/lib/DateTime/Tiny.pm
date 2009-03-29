08f83d2bc3b4361e55ca34721481b0c3f21fcfe0
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-03-29 11:26:27 +0000

    Pre-release nigglies

--- DateTime-Tiny/lib/DateTime/Tiny.pm
+++ DateTime-Tiny/lib/DateTime/Tiny.pm
@@ -385,7 +385,7 @@ L<DateTime>, L<Date::Tiny>, L<Time::Tiny>, L<Config::Tiny>, L<ali.as>
 
 =head1 COPYRIGHT
 
-Copyright 2006 - 2007 Adam Kennedy.
+Copyright 2006 - 2009 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
