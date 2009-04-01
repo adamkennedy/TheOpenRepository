81386af48aea322f93f2ff2d3419d9f403f69c21
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-04-01 16:51:04 +0000

    Starting to sketch out a new ORLite-based JSAN::Client design

---
+++ JSAN-Client2/lib/JSAN/Client2.pm
@@ -0,0 +1,21 @@
+package JSAN::Client2;
+
+=pod
+
+=head1 NAME
+
+JSAN::Client - JSAN Client 2.0 (migrated from Class::DBI to ORLite)
+
+=head1 DESCRIPTION
+
+TO BE COMPLETED
+
+=cut
+
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
