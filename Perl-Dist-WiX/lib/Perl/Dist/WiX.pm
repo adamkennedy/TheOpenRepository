20fe053c0cd5c50a285816cbd5006b482ed4dcb2
csjewell@cpan.org <csjewell@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-04-09 23:56:21 +0000

    Whitespace changes only.

--- Perl-Dist-WiX/lib/Perl/Dist/WiX.pm
+++ Perl-Dist-WiX/lib/Perl/Dist/WiX.pm
@@ -58,7 +58,7 @@ use     English               qw( -no_match_vars             );
 use     List::MoreUtils       qw( any none                   );
 use     Params::Util          qw( _HASH _STRING _INSTANCE    );
 use     Readonly              qw( Readonly                   );
-use	    Storable              qw( retrieve                   );
+use     Storable              qw( retrieve                   );
 use     File::Spec::Functions
   qw( catdir catfile catpath tmpdir splitpath rel2abs curdir );
 use     Archive::Tar     1.42 qw();
