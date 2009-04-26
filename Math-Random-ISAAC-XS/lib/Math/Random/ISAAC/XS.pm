d030753f3583408b349f21d90a92d57b734ab837
FREQUENCY@cpan.org <FREQUENCY@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-04-26 23:26:46 +0000

    - Upgraded version to 1.0.4

--- Math-Random-ISAAC-XS/lib/Math/Random/ISAAC/XS.pm
+++ Math-Random-ISAAC-XS/lib/Math/Random/ISAAC/XS.pm
@@ -20,11 +20,11 @@ Math::Random::ISAAC::XS - C implementation of the ISAAC PRNG Algorithm
 
 =head1 VERSION
 
-Version 1.0.3 ($Id$)
+Version 1.0.4 ($Id$)
 
 =cut
 
-use version; our $VERSION = qv('1.0.3');
+use version; our $VERSION = qv('1.0.4');
 
 =head1 SYNOPSIS
 
