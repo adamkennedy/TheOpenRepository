f0c2521f936e0b63f1fea2033ebb0d8ada0f7aa1
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-04-26 16:21:45 +0000

    More work on the new ORLite JSAN::Client

--- JSAN-Client2/lib/JSAN/Client2.pm
+++ JSAN-Client2/lib/JSAN/Client2.pm
@@ -22,3 +22,29 @@ BEGIN {
 }
 
 1;
+
+=pod
+
+=head1 SUPPORT
+
+Bugs should be reported via the CPAN bug tracker at
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=JSAN-Client2>
+
+For other issues, contact the author.
+
+=head1 AUTHOR
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2009 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
