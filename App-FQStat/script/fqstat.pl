41b86f3e6af2cb423149a4abda06bf88f1126e01
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-05-01 13:48:07 +0000

    no debugging

--- App-FQStat/script/fqstat.pl
+++ App-FQStat/script/fqstat.pl
@@ -15,7 +15,7 @@ use Term::ANSIScreen qw/RESET cls/;
 use Term::ReadKey;
 use Getopt::Long;
 
-use constant DEBUG => 2; # 0=no,1=some,2=lots incl. call tracing,...
+use constant DEBUG => 0; # 0=no,1=some,2=lots incl. call tracing,...
 use constant STARTTIME => Time::HiRes::time();
 
 #####################
