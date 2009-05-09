777187f72b3ef34caf81de90b8a98c8fd550d6e4
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-05-09 20:56:03 +0000

    Ready for release

--- CPAN-Mini-Visit/lib/CPAN/Mini/Visit.pm
+++ CPAN-Mini-Visit/lib/CPAN/Mini/Visit.pm
@@ -6,9 +6,55 @@ package CPAN::Mini::Visit;
 
 CPAN::Mini::Visit - A generalised API version of David Golden's visitcpan
 
+=head1 SYNOPSIS
+
+  CPAN::Mini::Visit->new(
+      minicpan => '/minicpan',
+      acme     => 0,
+      author   => 'ADAMK',
+      callback => sub {
+          print "# archive: $_[0]->{archive}\n";
+          print "# tempdir: $_[0]->{tempdir}\n";
+          print "# dist:    $_[0]->{dist}\n";
+          print "# author:  $_[0]->{author}\n";
+      }
+  )->run;
+  
+  # archive: /minicpan/authors/id/A/AD/ADAMK/Config-Tiny-1.00.tar.gz
+  # tempdir: /tmp/1a4YRmFAJ3/Config-Tiny-1.00
+  # dist:    ADAMK/Config-Tiny-1.00.tar.gz
+  # author:  ADAMK
+
 =head1 DESCRIPTION
 
-To be completed
+L<CPAN::Mini::Extract> has been relatively successful at allowing processes
+to run across the contents (or a subset of the contents) of an entire
+L<minicpan> checkout.
+
+However it has become evident that while it is useful (and theoretically
+optimal from a processing point of view) to maintain an expanded minicpan
+checkout the sheer size of an expanded minicpan is such that it becomes
+an undo burdon to manage, move, copy or even delete a directory tree with
+hundreds of thousands of file totalling in the high single gigabytes in size.
+
+Annoyed by this, David Golden created L<visitcpan> which takes an alternative
+approach of sequentially expanding the tarball of each distribution into a
+temporary directory, do the processing on that distribution, and then delete
+the temporary directory before moving on to the next directory.
+
+This method results in a longer computation time, but with the benefit of
+dramatically reduced system overhead, greater adaptability, and allow for
+easy ad-hoc computations.
+
+This improvement in flexibility turns out to be worth the extra computation
+time in almost all cases.
+
+B<CPAN::Mini::Visit> is a simplified and generalised API-based ersion of 
+David Golden's L<visitcpan> script.
+
+It implements only the process of discovering, iterating and expanding
+archives, before handing off control to an arbitrary callback function
+provided to the constructor.
 
 =cut
 
@@ -26,27 +72,53 @@ use Archive::Extract 0.30 ();
 our $VERSION = '0.01';
 
 use Object::Tiny 1.06 qw{
-	root
+	minicpan
 	authors
 	callback
 	acme
 	author
 };
 
+=pod
+
+=head2 new
+
+Takes a variety of parameters and creates a new visitor object.
+
+The C<minicpan> param should be the root directory of a L<CPAN::Mini>
+download.
+
+The C<callback> param should be a C<CODE> reference that will be called
+for each visit. The first parameter passed to the callback will be a C<HASH>
+reference containing the tarball location in the C<archive> key, the location
+of the temporary directory in the C<tempdir> key, the canonical CPAN
+distribution name in the C<dist> key, and the author id in the C<author> key.
+
+The C<acme> param (true by default) can be set to false to exclude any
+distributions that contain the string "Acme", allowing the visit to ignore
+any of the joke modules.
+
+The C<author> param can be provided to limit the visit to only the modules
+owned by a specific author.
+
+Returns a B<CPAN::Mini::Visit> object, or throws an exception on error.
+
+=cut
+
 sub new {
 	my $class = shift;
 	my $self  = bless { @_ }, $class;
 
 	# Check params
-	unless ( defined _STRING($self->root) and -d $self->root ) {
-		croak("Missing or invalid 'root' param");
+	unless ( defined _STRING($self->minicpan) and -d $self->minicpan ) {
+		croak("Missing or invalid 'minicpan' param");
 	}
 	unless ( _CODELIKE($self->callback) ) {
 		croak("Missing or invalid 'callback' param");
 	}
 
 	# Derive the authors directory
-	$self->{authors} = File::Spec->catdir( $self->root, 'authors', 'id' );
+	$self->{authors} = File::Spec->catdir( $self->minicpan, 'authors', 'id' );
 	unless ( -d $self->authors ) {
 		croak("Authors directory '$self->{authors}' does not exist");
 	}
@@ -54,6 +126,18 @@ sub new {
 
 	return $self;
 }
+
+=pod
+
+=head2 run
+
+The C<run> method executes the visit process, taking no parameters and
+returning true.
+
+Because the object contains no state information, you may call the C<run>
+method multiple times for a single visit object with no ill effects.
+
+=cut
 
 sub run {
 	my $self = shift;
@@ -68,10 +152,10 @@ sub run {
 	# Extract the archive
 	foreach my $file ( @files ) {
 		# Derive the main file properties
-		my $path     = File::Spec->catfile( $self->authors, $file );
-		my $distpath = $file;
-		$distpath =~ s|^[A-Z]/[A-Z][A-Z]/|| or die "Bad distpath for $file";
-		unless ( $distpath =~ /^([A-Z]+)/ ) {
+		my $path = File::Spec->catfile( $self->authors, $file );
+		my $dist = $file;
+		$dist =~ s|^[A-Z]/[A-Z][A-Z]/|| or die "Bad distpath for $file";
+		unless ( $dist =~ /^([A-Z]+)/ ) {
 			die "Bad author for $file";
 		}
 		my $author = "$1";
@@ -88,11 +172,16 @@ sub run {
 		}
 
 		# Change into the directory
-		my $dir   = $archive->extract_path;
-		my $pushd = File::pushd::pushd( $dir );
+		my $tempdir = $archive->extract_path;
+		my $pushd   = File::pushd::pushd( $tempdir );
 
 		# Invoke the callback
-		$self->callback->( $distpath, $path, $dir );
+		$self->callback->( {
+			tempdir => $tempdir,
+			archive => $path,
+			dist    => $dist,
+			author  => $author,
+		} );
 	}
 
 	return 1;
--- CPAN-Mini-Visit/t/02_main.t
+++ CPAN-Mini-Visit/t/02_main.t
@@ -6,16 +6,57 @@ BEGIN {
 	$^W = 1;
 }
 
-use Test::More tests => 2;
+use Test::More tests => 22;
+use File::Spec::Functions ':ALL';
 use CPAN::Mini::Visit ();
 
+my $minicpan = catdir('t', 'minicpan');
+ok( -d $minicpan, "Found root minicpan '$minicpan'" );
+
+my @data  = ();
 my $visit = new_ok( 'CPAN::Mini::Visit' => [
-	root     => 'D:\minicpan',
-	acme     => 0,
-	author   => 'ADAMK',
+	minicpan => $minicpan,
 	callback => sub {
-		print STDERR $_[0] . "\n";
+		push @data, { %{ $_[0] } };
 	},
 ] );
 
+# Kick off the visit
 ok( $visit->run, '->run ok' );
+
+# Do a detailed check of the results
+is( scalar(@data), 3, 'Triggered three visits' );
+ok( -f $data[0]->{archive} );
+is( $data[0]->{author}, 'ADAMK' );
+ok( ! -d $data[0]->{tempdir} );
+is( $data[0]->{dist}, 'ADAMK/CSS-Tiny-1.15.tar.gz' );
+ok( -f $data[1]->{archive} );
+is( $data[1]->{author}, 'ADAMK' );
+is( $data[1]->{dist}, 'ADAMK/Config-Tiny-2.12.tar.gz' );
+ok( ! -d $data[1]->{tempdir} );
+ok( -f $data[2]->{archive} );
+is( $data[2]->{author}, 'ANDYA' );
+is( $data[2]->{dist}, 'ANDYA/HTML-Tiny-1.05.tar.gz' );
+ok( ! -d $data[2]->{tempdir} );
+
+# Check the acme option
+my $acme = new_ok( 'CPAN::Mini::Visit' => [
+	minicpan => $minicpan,
+	acme     => 1,
+	callback => sub {
+		push @data, { %{ $_[0] } };
+	},
+] );
+ok( $acme->run, 'Acme ->run ok' );
+is( scalar(@data), 7, 'Acme triggered four visits' );
+
+# Check the author option
+my $author = new_ok( 'CPAN::Mini::Visit' => [
+	minicpan => $minicpan,
+	author   => 'ADAMK',
+	callback => sub {
+		push @data, { %{ $_[0] } };
+	},
+] );
+ok( $author->run, 'Author ->run ok' );
+is( scalar(@data), 9, 'Author triggered two visits' );
