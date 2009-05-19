89e6e50f93e12b5cb5d6afe33b8e44980d77ce09
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-05-19 04:32:46 +0000

    Allow pass-through of import params

--- ORDB-CPANTesters/lib/ORDB/CPANTesters.pm
+++ ORDB-CPANTesters/lib/ORDB/CPANTesters.pm
@@ -3,19 +3,22 @@ package ORDB::CPANTesters;
 use 5.008005;
 use strict;
 use warnings;
+use Params::Util   0.38 ();
 use ORLite::Mirror 1.12 ();
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 sub import {
-	my $class = shift;
+	my $class  = shift;
+	my $params = Params::Util::_HASH(shift) || {};
+
+	# Pass through any params from above
+	$params->{url}    ||= 'http://devel.cpantesters.org/cpanstats.db.bz2';
+	$params->{maxage} ||= 7 * 24 * 60 * 60; # One week
 
 	# Prevent double-initialisation
 	$class->can('orlite') or
-	ORLite::Mirror->import( {
-		url    => 'http://devel.cpantesters.org/cpanstats.db.bz2',
-		maxage => 7 * 24 * 60 * 60,
-	} );
+	ORLite::Mirror->import( $params );
 
 	return 1;
 }
