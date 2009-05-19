1e9b10ddc8db2ec080c8056ade49e3ccdf2faa46
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-05-19 04:54:10 +0000

    Adding support for import params

--- ORDB-CPANUploads/lib/ORDB/CPANUploads.pm
+++ ORDB-CPANUploads/lib/ORDB/CPANUploads.pm
@@ -3,19 +3,22 @@ package ORDB::CPANUploads;
 use 5.008005;
 use strict;
 use warnings;
+use Params::Util   0.38 ();
 use ORLite::Mirror 1.12 ();
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub import {
 	my $class = shift;
+	my $params = Params::Util::_HASH(shift) || {};
+
+	# Pass through any params from above
+	$params->{url}    ||= 'http://devel.cpantesters.org/uploads/uploads.db.bz2';
+	$params->{maxage} ||= 7 * 24 * 60 * 60; # One week
 
 	# Prevent double-initialisation
 	$class->can('orlite') or
-	ORLite::Mirror->import( {
-		url    => 'http://devel.cpantesters.org/uploads/uploads.db.bz2',
-		maxage => 7 * 24 * 60 * 60,
-	} );
+	ORLite::Mirror->import( $params );
 
 	return 1;
 }
