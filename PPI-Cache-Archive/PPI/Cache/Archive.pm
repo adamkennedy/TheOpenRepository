102c644496d2706b9b8aec984c89a55241ed0996
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-05-26 15:28:16 +0000

    Syncing

--- PPI-Cache-Archive/PPI/Cache/Archive.pm
+++ PPI-Cache-Archive/PPI/Cache/Archive.pm
@@ -2,13 +2,19 @@ package PPI::Cache::Archive;
 
 use 5.008;
 use strict;
-use PPI::Cache ();
+use PPI::Cache            1.203 ();
+use File::Find::Rule       0.30 ();
+use File::Find::Rule::VCS  1.05 ();
+use File::Find::Rule::Perl 1.06 ();
 
 our $VERSION = '0.01';
 
 sub create {
 	my $archive = shift;
 	my $cache   = shift;
+
+	# Scan the cache for documents
+	my @files   = File::Find::Rule->ignore_svn->
 
 }
 
