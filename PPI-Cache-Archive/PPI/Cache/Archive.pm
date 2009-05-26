baeb34e5002b2d9ca226bdfb9fd392ef5a3a7ed7
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-05-26 15:21:48 +0000

    Adding skeleton

---
+++ PPI-Cache-Archive/PPI/Cache/Archive.pm
@@ -0,0 +1,21 @@
+package PPI::Cache::Archive;
+
+use 5.008;
+use strict;
+use PPI::Cache ();
+
+our $VERSION = '0.01';
+
+sub create {
+	my $archive = shift;
+	my $cache   = shift;
+
+}
+
+sub extract {
+	my $archive = shift;
+	my $cache   = shift;
+
+}
+
+1;
