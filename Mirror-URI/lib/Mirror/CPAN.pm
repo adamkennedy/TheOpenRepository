d66e2d03e765c7a75f74c093af974c0e4b9b0958
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-06-01 18:15:13 +0000

    Adding comment copyright to the ones without POD

--- Mirror-URI/lib/Mirror/CPAN.pm
+++ Mirror-URI/lib/Mirror/CPAN.pm
@@ -15,3 +15,11 @@ sub filename {
 }
 
 1;
+
+# Copyright 2007 - 2009 Adam Kennedy.
+#
+# This program is free software; you can redistribute
+# it and/or modify it under the same terms as Perl itself.
+#
+# The full text of the license can be found in the
+# LICENSE file included with this module.
--- Mirror-URI/lib/Mirror/URI.pm
+++ Mirror-URI/lib/Mirror/URI.pm
@@ -274,3 +274,12 @@ sub get_mirrors {
 }
 
 1;
+
+# Copyright 2007 - 2009 Adam Kennedy.
+#
+# This program is free software; you can redistribute
+# it and/or modify it under the same terms as Perl itself.
+#
+# The full text of the license can be found in the
+# LICENSE file included with this module.
+
