91fd0c5ba2b911314ce1f98fd18a7eb2067e65a0
FREQUENCY@cpan.org <FREQUENCY@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-06-09 21:15:45 +0000

    Added a subclass to install libjio

---
+++ Alien-Libjio/inc/My/Builder.pm
@@ -0,0 +1,50 @@
+package My::Builder;
+
+use strict;
+use warnings;
+
+use Module::Build;
+use @ISA = ('Module::Build');
+
+use Config '%Config';
+
+use Cwd ();
+use File::Spec ();
+
+my $ORIG_DIR = Cwd::cwd();
+
+sub _chdir_in {
+  chdir File::Spec->catfile('libjio', 'libjio')
+    or die("Failed to chdir to libjio/libjio: $!");
+}
+sub _chdir_back {
+  chdir $ORIG_DIR or die("Failed to chdir to $ORIG_DIR: $!");
+}
+
+sub ACTION_code {
+  my ($self) = @_;
+
+  my $rc = $self->SUPER::ACTION_code;
+  if ($self->notes('build_libjio')) {
+    _chdir_in();
+    $rc = (system($self->notes('make')) == 0) ? 1 : 0;
+    _chdir_back();
+  }
+
+  return $rc;
+}
+
+sub ACTION_install {
+  my ($self) = @_;
+
+  my $rc = $self->SUPER::ACTION_install;
+  if ($self->notes('build_libjio')) {
+    _chdir_in();
+    $rc = (system($self->notes('make'), 'install') == 0) ? 1 : 0;
+    _chdir_back();
+  }
+
+  return $rc;
+}
+
+1;
