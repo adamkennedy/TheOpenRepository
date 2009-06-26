53dc926e821f3a945a1ea4caefa9fb3de5bfdbe9
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-06-26 15:47:27 +0000

    Updating to new path

--- ORDB-CPANMeta/lib/ORDB/CPANMeta.pm
+++ ORDB-CPANMeta/lib/ORDB/CPANMeta.pm
@@ -3,17 +3,17 @@ package ORDB::CPANMeta;
 use 5.008005;
 use strict;
 use warnings;
-use Params::Util   0.38 ();
-use ORLite::Mirror 1.14 ();
+use Params::Util   1.00 ();
+use ORLite::Mirror 1.15 ();
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub import {
 	my $class = shift;
 	my $params = Params::Util::_HASH(shift) || {};
 
 	# Pass through any params from above
-	$params->{url}    ||= 'http://svn.ali.as/db/cpanmeta.sqlite.gz';
+	$params->{url}    ||= 'http://svn.ali.as/db/cpanmeta.gz';
 	$params->{maxage} ||= 24 * 60 * 60; # One day
 
 	# Prevent double-initialisation
