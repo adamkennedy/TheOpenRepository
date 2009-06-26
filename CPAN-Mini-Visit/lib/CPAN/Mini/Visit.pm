c4c410843cc1797fe80ffcd2eb0787ff7137a653
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-06-26 09:13:40 +0000

    Attempt to repair directories with broken permissions

--- CPAN-Mini-Visit/lib/CPAN/Mini/Visit.pm
+++ CPAN-Mini-Visit/lib/CPAN/Mini/Visit.pm
@@ -69,6 +69,7 @@ use Carp                   'croak';
 use File::Spec        0.80 ();
 use File::Temp        0.21 ();
 use File::pushd       1.00 ();
+use File::chmod       0.31 ();
 use File::Find::Rule  0.27 ();
 use Params::Util      1.00 qw{
 	_HASH _STRING _ARRAYLIKE _CODELIKE _REGEX
@@ -76,7 +77,7 @@ use Params::Util      1.00 qw{
 use Archive::Extract  0.32 ();
 use CPAN::Mini       0.576 ();
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use Object::Tiny 1.06 qw{
 	minicpan
@@ -227,9 +228,12 @@ sub run {
 		}
 
 		# Explicitly ignore some damaging distributions
-		next if $path =~ /Text-SenseClusters/;
-		next if $path =~ /Bio-Affymetrix/;
-		next if $path =~ /Alien-MeCab/;
+		# if we are using Perl extraction
+		unless ( $self->prefer_bin ) {
+			next if $path =~ /Text-SenseClusters/;
+			next if $path =~ /Bio-Affymetrix/;
+			next if $path =~ /Alien-MeCab/;
+		}
 
 		# Extract the archive
 		local $Archive::Extract::WARN       = $self->warnings;
@@ -244,9 +248,18 @@ sub run {
 			next;
 		}
 
+		# If using bin tools, do an additional check for
+		# damaged tarballs with non-executable directories (on unix)
+		my $extract = $archive->extract;
+		unless ( -r $extract and -x $extract ) {
+			# Handle special case where we have screwed up
+			# permissions on the extract directory.
+			# Just assume we have permissions for that.
+			File::chmod::chmod( 0755, $extract );
+		}
+
 		# Change into the directory
-		my $tempdir = $archive->extract_path;
-		my $pushd   = File::pushd::pushd( $tempdir );
+		my $pushd = File::pushd::pushd( $tempdir );
 
 		# Invoke the callback
 		$self->callback->( {
