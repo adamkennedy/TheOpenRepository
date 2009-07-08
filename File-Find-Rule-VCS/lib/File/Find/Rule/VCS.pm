1f0352a9d4dfc283bb7e4b28d4010633fb8ed2e1
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-07-08 11:55:00 +0000

    Updating copyright

--- File-Find-Rule-VCS/lib/File/Find/Rule/VCS.pm
+++ File-Find-Rule-VCS/lib/File/Find/Rule/VCS.pm
@@ -231,7 +231,7 @@ L<http://ali.as/>, L<File::Find::Rule>
 
 =head1 COPYRIGHT
 
-Copyright 2005 - 2008 Adam Kennedy.
+Copyright 2005 - 2009 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
