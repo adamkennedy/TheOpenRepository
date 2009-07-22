0da5a2e98f7ccb06586f4ac367c3464b24fa1778
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-07-22 17:06:38 +0000

    First stab at updating for ORDB::CPANMeta::Generator

--- CPANDB-Generator/examples/cpanmeta.pl
+++ CPANDB-Generator/examples/cpanmeta.pl
@@ -4,7 +4,7 @@ use 5.008;
 use strict;
 use CPANDB::Generator;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 my $cpandb = CPANDB::Generator->new(
 	cpanmeta => 1,
--- CPANDB-Generator/lib/CPANDB/Generator.pm
+++ CPANDB-Generator/lib/CPANDB/Generator.pm
@@ -54,7 +54,7 @@ use Algorithm::Dependency::Weight           ();
 use Algorithm::Dependency::Source::DBI 0.05 ();
 use Algorithm::Dependency::Source::Invert   ();
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 use Object::Tiny 1.06 qw{
 	cpan
