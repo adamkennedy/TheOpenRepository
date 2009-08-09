49d48eee8c9daefa7154411bfd1e6e74a2d4d17a
csjewell@cpan.org <csjewell@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-08-09 10:33:23 +0000

    Adding an incomplete files fragment - the generate routine needs implemented.

---
+++ Perl-Dist-WiX/lib/Perl/Dist/WiX/Fragment/Files.pm
@@ -0,0 +1,37 @@
+package Perl::Dist::WiX::Fragment::Files;
+
+#####################################################################
+# Perl::Dist::WiX::Fragment::Files - A <Fragment> and <DirectoryRef> tag that
+# contains <Directory> or <DirectoryRef> elements, which contain <Component> and 
+# <File> tags.
+#
+# Copyright 2009 Curtis Jewell
+#
+# License is the same as perl. See Wix.pm for details.
+#
+#<<<
+use 5.008001;
+use Moose;
+use vars              qw( $VERSION );
+use WiX3::Exceptions;
+use File::List::Object;
+
+use version; $VERSION = version->new('1.100')->numify;
+
+extends 'WiX3::XML::Fragment';
+
+has files => (
+	is => 'ro',
+	isa	=> 'File::List::Object',
+	required => 1,
+	reader => 'get_files',
+);
+
+# This type of fragment needs regeneration.
+sub regenerate {
+	WiX3::Exception::Unimplemented->throw();
+
+	return;
+}
+
+1;
