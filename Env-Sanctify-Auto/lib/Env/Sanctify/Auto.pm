4c239d521b5cf1cb2d8cd170e9983a5f6e0449b8
FREQUENCY@cpan.org <FREQUENCY@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-08-18 04:46:41 +0000

    - Remove generated files from the repository - Use a simple string version, per David Golden's article: http://www.dagolden.com/index.php/369/version-numbers-should-be-boring/ - No longer use Perl::Critic to test - Fixed test sequence

--- Env-Sanctify-Auto/lib/Env/Sanctify/Auto.pm
+++ Env-Sanctify-Auto/lib/Env/Sanctify/Auto.pm
@@ -17,11 +17,12 @@ Env::Sanctify::Auto - Perl module that cleans up %ENV
 
 =head1 VERSION
 
-Version 1.0.2 ($Id$)
+Version 1.001 ($Id$)
 
 =cut
 
-use version; our $VERSION = qv('1.0.2');
+our $VERSION = '1.001';
+$VERSION = eval $VERSION;
 
 =head1 DESCRIPTION
 
@@ -290,9 +291,9 @@ or know how they work, feel free to send me an e-mail or patch.
 
 Copyright (C) 2009 by Jonathan Yu <frequency@cpan.org>
 
-This package is distributed under the same terms as Perl itself. Please see
-the LICENSE file included in this distribution for full details of these
-terms.
+This package is distributed under the same terms as Perl itself. Please
+see the LICENSE file included in this distribution for full details of
+these terms.
 
 =head1 DISCLAIMER OF WARRANTY
 
