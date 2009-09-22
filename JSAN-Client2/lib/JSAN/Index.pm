1dce7b03b7a4ac420011519367d567caa0cf49eb
nplatonov@cpan.org <nplatonov@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-09-22 17:32:14 +0000

    Copied remained parts of JSAN::Index

--- JSAN-Client2/lib/JSAN/Index.pm
+++ JSAN-Client2/lib/JSAN/Index.pm
@@ -28,6 +28,148 @@ use JSAN::Index::Author       ();
 use JSAN::Index::Release      ();
 use JSAN::Index::Distribution ();
 
+
+#####################################################################
+# Top-level Methods
+
+=pod
+
+=head2 dependency param => $value
+
+The C<dependency> method creates and returns an dependency resolution 
+object that is used by L<JSAN::Client> to schedule which releases to
+install.
+
+If the optional parameter 'build' is true, creates a build-time
+dependency resolve, which will additionally install releases only needed
+for testing.
+
+Returns an L<Algorithm::Dependency> object.
+
+=cut
+
+sub dependency {
+    my $class  = shift;
+    JSAN::Index::Release::_Dependency->new( @_ );
+}
+
+
+
+
+
+#####################################################################
+# Private Classes
+
+# Algorithm::Dependency::Ordered for the ::Release data
+
+package JSAN::Index::Release::_Dependency;
+
+use Params::Util '_INSTANCE';
+
+use base 'Algorithm::Dependency::Ordered';
+
+sub new {
+    my $class  = ref $_[0] ? ref shift : shift;
+    my %params = @_;
+
+    # Apply defaults
+    $params{source} ||= JSAN::Index::Release::_Source->new( %params );
+
+    # Hand off to superclass constructor
+    my $self = $class->SUPER::new( %params )
+        or Carp::croak("Failed to create JSAN::Index::Release::_Dependency object");
+
+    # Save the type for later
+    $self->{build} = !! $params{build};
+
+    $self;
+}
+
+sub build { $_[0]->{build} }
+
+sub schedule {
+    my $self     = shift;
+    my @schedule = @_;
+
+    # Convert things in the schedule from index objects to
+    # release source strings as needed
+    my @cleaned = ();
+    foreach my $item ( @schedule ) {
+        if ( defined $item and ! ref $item and $item =~ /^(?:\w+)(?:\.\w+)*$/ ) {
+            $item = JSAN::Index::Library->retrieve( name => $item );
+        }
+        if ( _INSTANCE($item, 'JSAN::Index::Library') ) {
+            $item = $item->release;
+        }
+        if ( _INSTANCE($item, 'JSAN::Index::Release') ) {
+            $item = $item->source;
+        }
+        push @cleaned, $item;
+    }
+
+    $self->SUPER::schedule(@cleaned);
+}
+
+# Algorithm::Dependency::Source for the ::Release data
+
+package JSAN::Index::Release::_Source;
+
+use Algorithm::Dependency::Item ();
+use base 'Algorithm::Dependency::Source';
+
+sub new {
+    my $class  = ref $_[0] ? ref shift : shift;
+    my %params = @_;
+
+    # Create the basic object
+    my $self = $class->SUPER::new();
+
+    # Set the methods to use
+    $self->{requires_releases} = 1;
+    if ( $params{build} ) {
+        $self->{build_requires_releases} = 1;
+    }
+
+    $self;
+}
+
+sub _load_item_list {
+    my $self = shift;
+
+    ### FIXME: This is crudely effective, but a little innefficient.
+    ###        Later, we should be able to determine which subset of
+    ###        these can never be called, and leave them out of the list.
+
+    # Get every single release in the index
+    my @releases = JSAN::Index::Release->retrieve_all;
+
+    # Wrap the releases in the Adapter objects
+    my @items  = ();
+    foreach my $release ( @releases ) {
+        my $id      = $release->source;
+
+        # Get the list of dependencies
+        my @depends = ();
+        if ( $self->{requires_releases} ) {
+            push @depends, $release->requires_releases;
+        }
+        if ( $self->{build_requires_releases} ) {
+            push @depends, $release->build_requires_releases;
+        }
+
+        # Convert to a distinct source list
+        my %seen = ();
+        @depends = grep { ! $seen{$_} } map { $_->source } @depends;
+
+        # Add the dependency
+        my $item = Algorithm::Dependency::Item->new( $id => @depends )
+            or die "Failed to create Algorithm::Dependency::Item";
+        push @items, $item;
+    }
+
+    \@items;
+}
+
 1;
 
 __END__
