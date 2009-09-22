0ac4d69d2e857172d6d3c26ef31c42cc0465e49d
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-09-22 12:52:42 +0000

    Adding skeleton for ORDB::JSAN

---
+++ ORDB-JSAN/lib/ORDB/JSAN.pm
@@ -0,0 +1,26 @@
+package ORDB::JSAN;
+
+use 5.008005;
+use strict;
+use warnings;
+use Params::Util   1.00 ();
+use ORLite::Mirror 1.15 ();
+
+our $VERSION = '0.01';
+
+sub import {
+	my $class  = shift;
+	my $params = Params::Util::_HASH(shift) || {};
+
+	# Pass through any params from above
+	$params->{url}          ||= 'http://openjsan.org/index.sqlite';
+	$params->{maxage}       ||= 24 * 60 * 60; # One day
+
+	# Prevent double-initialisation
+	$class->can('orlite') or
+	ORLite::Mirror->import( $params );
+
+	return 1;
+}
+
+1;
