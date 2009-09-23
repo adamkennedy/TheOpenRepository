480ead867b69060ce4ddc4d40f5deefd504497f6
nplatonov@cpan.org <nplatonov@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-09-23 08:12:21 +0000

    Copied pod docs from Client-one's Release

--- JSAN-Client2/lib/JSAN/Index/Release.pm
+++ JSAN-Client2/lib/JSAN/Index/Release.pm
@@ -428,6 +428,184 @@ sub version {
 
 __END__
 
+
+=pod
+
+=head1 NAME
+
+JSAN::Index::Release - A JavaScript Archive Network (JSAN) Release
+
+=head1 DESCRIPTION
+
+This class provides objects for a single release of a distribution by an author
+
+=head1 METHODS
+
+In addition to the general methods provided by L<Class::DBI>, it has the
+following methods
+
+=head2 id
+
+The C<id> accessor returns the unique identifier for the release (an integer)
+
+=head2 source
+
+The C<source> access returns the root-relative path within a JSAN mirror
+that the package can be found at.
+
+=head2 distribution
+
+The C<distribution> method returns the L<JSAN::Index::Distribution> for
+the distribution that this release is of.
+
+=head2 author
+
+The C<author> method returns the L<JSAN::Index::Author> for the JSAN author
+that uploaded the release.
+
+=head2 version
+
+The C<version> accessor returns the version of the release.
+
+=head2 created
+
+The C<created> accessor returns the time that the release was received and
+first indexed by the JSAN upload server.
+
+Returns an integer in unix epoch time.
+
+=head2 created_string
+
+The C<created_string> method returns the time that the release was recieved
+and first indexed by the JSAN upload server.
+
+Is equivalent to C<scalar localtime $object-E<gt>created>.
+
+Returns a time as a localtime string.
+
+=head2 doc
+
+The C<doc> accessor returns the root-relative location of the documentation
+for this release on the L<http://openjsan.org/> website.
+
+=head2 meta
+
+The C<meta> accessor returns the actual content of the C<META.yml> file that
+came with the distribution.
+
+=head2 meta_data
+
+The C<meta_data> method loads and deserialises the META.yml content
+contained in the index (and returned by the C<meta> method above).
+
+=head2 checksum
+
+The C<checksum> accessor returns the MD5 checksum for the release tarball.
+
+=head2 latest
+
+The C<latest> accessor returns a boolean flag indicating if the release is
+the most recent release of the distribution.
+
+=head2 requires
+
+The C<requires> method finds the set of run-time library dependencies for
+this release, as identified in the META.yml data contained in the index.
+
+Returns a reference to a HASH where the key is the name of a library as
+a string, and the value is the version for the dependency (or zero if the
+dependency is not for a specific version).
+
+=head2 requires_libraries
+
+The C<requires_libraries> method returns a list of the C<JSAN::Index::Library>
+dependencies as identified by the META.yml file for the release.
+
+=head2 requires_releases
+
+The C<requires_releases> method returns a list of the C<JSAN::Index::Release>
+dependencies based on the dependencies specified in the META.yml file for the
+release.
+
+=head2 build_requires
+
+The C<build_requires> method finds the set of build-time library
+dependencies for this release, as identified in the META.yml data contained
+in the index.
+
+Returns a reference to a HASH where the key is the name of a library as
+a string, and the value is the version for the dependency (or zero if the
+dependency is not for a specific version).
+
+=head2 build_requires_libraries
+
+The C<build_requires_libraries> method returns a list of the build-time
+C<JSAN::Index::Library> dependencies as identified by the META.yml file
+for the release.
+
+=head2 build_requires_releases
+
+The C<requires_releases> method returns a list of the C<JSAN::Index::Release>
+build-time depedencies based on those specified in the META.yml file for the
+release.
+
+=head2 file_path
+
+The C<file_path> method returns the location on the local disk where
+the release tarball should be, if mirrored.
+
+=head2 mirror
+
+The C<mirror> method fetches the tarball from your JSAN currently configured
+JSAN mirror as determined by L<JSAN::Transport> (if not already cached).
+
+Returns a file path to the tarball on the local machine, or may emit an
+exception thrown by the underlying L<JSAN::Transport> functions.
+
+=head2 file_mirrored
+
+The C<file_mirrored> method checks to see if the release tarball has previously
+been downloaded to the local mirror.
+
+Returns true if the file exists in the local mirror, or false if not.
+
+=head2 archive
+
+The C<archive> method returns the release as an in-memory archive.
+Depending on the type, this should be either a L<Archive::Tar> or an
+L<Archive::Zip> object.
+
+=head1 SUPPORT
+
+Bugs should be reported via the CPAN bug tracker at
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=JSAN-Client>
+
+For other issues, contact the author.
+
+=head1 AUTHOR
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+L<JSAN::Index>, L<JSAN::Shell>, L<http://openjsan.org>
+
+=head1 COPYRIGHT
+
+Copyright 2005 - 2007 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
+
+
+
 =head1 NAME
 
 JSAN::Index::Release - JSAN::Index class for the release table
