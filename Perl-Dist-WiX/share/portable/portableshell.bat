c704df485997c57ca76286fc1d85e26e9a39054e
csjewell@cpan.org <csjewell@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-09-23 15:29:09 +0000

    Adding files used only in portable.

---
+++ Perl-Dist-WiX/share/portable/portableshell.bat
@@ -0,0 +1,11 @@
+@echo off
+set PATH=%~dp0\perl\bin;%~dp0\c\bin;%PATH%
+echo ----------------------------------------------
+echo  Welcome to Strawberry Perl Portable Edition!
+echo  * URL - http://www.strawberryperl.com/ 
+echo  * see README.portable.TXT for more info
+echo ----------------------------------------------
+perl -e "printf("""Perl executable: %%s\nPerl version   : %%vd\n""", $^X, $^V)" 2>nul
+if ERRORLEVEL==1 echo.&echo FATAL ERROR: 'perl' does not work; check if your strawberry pack is complete!
+echo.
+cmd
