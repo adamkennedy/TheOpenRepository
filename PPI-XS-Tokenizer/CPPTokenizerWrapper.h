d61572aff5db903f99e2fc1124bf24a6f04073b0
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-09-27 15:25:21 +0000

    docs

--- PPI-XS-Tokenizer/CPPTokenizerWrapper.h
+++ PPI-XS-Tokenizer/CPPTokenizerWrapper.h
@@ -39,8 +39,8 @@ namespace PPITokenizer {
       static const char* fgTokenClasses[43];
       static const int fgSpecialToken[43];
 
-      static SV* S_newPerlObject(const char* className);
-      static char* S_stealPV(SV* sv, STRLEN& len);
+      static SV* S_newPerlObject(const char* className); /// Create a new Perl (hash) object blessed into className
+      static char* S_stealPV(SV* sv, STRLEN& len); /// Steals ownership of the PV (string) contents of the scalar
       static void S_makeSections(ExtendedToken* token, HV* objHash);
       static void S_handleHereDoc(ExtendedToken* token, HV* objHash);
       static const char* S_getQuoteOperatorString(Token* token, unsigned long* length);
