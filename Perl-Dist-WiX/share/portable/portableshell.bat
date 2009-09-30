1f30b7bb09238f1b28257199ff1e601d69993c4e
kmx@cpan.org <kmx@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-09-30 07:58:05 +0000

    Portable README + portableshell.bat (added TERM variable + README made visually similar to non-portable README)

--- Perl-Dist-WiX/share/portable/portableshell.bat
+++ Perl-Dist-WiX/share/portable/portableshell.bat
@@ -1,5 +1,6 @@
 @echo off
 set PATH=%~dp0\perl\bin;%~dp0\c\bin;%PATH%
+set TERM=dumb
 echo ----------------------------------------------
 echo  Welcome to Strawberry Perl Portable Edition!
 echo  * URL - http://www.strawberryperl.com/ 
