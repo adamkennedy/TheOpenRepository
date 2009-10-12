e6dd64715b70fe4b9e073642e6cc7c4cf39006c5
csjewell@cpan.org <csjewell@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-10-12 05:43:40 +0000

    Taking out reordering for now as per e-mail/IRC convo with Alias.

--- Perl-Dist-WiX/lib/Perl/Dist/WiX/Asset/Perl.pm
+++ Perl-Dist-WiX/lib/Perl/Dist/WiX/Asset/Perl.pm
@@ -158,16 +158,18 @@ EOF
 		$self->_make(qw/install UNINST=1/);
 	} ## end SCOPE:
 
-	$self->_trace_line( 2, "Copying sitecustomize.pl...\n" );
-	$self->_copy(
-		catfile(
-			$self->_get_wix_dist_dir,
-			qw(default perl site lib sitecustomize.pl)
-		),
-		catfile(
-			$self->_get_image_dir, qw(perl site lib sitecustomize.pl)
-		),
-	);
+# TODO: Reactivate once the toolchain and p5p people let me
+# do the reordering.
+#	$self->_trace_line( 2, "Copying sitecustomize.pl...\n" );
+#	$self->_copy(
+#		catfile(
+#			$self->_get_wix_dist_dir,
+#			qw(default perl site lib sitecustomize.pl)
+#		),
+#		catfile(
+#			$self->_get_image_dir, qw(perl site lib sitecustomize.pl)
+#		),
+#	);
 
 	my $fl_lic = File::List::Object->new()
 	  ->readdir( catdir( $self->_get_image_dir, 'licenses', 'perl' ) );
