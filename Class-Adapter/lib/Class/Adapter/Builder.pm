b9b30dabc70dad536ae8b31491b479c5afff3639
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2009-11-23 11:49:36 +0000

    Updating copyright

--- Class-Adapter/lib/Class/Adapter/Builder.pm
+++ Class-Adapter/lib/Class/Adapter/Builder.pm
@@ -457,7 +457,7 @@ L<Class::Adapter>, L<Class::Adapter::Clear>
 
 =head1 COPYRIGHT
 
-Copyright 2005 - 2008 Adam Kennedy.
+Copyright 2005 - 2009 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
--- Class-Adapter/lib/Class/Adapter/Clear.pm
+++ Class-Adapter/lib/Class/Adapter/Clear.pm
@@ -164,7 +164,7 @@ L<Class::Adapter>, L<Class::Adapter::Builder>
 
 =head1 COPYRIGHT
 
-Copyright 2005 - 2008 Adam Kennedy.
+Copyright 2005 - 2009 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
