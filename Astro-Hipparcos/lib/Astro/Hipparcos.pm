b2dbe0342d4e3882109cb72fb5e35d35e857e82c
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-01-02 21:31:46 +0000

    Astro::Hipparcos 0.08

--- Astro-Hipparcos/lib/Astro/Hipparcos.pm
+++ Astro-Hipparcos/lib/Astro/Hipparcos.pm
@@ -6,7 +6,7 @@ use warnings;
 use Carp 'croak';
 use File::Spec;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use constant LINE_LENGTH => 451; # I wish they were using a modern file format.
 
