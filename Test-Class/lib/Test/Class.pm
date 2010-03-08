2523f1bef4b99e0278f422c5e566191b051a7e70
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-03-08 06:05:30 +0000

    Updating copyright

--- Test-Class/lib/Test/Class.pm
+++ Test-Class/lib/Test/Class.pm
@@ -1679,6 +1679,6 @@ A very simple unit testing framework.
 
 =head1 LICENCE
 
-Copyright 2002-2009 Adrian Howard, All Rights Reserved.
+Copyright 2002-2010 Adrian Howard, All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
