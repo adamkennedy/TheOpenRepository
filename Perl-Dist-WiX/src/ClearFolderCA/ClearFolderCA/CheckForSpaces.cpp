982f5b8a6a4c0a2b5915a0f2c488caa39f06cdaf
csjewell@cpan.org <csjewell@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-03-16 05:15:54 +0000

    First pass at adding the CheckForSpaces custom action in.

---
+++ Perl-Dist-WiX/src/ClearFolderCA/ClearFolderCA/CheckForSpaces.cpp
@@ -0,0 +1,124 @@
+// ClearFolderCA.cpp : Defines the Clear Folder custom action.
+//
+// Copyright (c) Curtis Jewell 2009.
+//
+// This code is free software; you can redistribute it and/or modify it
+// under the same terms as Perl itself.
+
+/* 
+<CustomAction Id='CA_ClearFolder' BinaryKey='B_ClearFolder' DllEntry='ClearFolder' />
+
+<InstallExecuteSequence>
+  <Custom Action='CA_ClearFolder' Before='InstallInitialize'>REMOVE="ALL"</Custom>
+</InstallExecuteSequence>
+
+<Binary Id='B_ClearFolder' SourceFile='share\ClearFolderCA.dll' />
+*/
+
+#include "stdafx.h"
+
+// Helper macros for error checking.
+
+#define MSI_OK(x) \
+	if (ERROR_SUCCESS != x) { \
+		return x; \
+	}
+ 
+#define MSI_OK_FREE(x, y) \
+	if (ERROR_SUCCESS != x) { \
+		free(y); \
+		return x; \
+	}
+
+#define MSI_OK_FREE_2(x, y, z) \
+	if (ERROR_SUCCESS != x) { \
+		free(y); \
+		free(z); \
+		return x; \
+	}
+
+#define HANDLE_OK(x) \
+	if (NULL == x) { \
+		return ERROR_INSTALL_FAILURE; \
+	}
+
+UINT __stdcall CheckForSpaces(
+	MSIHANDLE hModule) // Handle of MSI being installed. [in]
+	                   // Passed to most other routines.
+{
+	TCHAR sInstallDir[MAX_PATH + 1];
+	TCHAR sNum[11];
+	DWORD dwPropLength;
+	UINT uiAnswer;
+
+	// Get directory to search.
+	dwPropLength = 10; 
+	uiAnswer = ::MsiGetProperty(hModule, TEXT("WIXUI_INSTALLDIR_VALID"), sNum, &dwPropLength); 
+	if (ERROR_MORE_DATA == uiAnswer) {
+		uiAnswer = ERROR_SUCCESS;
+	}
+	MSI_OK(uiAnswer)
+
+	if (0 != _tcscmp(sNum, _T("1"))) {
+		return ERROR_SUCCESS;
+	}
+
+	// Get directory to check.
+	dwPropLength = MAX_PATH; 
+	uiAnswer = ::MsiGetProperty(hModule, TEXT("INSTALLDIR"), sInstallDir, &dwPropLength); 
+	MSI_OK(uiAnswer)
+
+	if (NULL != _tcschr(sInstallDir, _T(' '))) {
+		return ::MsiSetProperty(hModule, TEXT("WIXUI_INSTALLDIR_VALID"), TEXT("-2"));
+	}
+
+	// Check the "long name" of that directory.
+	TCHAR sInstallDirWork[MAX_PATH + 1];
+	TCHAR sInstallDirLong[MAX_PATH + 1];
+	_tcscpy_s(sInstallDir, MAX_PATH, sInstallDirWork);
+	DWORD dwAnswer = ::GetLongPathName(sInstallDirWork, sInstallDirLong, MAX_PATH);
+
+	TCHAR* pcSlash;
+	DWORD dwError;
+	while (0 == dwAnswer) {
+
+		dwError = ::GetLastError();
+
+		// Keep working backwards until we get it right, or until there are no more backslashes.
+		pcSlash = _tcsrchr(sInstallDirWork, _T('\\'));
+		if (NULL == pcSlash) 
+			break;
+
+		*pcSlash = _T('\0');
+		dwAnswer = ::GetLongPathName(sInstallDirWork, sInstallDirLong, MAX_PATH);
+	}
+
+	if (NULL != _tcschr(sInstallDir, _T(' '))) {
+		return ::MsiSetProperty(hModule, TEXT("WIXUI_INSTALLDIR_VALID"), TEXT("-2"));
+	}
+
+	return ::MsiSetProperty(hModule, TEXT("WIXUI_INSTALLDIR_VALID"), TEXT("1"));
+
+
+/*
+	_tcscpy_s(sSiteDirectory, MAX_PATH, sInstallDirectory);
+	_tcscat_s(sSiteDirectory, MAX_PATH, TEXT("perl\\site\\"));
+
+	// Get component to add files to delete to.
+	uiAnswer = GetComponent(hModule, sPerlModuleID);
+	MSI_OK(uiAnswer)
+	
+	TCHAR sSiteDirID[51];
+	_tcscpy_s(sSiteDirID, 50, TEXT("D_PerlSite"));
+	if (_tcscmp(sPerlModuleID, TEXT("")) != 0) {
+		_tcscat_s(sSiteDirID, 50, TEXT("."));
+		_tcscat_s(sSiteDirID, 50, sPerlModuleID);
+	}
+
+	// Start getting files to delete (recursive)
+	bool bDeleteEntryCreated = false;
+	uiAnswer = AddDirectory(
+		hModule, sSiteDirectory, sSiteDirID, true, bDeleteEntryCreated);
+*/
+    return uiAnswer;
+}
