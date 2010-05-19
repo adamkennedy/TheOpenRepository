0e91d5e7cc0d82abcf489b06c7094adccaa21a74
csjewell@cpan.org <csjewell@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-05-19 06:36:00 +0000

    Ignore Win32::Exe's new warning about no resource section.

--- Perl-Dist-WiX/lib/Perl/Dist/WiX/Fragment/Files.pm
+++ Perl-Dist-WiX/lib/Perl/Dist/WiX/Fragment/Files.pm
@@ -478,8 +478,13 @@ sub _add_file_component {
 		# when we create the tag.
 		my $language;
 		my $exe = Win32::Exe->new($file);
-		my $vi  = $exe->version_info();
-
+		my $vi;
+		{
+			# Win32::Exe prints an annoying warning here. Ignore it.
+			local $SIG{__WARN__} = sub {};
+			$vi = $exe->version_info();
+		}
+		
 		if ( defined $vi ) {
 			$vi->get('OriginalFilename'); # To load the variable used below.
 			$language = hex substr $vi->{'cur_trans'}, 0, 4;
