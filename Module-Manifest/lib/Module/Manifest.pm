62f9c38eba7d72d41bf98616dec563ae588d5bd2
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-06-15 08:36:06 +0000

    Updating copyright

--- Module-Manifest/lib/Module/Manifest.pm
+++ Module-Manifest/lib/Module/Manifest.pm
@@ -455,7 +455,7 @@ L<ExtUtils::Manifest>
 
 =head1 COPYRIGHT
 
-Copyright 2006-2009, Adam Kennedy
+Copyright 2006 - 2010 Adam Kennedy
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
