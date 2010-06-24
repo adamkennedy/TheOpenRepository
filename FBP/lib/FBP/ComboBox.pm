52f648ab130fdf4b62ea41df4f19e7f6f842dc26
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-06-24 05:03:50 +0000

    - Added FBP::ComboBox::value attribute 	- Added FBP::ComboBox::style attribute

--- FBP/lib/FBP/ComboBox.pm
+++ FBP/lib/FBP/ComboBox.pm
@@ -7,4 +7,16 @@ our $VERSION = '0.04';
 extends 'FBP::Window';
 with    'FBP::Control';
 
+has value => (
+	is       => 'ro',
+	isa      => 'Str',
+	required => 1,
+	default  => '',
+);
+
+has style => (
+	is  => 'ro',
+	isa => 'Str',
+);
+	
 1;
