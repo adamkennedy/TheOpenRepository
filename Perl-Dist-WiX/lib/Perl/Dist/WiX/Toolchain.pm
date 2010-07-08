4438183468aa0926bb2d0a6cb588149998dbe24c
csjewell@cpan.org <csjewell@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-07-08 19:32:03 +0000

    Reorder so that Test::Script and its prereqs come before File::Which.

--- Perl-Dist-WiX/lib/Perl/Dist/WiX/Toolchain.pm
+++ Perl-Dist-WiX/lib/Perl/Dist/WiX/Toolchain.pm
@@ -228,12 +228,11 @@ sub _modules_build {
 		  Win32::WinError
 		  Win32API::Registry
 		  Win32::TieRegistry
+		  Probe::Perl
+		  Test::Script		  
 		  File::Which
 		  File::HomeDir
 		  IPC::Run3
-		  Probe::Perl
-		  Test::Script
-		  File::Which
 		  Archive::Zip
 		  Package::Constants
 		  IO::String
