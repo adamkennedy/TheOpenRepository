7d3d64a67c346be59c39e7f3bf4002ad8c056e8e
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-07-20 07:12:45 +0000

    Tweak author statement

--- Text-FindIndent/lib/Text/FindIndent.pm
+++ Text-FindIndent/lib/Text/FindIndent.pm
@@ -634,13 +634,15 @@ For other issues, contact the author.
 
 =head1 AUTHOR
 
-Adam Kennedy E<lt>adamk@cpan.orgE<gt>, Steffen Mueller E<lt>smueller@cpan.orgE<gt>
+Steffen Mueller E<lt>smueller@cpan.orgE<gt>
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
 
 =head1 COPYRIGHT
 
-Copyright 2008-2009 Adam Kennedy,
+Copyright 2008 - 2009 Steffen Mueller.
 
-Copyright 2008-2009 Steffen Mueller.
+Copyright 2008 - 2009 Adam Kennedy,
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
