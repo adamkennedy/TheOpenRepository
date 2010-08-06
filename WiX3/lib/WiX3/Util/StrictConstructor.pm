0b3bb56823316bdf75c826ecb17573a3fe312dae
csjewell@cpan.org <csjewell@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-08-06 13:07:00 +0000

    Updating StrictConstructor code in WiX3.

--- WiX3/lib/WiX3/Util/StrictConstructor.pm
+++ WiX3/lib/WiX3/Util/StrictConstructor.pm
@@ -1,38 +1,23 @@
 package                                # Hide from PAUSE.
   WiX3::Util::StrictConstructor;
-
+# Corresponds to MooseX::StrictConstructor.
+  
 use 5.008001;
-use Moose 0.74 qw();
+use Moose 0.94 qw();
 use Moose::Exporter;
 use Moose::Util::MetaRole;
 use WiX3::Util::Role::StrictConstructor;
 use WiX3::Util::Role::StrictConstructorMeta;
 
-our $VERSION = '0.009100';
+our $VERSION = '0.010';
 $VERSION =~ s/_//ms;
 
-Moose::Exporter->setup_import_methods();
-
-sub init_meta {
-	shift;
-	my %p = @_;
-
-	Moose->init_meta(%p);
-
-	my $caller = $p{for_class};
-
-	Moose::Util::MetaRole::apply_metaclass_roles(
-		for_class => $caller,
-		constructor_class_roles =>
-		  ['WiX3::Util::Role::StrictConstructorMeta'],
-	);
-
-	Moose::Util::MetaRole::apply_base_class_roles(
-		for_class => $caller,
-		roles     => ['WiX3::Util::Role::StrictConstructor'],
-	);
-
-	return $caller->meta();
-} ## end sub init_meta
+Moose::Exporter->setup_import_methods(
+    class_metaroles => {
+        constructor =>
+            ['WiX3::Util::Role::StrictConstructorMeta']
+    },
+    base_class_roles => ['WiX3::Util::Role::StrictConstructor'],
+);
 
 1;
