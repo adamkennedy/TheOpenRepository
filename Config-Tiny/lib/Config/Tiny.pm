8765100a7949e11f187e54c6a9307d78452fac36
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-09-03 01:26:03 +0000

    Updated copyright year

--- Config-Tiny/lib/Config/Tiny.pm
+++ Config-Tiny/lib/Config/Tiny.pm
@@ -268,7 +268,7 @@ L<Config::Simple>, L<Config::General>, L<ali.as>
 
 =head1 COPYRIGHT
 
-Copyright 2002 - 2007 Adam Kennedy.
+Copyright 2002 - 2010 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
