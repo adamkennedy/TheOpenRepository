d2c5ca8d5c52c3693a7b97abf2123a367cfece5a
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-09-10 12:22:05 +0000

    File::ShareDir::PAR 0.06

--- File-ShareDir-PAR/lib/File/ShareDir/PAR.pm
+++ File-ShareDir-PAR/lib/File/ShareDir/PAR.pm
@@ -86,7 +86,7 @@ use File::Path       ();
 
 use vars qw{$VERSION @EXPORT_OK %EXPORT_TAGS %CLEANUP_DIRS};
 BEGIN {
-  $VERSION     = '0.05';
+  $VERSION     = '0.06';
   @EXPORT_OK   = qw{dist_dir dist_file module_dir module_file class_file};
   %EXPORT_TAGS = (
     ALL => [ @EXPORT_OK ],
