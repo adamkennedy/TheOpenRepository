fa71c535c957e90b5811872e5221088636fd6790
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-09-20 15:38:12 +0000

    update copyright

--- Number-WithError/lib/Number/WithError.pm
+++ Number-WithError/lib/Number/WithError.pm
@@ -1542,7 +1542,7 @@ Steffen Mueller E<lt>modules at steffen-mueller dot netE<gt>, L<http://steffen-m
 
 =head1 COPYRIGHT
 
-Copyright 2006 Steffen Mueller.
+Copyright 2006-2010 Steffen Mueller.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
