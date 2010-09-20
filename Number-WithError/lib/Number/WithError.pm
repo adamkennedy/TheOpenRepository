a4f7499e2fd71abc5b4e65a8751d7fceab955a65
smueller@cpan.org <smueller@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-09-20 15:44:46 +0000

    version bump

--- Number-WithError/lib/Number/WithError.pm
+++ Number-WithError/lib/Number/WithError.pm
@@ -6,7 +6,7 @@ use warnings;
 use Params::Util qw/_ARRAY _INSTANCE _ARRAY0/;
 use prefork 'Math::BigFloat';
 
-our $VERSION = '0.08';
+our $VERSION = '1.00';
 
 use base 'Exporter';
 our @EXPORT_OK = qw(
