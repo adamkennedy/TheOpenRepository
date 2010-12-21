32692702e3af4cf7dbb1d93042b160490487b2c8
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-12-21 05:06:46 +0000

    Adding a basic display method for Material

--- SDL-Tutorial-3DWorld/lib/SDL/Tutorial/3DWorld/Material.pm
+++ SDL-Tutorial-3DWorld/lib/SDL/Tutorial/3DWorld/Material.pm
@@ -3,6 +3,7 @@ package SDL::Tutorial::3DWorld::Material;
 use 5.008;
 use strict;
 use warnings;
+use SDL::Tutorial::3DWorld::OpenGL ();
 
 our $VERSION = '0.21';
 
@@ -15,7 +16,14 @@ our $VERSION = '0.21';
 
 sub new {
 	my $class = shift;
-	my $self  = bless { @_ }, $class;
+	my $self  = bless {
+		# Default elements
+		ambient   => [ 0.2, 0.2, 0.2, 1.0 ],
+		diffuse   => [ 0.8, 0.8, 0.8, 1.0 ],
+		specular  => [ 0.0, 0.0, 0.0, 0.0 ],
+		shininess => 8,
+		@_,
+	}, $class;
 	return $self;
 }
 
@@ -50,7 +58,32 @@ sub init {
 	return 1;
 }
 
+# Apply the material to the current OpenGL context
 sub display {
+	my $self = shift;
+
+	# Apply the material properties
+	OpenGL::glMaterialfv_p(
+		OpenGL::GL_FRONT_AND_BACK,
+		OpenGL::GL_AMBIENT,
+		@{ $self->{ambient} },
+	);
+	OpenGL::glMaterialfv_p(
+		OpenGL::GL_FRONT_AND_BACK,
+		OpenGL::GL_DIFFUSE,
+		@{ $self->{diffuse} },
+	);
+	OpenGL::glMaterialfv_p(
+		OpenGL::GL_FRONT_AND_BACK,
+		OpenGL::GL_SPECULAR,
+		@{ $self->{specular} },
+	);
+	OpenGL::glMaterialf(
+		OpenGL::GL_FRONT_AND_BACK,
+		OpenGL::GL_SHININESS,
+		$self->{shininess},
+	);
+
 	return 1;
 }
 
