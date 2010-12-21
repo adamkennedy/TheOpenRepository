f53bfbe963c8db4becdd4041691a12037488981e
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-12-21 06:45:45 +0000

    Adding default for, and documenting, illumination setting

--- SDL-Tutorial-3DWorld/lib/SDL/Tutorial/3DWorld/Material.pm
+++ SDL-Tutorial-3DWorld/lib/SDL/Tutorial/3DWorld/Material.pm
@@ -18,10 +18,11 @@ sub new {
 	my $class = shift;
 	my $self  = bless {
 		# Default elements
-		ambient   => [ 0.2, 0.2, 0.2, 1.0 ],
-		diffuse   => [ 0.8, 0.8, 0.8, 1.0 ],
-		specular  => [ 0.0, 0.0, 0.0, 0.0 ],
-		shininess => 8,
+		ambient      => [ 0.2, 0.2, 0.2, 1.0 ],
+		diffuse      => [ 0.8, 0.8, 0.8, 1.0 ],
+		specular     => [ 0.0, 0.0, 0.0, 0.0 ],
+		shininess    => 8,
+		illumination => 1,
 		@_,
 	}, $class;
 	return $self;
@@ -43,6 +44,18 @@ sub shininess {
 	$_[0]->{shininess};
 }
 
+# Illumination mode
+# 0. Color on and Ambient off
+# 1. Color on and Ambient on
+# 2. Highlight on
+# 3. Reflection on and Ray trace on
+# 4. Transparency: Glass on, Reflection: Ray trace on
+# 5. Reflection: Fresnel on and Ray trace on
+# 6. Transparency: Refraction on, Reflection: Fresnel off and Ray trace on
+# 7. Transparency: Refraction on, Reflection: Fresnel on and Ray trace on
+# 8. Reflection on and Ray trace off
+# 9. Transparency: Glass on, Reflection: Ray trace off
+# 10. Casts shadows onto invisible surfaces
 sub illumination {
 	$_[0]->{illumination};
 }
