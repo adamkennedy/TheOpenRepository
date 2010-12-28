16359e7eb76c6e5f49504de12697c03070835e87
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2010-12-28 10:20:50 +0000

    We now draw an axis along with the bounding box

--- SDL-Tutorial-3DWorld/lib/SDL/Tutorial/3DWorld/Actor/Box.pm
+++ SDL-Tutorial-3DWorld/lib/SDL/Tutorial/3DWorld/Actor/Box.pm
@@ -38,9 +38,12 @@ sub new {
 sub init {
 	my $self = shift;
 
-	# Generate the cube display list
-	$self->{list} = OpenGL::List::glpList {
-		$self->compile;
+	# Generate the cube and axis display lists
+	$self->{boxlist} = OpenGL::List::glpList {
+		$self->compile_box;
+	};
+	$self->{axislist} = OpenGL::List::glpList {
+		$self->compile_axis;
 	};
 
 	return 1;
@@ -51,10 +54,23 @@ sub box {
 }
 
 sub display {
-	my $self = shift;
+	my $self     = shift;
+	my $position = $self->{parent}->position;
+	my @box      = $self->{parent}->box or return;
 
-	# Get the full bounding box of our parent
-	my @box = $self->{parent}->box or return;
+	# Translate to the model origin and call the axis display list,
+	# even if the model doesn't have an actual bounding box.
+	# Scale to the bounding box if we have one, or just draw it
+	# plain and 1 metre on a side otherwise.
+	OpenGL::glPushMatrix();
+	OpenGL::glTranslatef( @$position );
+	OpenGL::glScalef(
+		$box[3] - $box[0],
+		$box[4] - $box[1],
+		$box[5] - $box[2],
+	) if @box;
+	OpenGL::glCallList( $self->{axislist} );
+	OpenGL::glPopMatrix();
 
 	# Translate to the negative corner
 	OpenGL::glTranslatef( @box[0..2] );
@@ -64,13 +80,50 @@ sub display {
 		$box[3] - $box[0],
 		$box[4] - $box[1],
 		$box[5] - $box[2],
-	);
+	);		
 
 	# Call the display list to render the cube
-	OpenGL::glCallList( $self->{list} );
+	OpenGL::glCallList( $self->{boxlist} );
 }
 
-sub compile {
+sub compile_axis {
+	my $self = shift;
+
+	# The axis is plain opaque full-bright and ignores lighting
+	OpenGL::glDisable( OpenGL::GL_LIGHTING );
+	OpenGL::glDisable( OpenGL::GL_TEXTURE_2D );
+
+	# Enable line smoothing
+	#OpenGL::glBlendFunc( OpenGL::GL_SRC_ALPHA, OpenGL::GL_ONE_MINUS_SRC_ALPHA );
+	OpenGL::glDisable( OpenGL::GL_BLEND );
+	OpenGL::glDisable( OpenGL::GL_LINE_SMOOTH );
+
+	# Draw the (R)ed X Axis
+	OpenGL::glColor4f( 1.0, 0.0, 0.0, 1.0 );
+	OpenGL::glBegin( OpenGL::GL_LINES );
+	OpenGL::glVertex3f( 0, 0, 0 );
+	OpenGL::glVertex3f( 1.1, 0, 0 );
+	OpenGL::glEnd();
+
+	# Draw the (G)reen Y Axis
+	OpenGL::glColor4f( 0.0, 1.0, 0.0, 1.0 );
+	OpenGL::glBegin( OpenGL::GL_LINES );
+	OpenGL::glVertex3f( 0, 0, 0 );
+	OpenGL::glVertex3f( 0, 1.1, 0 );
+	OpenGL::glEnd();
+
+	# Draw the (B)lue Z Axis
+	OpenGL::glColor4f( 0.0, 0.0, 1.0, 1.0 );
+	OpenGL::glBegin( OpenGL::GL_LINES );
+	OpenGL::glVertex3f( 0, 0, 0 );
+	OpenGL::glVertex3f( 0, 0, 1.1 );
+	OpenGL::glEnd();
+
+	# Revert the light disable
+	OpenGL::glEnable( OpenGL::GL_LIGHTING );
+}
+
+sub compile_box {
 	my $self = shift;
 
 	# The cube is plain opaque full-bright white and ignores lighting
@@ -83,7 +136,7 @@ sub compile {
 	OpenGL::glDisable( OpenGL::GL_LINE_SMOOTH );
 
 	# Draw all the lines in the cube
-	OpenGL::glColor4f( 0.5, 1, 0.50, 1.0 );
+	OpenGL::glColor4f( 0.5, 1.0, 0.50, 1.0 );
 	OpenGL::glBegin( OpenGL::GL_LINES );
 	OpenGL::glVertex3f( 0, 0, 0 ); OpenGL::glVertex3f( 1, 0, 0 );
 	OpenGL::glVertex3f( 0, 0, 0 ); OpenGL::glVertex3f( 0, 1, 0 );
@@ -98,10 +151,6 @@ sub compile {
 	OpenGL::glVertex3f( 1, 0, 1 ); OpenGL::glVertex3f( 1, 1, 1 );
 	OpenGL::glVertex3f( 0, 1, 1 ); OpenGL::glVertex3f( 1, 1, 1 );
 	OpenGL::glEnd();
-
-	# Disable line smoothing
-	OpenGL::glDisable( OpenGL::GL_LINE_SMOOTH );
-	OpenGL::glDisable( OpenGL::GL_BLEND );
 
 	# Revert the light disable
 	OpenGL::glEnable( OpenGL::GL_LIGHTING );
