ea4c7a721d62aa0c3814a16af3c3b6db924a9937
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-01-03 23:01:30 +0000

    Adding the missing Sprite.pm

---
+++ SDL-Tutorial-3DWorld/lib/SDL/Tutorial/3DWorld/Actor/Sprite.pm
@@ -0,0 +1,69 @@
+package SDL::Tutorial::3DWorld::Actor::Sprite;
+
+use 5.008;
+use strict;
+use warnings;
+use SDL::Tutorial::3DWorld           ();
+use SDL::Tutorial::3DWorld::OpenGL   ();
+use SDL::Tutorial::3DWorld::Texture  ();
+use SDL::Tutorial::3DWorld::Material ();
+use SDL::Tutorial::3DWorld::Actor    ();
+
+our $VERSION = '0.29';
+our @ISA     = 'SDL::Tutorial::3DWorld::Actor';
+
+
+
+
+
+######################################################################
+# Constructor and Accessors
+
+sub new {
+	my $class = shift;
+	my $self  = bless {
+		blending => 1,
+		@_,
+	}, $class;
+
+	# Convert the texture to a full material
+	$self->{material} = SDL::Tutorial::3DWorld::Material->new(
+		ambient => [ 1, 1, 1, 0.5 ],
+		diffuse => [ 1, 1, 1, 0.5 ],
+		texture => SDL::Tutorial::3DWorld::Texture->new(
+			file => $self->{texture},
+			tile => 0,
+		),
+	);
+
+	return $self;
+}
+
+sub display {
+	my $self = shift;
+	$self->SUPER::display(@_);
+
+	# Rotate towards the camera
+	OpenGL::glRotatef( 
+		-SDL::Tutorial::3DWorld->current->camera->{angle},
+		0, 1, 0,
+	);
+
+	# Switch to the sprite
+	$self->{material}->display;
+
+	# Draw the sprite quad.
+	# The texture seems to wrap a little unless we use the 0.01 here.
+	OpenGL::glDisable( OpenGL::GL_LIGHTING );
+	OpenGL::glBegin( OpenGL::GL_QUADS );
+	OpenGL::glTexCoord2f( 0, 0.01 ); OpenGL::glVertex3f( -1,  2,  0 ); # Top Left
+	OpenGL::glTexCoord2f( 0, 1 );    OpenGL::glVertex3f( -1,  0,  0 ); # Bottom Left
+	OpenGL::glTexCoord2f( 1, 1 );    OpenGL::glVertex3f(  1,  0,  0 ); # Bottom Right
+	OpenGL::glTexCoord2f( 1, 0.01 ); OpenGL::glVertex3f(  1,  2,  0 ); # Top Right
+	OpenGL::glEnd();
+	OpenGL::glEnable( OpenGL::GL_LIGHTING );
+
+	return 1;
+}
+
+1;
