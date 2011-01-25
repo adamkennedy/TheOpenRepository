7d99c9143151e2c32a30f4062c2bcd9625243883
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-01-25 07:23:35 +0000

    Restored the commented out ListbookPage methods

--- FBP/lib/FBP/ListbookPage.pm
+++ FBP/lib/FBP/ListbookPage.pm
@@ -7,19 +7,14 @@ our $VERSION = '0.17';
 extends 'FBP::Object';
 with    'FBP::Children';
 
-# has bitmap => (
-	# is  => 'ro',
-	# isa => 'Str',
-# );
-# 
-# has label => (
-	# is  => 'ro',
-	# isa => 'Str',
-# );
-# 
-# has select => (
-	# is  => 'ro',
-	# isa => 'Int',
-# );
+has label => (
+	is  => 'ro',
+	isa => 'Str',
+);
+
+has select => (
+	is  => 'ro',
+	isa => 'Int',
+);
 
 1;
