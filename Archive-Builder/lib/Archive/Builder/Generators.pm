9305313f908ebbb575618f3ac8d3b6eff597250a
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-02-22 23:45:45 +0000

    Update copyright

--- Archive-Builder/lib/Archive/Builder/Generators.pm
+++ Archive-Builder/lib/Archive/Builder/Generators.pm
@@ -258,7 +258,7 @@ L<Archive::Tar>, L<Archive::Zip>.
 
 =head1 COPYRIGHT
 
-Copyright 2002 - 2008 Adam Kennedy.
+Copyright 2002 - 2011 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
