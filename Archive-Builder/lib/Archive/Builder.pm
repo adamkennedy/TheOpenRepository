b9bd195d4d4c15eab92c3f4b97a6b520ed2f4c47
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-02-22 23:31:33 +0000

    Updating to Module::Install::DSL 1.00 Removing Test::ClassAPI dependency

--- Archive-Builder/lib/Archive/Builder.pm
+++ Archive-Builder/lib/Archive/Builder.pm
@@ -6,15 +6,15 @@ package Archive::Builder;
 use 5.005;
 use strict;
 use Scalar::Util     ();
-use List::Util       ();
-use File::Spec       ();
+use List::Util   1.15 ();
+use File::Spec  0.80 ();
 use File::Spec::Unix ();
-use Params::Util     '_INSTANCE', '_STRING';
-use Class::Inspector ();
-use IO::String       ();
+use Params::Util 0.22 ('_INSTANCE', '_STRING');
+use Class::Inspector 1.12 ();
+use IO::String     1.08 ();
 
 # Autoload anything any of our children might need
-use Class::Autouse 'File::Flat';
+use Class::Autouse 1.27 ('File::Flat');
 
 # Load the rest of the classes;
 use Archive::Builder::Section    ();
