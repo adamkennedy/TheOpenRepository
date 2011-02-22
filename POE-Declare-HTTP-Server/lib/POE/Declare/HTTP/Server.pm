73f25939d45e156dfd8fabb531342f403dc4ac6d
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-02-22 13:28:00 +0000

    And it works already!

--- POE-Declare-HTTP-Server/lib/POE/Declare/HTTP/Server.pm
+++ POE-Declare-HTTP-Server/lib/POE/Declare/HTTP/Server.pm
@@ -1,9 +1,198 @@
 package POE::Declare::HTTP::Server;
 
+=pod
+
+=head1 NAME
+
+POE::Declare::HTTP::Server - A simple HTTP server based on POE::Declare
+
+=head1 DESCRIPTION
+
+This module demonstrates a simple HTTP server based on L<POE::Declare>.
+
+=head1 METHODS
+
+=cut
+
 use 5.008;
 use strict;
 use warnings;
+use Params::Util         1.00 ();
+use HTTP::Headers       5.835 ();
+use HTTP::Request       5.827 ();
+use HTTP::Response      5.836 ();
+use POE                 1.299 ();
+use POE::Filter::HTTPD  1.299 ();
+use POE::Wheel::ReadWrite     ();
+use POE::Wheel::SocketFactory ();
 
 our $VERSION = '0.01';
 
-1;
+
+
+
+
+######################################################################
+# Constructor and Accessors
+
+sub new {
+	my $self = shift->SUPER::new(@_);
+
+	# Check params
+	unless ( Params::Util::_STRING($self->Hostname) ) {
+		die "Missing or invalid Hostname param";
+	}
+	unless ( Params::Util::_POSINT($self->Port) ) {
+		die "Missing or invalid Port param";
+	}
+	unless ( Params::Util::_CODE($self->Handler) ) {
+		die "Missing or invalid Handler param";
+	}
+
+	return $self;
+}
+
+use POE::Declare 0.50 {
+	Hostname => 'Param',
+	Port     => 'Param',
+	Handler  => 'Param',
+	server   => 'Internal',
+	socket   => 'Internal',
+};
+
+
+
+
+
+######################################################################
+# Control Methods
+
+sub start {
+	my $self = shift;
+	unless ( $self->spawned ) {
+		$self->spawn;
+		$self->post('startup');
+	}
+	return 1;
+}
+
+sub stop {
+	my $self = shift;
+	if ( $self->spawned ) {
+		$self->post('shutdown');
+	}
+	return 1;
+}
+
+
+
+
+
+######################################################################
+# Event Methods
+
+sub startup : Event {
+
+	# Create the socket factory
+	$_[SELF]->{server} = POE::Wheel::SocketFactory->new(
+		Reuse        => 1,
+		BindPort     => $_[SELF]->Port,
+		SuccessEvent => 'connect',
+		FailureEvent => 'error',
+	) or die 'Failed to create socket factory';
+
+	return;
+}
+
+sub connect : Event {
+	# We can only deal with one request at a time.
+	$_[SELF]->{server}->pause_accept;
+
+	# Create the socket
+	$_[SELF]->{socket} = POE::Wheel::ReadWrite->new(
+		Filter       => POE::Filter::HTTPD->new,
+		Handle       => $_[ARG0],
+		InputEvent   => 'request',
+		FlushedEvent => 'flushed',
+		ErrorEvent   => 'dropped',
+	);
+
+	return;
+}
+
+sub request : Event {
+
+	# Create the default response
+	my $response = HTTP::Response->new( 200 );
+
+	# Pass the request for processing
+	$_[SELF]->Handler->( $_[ARG0], $response );
+
+	# Send the response back to the client
+	$_[SELF]->{socket}->put( $response );
+
+	# Return and wait for the socket to flush
+	return;
+}
+
+sub flushed : Event {
+
+	# Clean up and prepare for the next request
+	$_[SELF]->{socket} = undef;
+	if ( $_[SELF]->{server} ) {
+		$_[SELF]->{server}->resume_accept;
+	}
+
+	return;
+}
+
+sub dropped : Event {
+
+	# Clean up and prepare for the next request
+	$_[SELF]->{socket} = undef;
+	if ( $_[SELF]->{server} ) {
+		$_[SELF]->{server}->resume_accept;
+	}
+
+	return;
+}
+
+sub shutdown : Event {
+
+	# Shut down the server and any active connection
+	$_[SELF]->{server} = undef;
+	$_[SELF]->{socket} = undef;
+
+}
+
+compile;
+
+=pod
+
+=head1 SUPPORT
+
+Bugs should be always be reported via the CPAN bug tracker at
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=POE-Declare>
+
+For other issues, or commercial enhancement or support, contact the author.
+
+=head1 AUTHORS
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+L<POE>, L<http://ali.as/>
+
+=head1 COPYRIGHT
+
+Copyright 2006 - 2010 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
---
+++ POE-Declare-HTTP-Server/t/03_start.t
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+# Simple start/stop operation without making any requests
+
+use strict;
+BEGIN {
+	$| = 1;
+	$^W = 1;
+}
+
+use Test::More tests => 7;
+use Test::NoWarnings;
+use POE::Declare::HTTP::Server ();
+use POE;
+
+# Test event firing order
+my $order = 0;
+sub order {
+	my $position = shift;
+	my $message  = shift;
+	is( $order++, $position, "$message ($position)" );
+}
+
+
+
+
+
+######################################################################
+# Test Class Generation
+
+# Create the server
+my $server = POE::Declare::HTTP::Server->new(
+	Hostname => '127.0.0.1',
+	Port     => '8010',
+	Handler  => sub {
+		my $request  = shift;
+		my $response = shift;
+
+		$response->header( 'Content-Type' => 'text/plain' );
+		$response->content( 'Hello World!' );
+
+		return 1;
+	},
+);
+isa_ok( $server, 'POE::Declare::HTTP::Server' );
+
+
+
+
+
+######################################################################
+# Test Execution
+
+# Set up the test session
+POE::Session->create(
+	inline_states => {
+
+		_start => sub {
+			# Start the server
+			order( 0, 'Fired main::_start' );
+			ok( $server->start, '->start ok' );
+
+			# Start the timeout
+			$_[KERNEL]->delay_set( timeout => 2 );
+		},
+
+		timeout => sub {
+			order( 1, 'Fired main::timeout' );
+			ok( $server->stop, '->stop ok' );
+		},
+
+		_stop => sub {
+			order( 2, 'Fired main::_stop' );
+		},
+
+	},
+);
+
+POE::Kernel->run;
