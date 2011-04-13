9e2c4f1ca9b389e11df950813075a8dfaf000442
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-04-13 03:17:31 +0000

    Add the beginning of this module

---
+++ POE-Declare-HTTP-Online/lib/POE/Declare/HTTP/Online.pm
@@ -0,0 +1,13 @@
+package POE::Declare::HTTP::Online;
+
+=pod
+
+=head1 NAME
+
+POE::Declare::HTTP::Online - Does your POE process have access to the web
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
