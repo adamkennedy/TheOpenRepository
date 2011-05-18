196821c4543f5d1b4b8f1850d5621d7d0a0a394f
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-05-18 02:58:09 +0000

    Fix copyright year

--- Aspect-Library-TestClass/lib/Aspect/Library/TestClass.pm
+++ Aspect-Library-TestClass/lib/Aspect/Library/TestClass.pm
@@ -148,7 +148,7 @@ distribution.
 
 Copyright 2001 by Marcel GrE<uuml>nauer
 
-Some parts copyright 2009 - 2010 Adam Kennedy.
+Some parts copyright 2009 - 2011 Adam Kennedy.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
