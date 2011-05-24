7d7dad50a17b96a03f6c99eae6f463cb7e5eed69
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-05-24 06:42:02 +0000

    Updated to Aspect 0.98. Updated to Module::Install 1.01

--- ORLite-Profile/lib/ORLite/Profile.pm
+++ ORLite-Profile/lib/ORLite/Profile.pm
@@ -48,12 +48,10 @@ When the program exits, the profing information is written to STDERR.
 use 5.008;
 use strict;
 use warnings;
-use Aspect;
+use ORLite 1.20 ();
+use Aspect 0.98;
 
-use vars qw{$VERSION};
-BEGIN {
-	$VERSION = '0.01';
-}
+our $VERSION = '0.01';
 
 aspect Profiler => call qr/^DBI::(?:connect|db::prepare|db::do|db::select|st::execute|st::fetch)/;
 
