edd7a5d697b8a9febc5854ea1e6b47d901eb6cd8
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-05-31 15:17:17 +0000

    Adding normalize

--- EVE/lib/EVE/DB.pm
+++ EVE/lib/EVE/DB.pm
@@ -3,10 +3,12 @@ package EVE::DB;
 use 5.008;
 use strict;
 use warnings;
+use ORLite         1.49 ();
 use ORLite::Mirror 1.21 {
 	url           => 'http://eve.no-ip.de/inc15/inc15-sqlite3-v1.db.bz2',
-	show_progress => 1,
 	maxage        => 999999999999,
+	show_progress => 1,
+	normalize     => 1,
 };
 
 our $VERSION = '0.01';
