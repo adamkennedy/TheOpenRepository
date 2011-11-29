8888f2679cccda7b33a3611c4fb152fd33b99004
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2011-11-29 01:52:40 +0000

    Fix copyright

--- Parse-CSV/lib/Parse/CSV.pm
+++ Parse-CSV/lib/Parse/CSV.pm
@@ -499,7 +499,7 @@ L<Text::CSV_XS>, L<http://ali.as/>
 
 =head1 COPYRIGHT
 
-Copyright 2006 Adam Kennedy.
+Copyright 2006 - 2011 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
