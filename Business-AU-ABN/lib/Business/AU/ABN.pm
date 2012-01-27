76fb1f5f7714abc377aba2a3afb8a1150d62dacf
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2012-01-27 06:29:27 +0000

    Updated copyright year

--- Business-AU-ABN/lib/Business/AU/ABN.pm
+++ Business-AU-ABN/lib/Business/AU/ABN.pm
@@ -303,7 +303,7 @@ L<http://ali.as/>
 
 =head1 COPYRIGHT
 
-Copyright 2003 - 2008 Adam Kennedy.
+Copyright 2003 - 2012 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
