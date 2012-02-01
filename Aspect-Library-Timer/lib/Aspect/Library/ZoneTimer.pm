60bc78987fc3bd74f75e113d05887be6963839ee
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2012-02-01 02:07:49 +0000

    Update copyright

--- Aspect-Library-Timer/lib/Aspect/Library/ZoneTimer.pm
+++ Aspect-Library-Timer/lib/Aspect/Library/ZoneTimer.pm
@@ -215,7 +215,7 @@ L<Aspect>, L<Aspect::Library::Timer>
 
 =head1 COPYRIGHT
 
-Copyright 2010 - 2011 Adam Kennedy.
+Copyright 2010 - 2012 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
