98d205b1bd5c5491ea6f02ff227fedaa57e3f644
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2012-02-01 04:53:40 +0000

    Completing the basic benchmark

--- Aspect/benchmark/advice.pl
+++ Aspect/benchmark/advice.pl
@@ -25,6 +25,14 @@ SCOPE: {
 		return 1;
 	}
 
+	sub after_returning {
+		return 1;
+	}
+
+	sub after_throwing {
+		return 1;
+	}
+
 	sub around {
 		return 1;
 	}
@@ -37,16 +45,27 @@ SCOPE: {
 ######################################################################
 # Aspect Setup
 
+my $foo = 1;
+
 before {
-	1;
+	$foo++;
 } call 'Foo::before';
 
 after {
-	1;
+	$foo++;
 } call 'Foo::after';
 
+after {
+	$foo++;
+} call 'Foo::after_returning' & returning;
+
+after {
+	$foo++;
+} call 'Foo::after_throwing' & throwing;
+
 around {
-	1;
+	$foo++;
+	$_->proceed
 } call 'Foo::around';
 
 
@@ -56,3 +75,13 @@ around {
 ######################################################################
 # Benchmark Execution
 
+use Benchmark ':all';
+
+timethese( 500000, {
+	control         => 'Foo::control()',
+	before          => 'Foo::before()',
+	after           => 'Foo::after()',
+	after_returning => 'Foo::after_returning()',
+	after_throwing  => 'Foo::after_throwing()',
+	around          => 'Foo::around()',
+} );
