ed1445cb518bd0b85ed0e5a9438f15a97c16b0a1
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2012-02-02 13:59:35 +0000

    Updated copyright

--- ORDB-CPANTS/lib/ORDB/CPANTS.pm
+++ ORDB-CPANTS/lib/ORDB/CPANTS.pm
@@ -182,7 +182,7 @@ Adam Kennedy
 
 =head1 COPYRIGHT
 
-Copyright 2009 Adam Kennedy.
+Copyright 2009 - 2012 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
