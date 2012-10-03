76bd6e5f14d8385ba4c01ce927857343d4c819a7
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2012-10-03 01:46:32 +0000

    Another big chunk of implementation

--- ADAMK-Release/lib/ADAMK/Release.pm
+++ ADAMK-Release/lib/ADAMK/Release.pm
@@ -4,6 +4,7 @@ use 5.10.0;
 use strict;
 use warnings;
 use Carp                          ();
+use Devel::PPPort                 ();
 use File::Spec::Functions    0.80 ':ALL';
 use File::Slurp           9999.19 ();
 use File::Find::Rule         0.32 ();
@@ -17,6 +18,8 @@ our $VERSION = '0.01';
 use Object::Tiny 1.01 qw{
 	module
 	github
+	verbose
+	no_test
 };
 
 
@@ -65,26 +68,386 @@ sub run {
 		);
 	}
 
+	# This is total bulldozer coding, there is no reason whatsoever why
+	# this stuff should be in seperate methods except that it provides
+	# a little cleaner logical breakup, and maybe I want to subclass this
+	# someday or something.
+	$self->validate;
+	$self->assemble;
+	$self->build;
 
+	return;
+}
 
+sub validate {
+	my $self = shift;
 
-
-	# Validation Phase
 	unless ( $self->dist_version ) {
-		$self->error("Failed to find version number in $file");
+		$self->error("Failed to find version number in main module");
 	}
 	unless ( $self->makefile_pl or $self->build_pl ) {
 		$self->error("Failed to find Makefile.PL or Build.PL");
 	}
 
+	return;
+}
 
+sub assemble {
+	my $self = shift;
 
-
-
-	# Assembly Phase
+	# Create MANIFEST.SKIP
 	if ( -f $self->dist_manifest_add ) {
+		$self->shell(
+			$self->bin_cat,
+			$self->shared_manifest_skip,
+			$self->dist_manifest_add,
+			'>',
+			$self->dist_manifest_skip,
+			"Failed to merge common MANIFEST.SKIP with extra one",
+		);
 
+	} elsif ( not -f $self->dist_manifest ) {
+		copy( $self->shared_manifest_skip => $self->dist_manifest_skip );
 	}
+
+	# Apply a default LICENSE file
+	unless ( -f $self->dist_license ) {
+		copy( $self->shared_license => $self->dist_license );
+	}
+
+	# Add ppport.h if any XS files use it
+	if ( $self->find_ppport->in( $self->dist_dir ) ) {
+		Devel::PPPort::WriteFile( $self->dist_ppport );
+	}
+
+	# Copy in author tests as needed
+	unless ( -f $self->dist_99_author ) {
+		foreach my $xt ( qw{ meta.t pod.t pmv.t } ) {
+			next if -f catfile( $self->dist_xt, $xt );
+			$self->copy(
+				catfile( $self->shared_dir, $xt ),
+				catfile( $self->dist_xt,     $xt ),
+			);
+		}
+	}
+
+	# Create the README file
+	unless ( -f $self->dist_readme ) {
+		my $dist_readme = $self->dist_readme;
+		$self->shell(
+			$self->bin_cat,
+			$self->module_pod,
+			"| pod2text >",
+			$dist_readme,
+			"Error while generating README file '$dist_readme'",
+		)
+	}
+
+	# Localise all newlines in text files
+	$self->file_localize->localize( $self->dist_dir );
+	
+	# Check for various unsafe things in Makefile.PL
+	if ( $self->makefile_pl ) {
+		if ( $self->makefile_pl =~ /use inc::Module::Install/ ) {
+			if ( $self->makefile_pl =~ /\bauto_install\b/ ) {
+				$self->error("Makefile.PL contains dangerous auto_install");
+			}
+		} else {
+			unless ( $self->makefile_pl =~ /use strict/ ) {
+				$self->error("Makefile.PL does not use strict");
+			}
+			unless ( $self->makefile_pl =~ /(?:use|require) \d/ ) {
+				$self->error("Makefile.PL does not declare a minimum Perl version");
+			}
+		}
+	}
+
+	# Check file permissions
+	foreach my $file ( sort $self->find_0644->in( $self->dist_dir ) ) {
+		my $mode = (stat($file))[2] & 07777;
+		next if $mode == 0644;
+		$self->shell(
+			$self->bin_chmod,
+			'0644',	
+			$file,
+			"Error setting $file to 0644 permissions",
+		);
+	}
+
+	# Make sure exe files are marked with executable permissions
+	if ( $self->find_executable->in( $self->dist_dir ) ) {
+		$self->error("Found at least one .exe file without -x unix permissions");
+	}
+
+	# Check the Changes file
+	SCOPE: {
+		# Read in the Changes file
+		unless ( -f $self->dist_changes ) {
+			$self->error("Distribution does not have a Changes file");
+		}
+		unless ( open( CHANGES, $self->dist_changes ) ) {
+			$self->error("Failed to open Changes file");
+		}
+		my @lines = <CHANGES>;
+		close CHANGES;
+		unless ( @lines >= 3 ) {
+			$self->error("Changes file is empty or too small");
+		}
+
+		# The Changes version should be the first thing on the third line
+		my $current   = $lines[2];
+		my ($version) = split /\s+/, $current;
+		unless ( $version =~ /[\d\._]{3}/ ) {
+			$self->error(
+				"Failed to find current version, or too short, in '%2'",
+				$current,
+			);
+		}
+
+		# Does it match the version in the main module
+		unless ( $version eq $self->dist_version ) {
+			$self->error(
+				"Version in Changes file (%s) does not match module version (%s)",
+				$version,
+				$self->dist_version,
+			);
+		}
+	}
+
+	# Check that the main module documentation Copyright is the current year
+	SCOPE: {
+		# Read the file
+		unless ( open( MODULE, $self->module_doc ) ) {
+			$self->error(
+				"Failed to open '%s'",
+				$self->module_doc,
+			);
+		}
+		my @lines = <MODULE>;
+		close MODULE;
+
+		# Look for the current year
+		my $year = 1900 + (localtime time)[5];
+		unless ( grep { /copyright/i and /$year/ } @lines ) {
+			$self->error("Missing Copyright, or does not refer to current year");
+		}
+
+		# TODO Merge the module to a single string
+		# my $merged = join "\n", @lines;
+		# unless ( $NO_RT ) {
+			# unless ( $merged =~ /L\<http\:\/\/rt\.cpan\.org\/.+?=([\w-]+)\>/ ) {
+				# $self->error("Failed to find a link to the public RT queue");
+			# }
+			# unless ( $dist eq $1 ) {
+				# die "Expected a public link to $dist RT queue, but found a link to the $1 queue";
+			# }
+		# }
+	}
+
+	# Touch all files to correct any potential time skews
+	foreach my $file ( $self->find_files->in( $self->dist_dir ) ) {
+		shell(
+			$self->bin_touch,
+			$file,
+			"Error while touching $file to prevent clock skew",
+		);
+	}
+
+	return;
+}
+
+sub build {
+	my $self = shift;
+
+	# Prevent environment variables from outside this script
+	# infecting the way we build things inside here.
+	local $ENV{AUTOMATED_TESTING} = '';
+	local $ENV{RELEASE_TESTING}   = '';
+
+	# Run either of the build protocols
+	if ( $self->makefile_pl ) {
+		$self->build_make;
+
+	} elsif ( $self->build_pl ) {
+		$self->build_perl;
+
+	} else {
+		$self->error("Module does not have a Makefile.PL or Build.PL");
+	}
+
+	# Confirm the build worked
+	unless ( -e $self->dist_tardist ) {
+		$self->error(
+			"Failed to create tardist at '%s'",
+			$self->dist_tardist,
+		);
+	}
+
+	return;
+}
+
+sub build_make {
+	my $self = shift;
+
+	# Create the Makefile and MANIFEST
+	$self->build_makefile;
+	$self->build_makefile_manifest;
+
+	# Create the MANIFEST
+	$self->shell(
+		$self->bin_make,
+		"manifest",
+		"Error while creating the MANIFEST",
+	);
+
+	unless ( $self->no_test ) {
+		# Test the distribution normally
+		$self->shell(
+			$self->bin_make,
+			'disttest',
+			'disttest failed',
+		);
+
+		# Test with AUTOMATED_TESTING on
+		SCOPE: {
+			local $ENV{AUTOMATED_TESTING} = 1;
+			$self->build_makefile;
+			$self->shell(
+				$self->bin_make,
+				"disttest",
+				'disttest failed',
+			);
+		}
+
+		# Test with RELEASE_TESTING on
+		SCOPE: {
+			local $ENV{RELEASE_TESTING} = 1;
+			$self->build_makefile;
+			$self->shell(
+				$self->bin_make,
+				"disttest",
+				'disttest failed',
+			);
+		}
+
+		# Test with RELEASE_TESTING and root permissions.
+		# This catches bad test script assumptions in modules related
+		# to files and permissions (File::Remove, File::Flat etc).
+		SCOPE: {
+			local $ENV{RELEASE_TESTING}   = 1;
+			$self->sudo(
+				qw{ perl Makefile.PL },
+				'Error while creating Makefile',
+			);
+			$self->sudo(
+				$self->bin_make,
+				"disttest",
+				'disttest failed',
+			);
+
+			# Clean up the leftover root files
+			$self->sudo(
+				$self->bin_make,
+				"realclean",
+				'sudo make clean failed',
+			);
+			$self->remove( "MANIFEST" );
+
+			# Rebuild from scratch again
+			$self->build_makefile;
+			$self->build_makefile_manifest;
+
+			# Run the test suite one last time to make sure we
+			# didn't break anything.
+			$self->sudo(
+				$self->bin_make,
+				"disttest",
+				'disttest failed',
+			);
+		}
+	}
+
+	# Build the tardist
+	$self->shell(
+		$self->bin_make,
+		"tardist",
+		'Error making distribution tarball',
+	);
+
+	return;
+}
+
+sub build_makefile {
+	my $self = shift;
+
+	# Execute Makefile.PL with the current environment's perl
+	$self->shell(
+		qw{ perl Makefile.PL },
+		'Error while creating Makefile',
+	);
+
+	# Add the build-system-specific elements to the META.yml
+	my $meta = YAML::Tiny->read( $self->dist_meta_yml );
+	return unless defined $meta;
+
+	# Add the resources
+	my $save = 0;
+	unless ( $meta->[0]->{resources} ) {
+		$meta->[0]->{resources} = {};
+		$save = 1;
+	}
+	unless ( $meta->[0]->{resources}->{repository} ) {
+		$meta->[0]->{resources}->{repository} = $self->dist_resource_repository;
+		$save = 1;
+	}
+	if ( $save ) {
+		YAML::Tiny->write( $self->dist_meta_yml );
+	}
+
+	return;
+}
+
+sub build_makefile_manifest {
+	my $self = shift;
+
+	$self->shell(
+		$self->bin_make,
+		"manifest",
+		"Error while creating the MANIFEST",
+	);	
+}
+
+sub build_perl {
+	my $self = shift;
+
+	# Create the Build file
+	$self->shell(
+		qw{ perl Build.PL },
+		'Error while creating Makefile',
+	);
+
+	# Create the MANIFEST file
+	$self->shell(
+		"./Build",
+		"manifest",
+		'Error while creating the MANIFEST',
+	);
+
+	unless ( $self->no_test ) {
+		# Test the distribution normally
+		$self->shell(
+			qw{ ./Build disttest },
+			'disttest failed',
+		);
+	}
+
+	# Build the tardist
+	$self->shell(
+		qw{ ./Build dist },
+		'Error making distribution tarball',
+	);
+
+	return;
 }
 
 
@@ -94,18 +457,48 @@ sub run {
 ######################################################################
 # Content and Scanning Methods
 
+# Get the main github repository url for this release
+sub dist_resource_repository {
+	my $self = shift;
+
+	return join( '',
+		"https://github.com/",
+		$self->github->username,
+		$self->github->repository,
+		'.git',
+	);
+}
+
 sub makefile_pl {
 	my $self = shift;
-	my $file = $self->dist_makefile_pl;
-	return undef unless -f $file;
-	return File::Slurp::read_file($file);
+	unless ( defined $self->{makefile_pl} ) {
+		my $file = $self->dist_makefile_pl;
+		return undef unless -f $file;
+		$self->{makefile_pl} = File::Slurp::read_file($file);
+	}
+	return $self->{makefile_pl};
 }
 
 sub build_pl {
 	my $self = shift;
-	my $file = $self->dist_build_pl;
-	return undef unless -f $file;
-	return File::Slurp::read_file($file);
+	unless ( defined $self->{build_pl} ) {
+		my $file = $self->dist_build_pl;
+		return undef unless -f $file;
+		$self->{build_pl} = File::Slurp::read_file($file);
+	}
+	return $self->{build_pl};
+}
+
+sub module_doc {
+	my $self = shift;
+	unless ( exists $self->{module_doc} ) {
+		if ( -f $self->module_pod ) {
+			$self->{module_doc} = $self->module_pod;
+		} else {
+			$self->{module_doc} = $self->module_pm;
+		}
+	}
+	return $self->{module_doc};
 }
 
 sub module_version {
@@ -167,6 +560,10 @@ sub dist {
 	my $dist = $self->module;
 	$dist =~ s/::/-/g;
 	return $dist;
+}
+
+sub dist_dir {
+	curdir();
 }
 
 sub dist_tardist {
@@ -268,43 +665,19 @@ sub shared_dir {
 ######################################################################
 # Support Methods
 
-sub sudo {
-	my $self = shift;
-	my $cmd  = shift;
-	my $env  = $self->env(
-		ADAMK_RELEASE     => 1,
-		RELEASE_TESTING   => $ENV{RELEASE_TESTING}   ? 1 : 0,
-		AUTOMATED_TESTING => $ENV{AUTOMATED_TESTING} ? 1 : 0,
-	);
-	print "> (sudo) $cmd\n" if 0; # $VERBOSE;
-	my $sudo = $self->bin_sudo;
-	my $rv   = ! system( "$sudo bash -c '$env $cmd'" );
-	if ( $rv or ! @_ ) {
-		return $rv;
+# Is a particular program installed, and where
+sub which {
+	my $self    = shift;
+	my $program = shift;
+	my ($location) = (`which $program`);
+	chomp $location;
+	unless ( $location ) {
+		$self->error("Can't find the required program '$program'. Please install it");
 	}
-	$self->error($_[0]);
-}
-
-sub shell {
-	my $self = shift;
-	my $cmd  = shift;
-	my $env  = $self->env(
-		ADAMK_RELEASE     => 1,
-		RELEASE_TESTING   => $ENV{RELEASE_TESTING}   ? 1 : 0,
-		AUTOMATED_TESTING => $ENV{AUTOMATED_TESTING} ? 1 : 0,
-	);
-	print "> $cmd\n" if 0; # $VERBOSE;
-	my $rv = ! system( "$env $cmd" );
-	if ( $rv or ! @_ ) {
-		return $rv;
+	unless ( -r $location and -x $location ) {
+		$self->error("The required program '$program' is installed, but I do not have permission to read or execute it");
 	}
-	die $_[0];
-}
-
-sub env {
-	my $self = shift;
-	my %env  = @_;
-	join ' ', map { "$_=$env{$_}" } sort keys %env;
+	return $location;
 }
 
 sub copy {
@@ -324,29 +697,56 @@ sub move {
 }
 
 sub remove {
+	my $self = shift;
 	my $path = shift;
 	if ( -e $path ) {
 		sudo( "rm -rf $path" );
 		if ( -e $path ) {
-			die "Failed to remove '$path'";
+			$self->error("Failed to remove '$path'");
 		}
 	}
 	return 1;
 }
 
-# Is a particular program installed, and where
-sub which {
+sub sudo {
 	my $self    = shift;
-	my $program = shift;
-	my ($location) = (`which $program`);
-	chomp $location;
-	unless ( $location ) {
-		$self->error("Can't find the required program '$program'. Please install it");
+	my $message = pop @_;
+	my $cmd     = join ' ', @_;
+	my $env     = $self->env(
+		ADAMK_RELEASE     => 1,
+		RELEASE_TESTING   => $ENV{RELEASE_TESTING}   ? 1 : 0,
+		AUTOMATED_TESTING => $ENV{AUTOMATED_TESTING} ? 1 : 0,
+	);
+	print "> (sudo) $cmd\n" if $self->verbose;
+	my $sudo = $self->bin_sudo;
+	my $rv   = ! system( "$sudo bash -c '$env $cmd'" );
+	if ( $rv or ! @_ ) {
+		return $rv;
 	}
-	unless ( -r $location and -x $location ) {
-		$self->error("The required program '$program' is installed, but I do not have permission to read or execute it");
+	$self->error($message);
+}
+
+sub shell {
+	my $self    = shift;
+	my $message = pop @_;
+	my $cmd     = join ' ', @_;
+	my $env     = $self->env(
+		ADAMK_RELEASE     => 1,
+		RELEASE_TESTING   => $ENV{RELEASE_TESTING}   ? 1 : 0,
+		AUTOMATED_TESTING => $ENV{AUTOMATED_TESTING} ? 1 : 0,
+	);
+	print "> $cmd\n" if $self->verbose;
+	my $rv = ! system( "$env $cmd" );
+	if ( $rv or ! @_ ) {
+		return $rv;
 	}
-	return $location;
+	$self->error($message);
+}
+
+sub env {
+	my $self = shift;
+	my %env  = @_;
+	join ' ', map { "$_=$env{$_}" } sort keys %env;
 }
 
 sub error {
