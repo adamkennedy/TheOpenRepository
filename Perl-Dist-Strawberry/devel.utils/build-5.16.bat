ad96f9d228f37b8d838bb16c790aa187aa454ae8
kmx@cpan.org <kmx@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2014-08-22 08:10:36 +0000

    moved to github

--- Perl-Dist-Strawberry/devel.utils/build-5.16.bat
+++
@@ -1,6 +0,0 @@
-@cls
-call ..\build.bat test
-perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.16.3.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
-perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\32bit-5.16.3.1-SPP-min.pp -nointeractive -norestorepoints
-perl -Mblib ..\script\perldist_strawberry -cpan_url file:///z:/_cpan-mirror/ -job ..\share\64bit-5.16.3.1.pp         -nointeractive -norestorepoints -wixbin_dir=z:\sw\Wix38
-pause
--- Perl-Dist-Strawberry/share/extra-files/ppm/README.txt
+++
@@ -1,1 +0,0 @@
-This is the temporary working directory for PPM installation
